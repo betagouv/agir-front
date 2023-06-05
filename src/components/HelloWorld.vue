07a21007080fe1e729d0b66d4a613d5827d6fb7a
dlamande <contact@dorianlamande.com>
2023-06-05 18:56:55 +0200

    utilisation du procfile et nginx

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count++">count is {{ count }} v2</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
