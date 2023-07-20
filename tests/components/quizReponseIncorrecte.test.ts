7713e80b43bb93ac5c28f91edf60926d110ef69c
dlamande <contact@dorianlamande.com>
2023-07-20 12:21:19 +0200

    ajout de tests

---
+++ tests/components/quizReponseIncorrecte.test.ts
@@ -0,0 +1,73 @@
+import QuizReponseIncorrecte from "@/components/QuizReponseIncorrecte.vue";
+import { mount } from "@vue/test-utils";
+import router from "../../src/router";
+
+test("Dans le cas d'une mauvaise réponse sur un quiz non terminé, on affiche le texte d'explication associé et un bouton continuer", async () => {
+  // GIVEN
+  const wrapper = mount(QuizReponseIncorrecte, {
+    props: {
+      etapeCourante: 1,
+      resultatFinalDuQuiz: false,
+      item: {
+        id: "id",
+        intitule: "intitule",
+        reponsesPossibles: ["reponse 1", "reponse 2"],
+        ordre: 1,
+        texteExplication: "so bad",
+        solution: "reponse 1",
+      },
+      quizViewModel: {
+        titre: "tire",
+        questions: [
+          { id: "id", intitule: "intitule", reponsesPossibles: ["reponse 1", "reponse 2"], ordre: 1, texteExplication: "so bad", solution: "reponse 1" },
+        ],
+        steps: "2",
+      },
+    },
+    global: { plugins: [router] },
+  });
+
+  // WHEN
+  const continuerButton = wrapper.find("button");
+  await wrapper.find("button").trigger("click");
+
+  // THEN
+  expect(continuerButton.text()).toBe("Continuer");
+  expect(wrapper.text()).toContain("so bad");
+  const questionSuivanteEvent = wrapper.emitted("question-suivante");
+  expect(questionSuivanteEvent[0]).toEqual([2, "ETAT_INITIAL"]);
+});
+
+test("Dans le cas d'une mauvaise réponse sur un quiz terminé, on affiche le texte d'explication associé et un lien retour au coach", async () => {
+  // GIVEN
+  const wrapper = mount(QuizReponseIncorrecte, {
+    props: {
+      etapeCourante: 1,
+      resultatFinalDuQuiz: true,
+      item: {
+        id: "id",
+        intitule: "intitule",
+        reponsesPossibles: ["reponse 1", "reponse 2"],
+        ordre: 1,
+        texteExplication: "so bad",
+        solution: "reponse 1",
+      },
+      quizViewModel: {
+        titre: "tire",
+        questions: [
+          { id: "id", intitule: "intitule", reponsesPossibles: ["reponse 1", "reponse 2"], ordre: 1, texteExplication: "so bad", solution: "reponse 1" },
+        ],
+        steps: "2",
+      },
+    },
+    global: { plugins: [router] },
+  });
+
+  // WHEN
+  const lienRetourAuCoach = wrapper.find("a");
+
+  // THEN
+  expect(lienRetourAuCoach.text()).toBe("Revenir au coach");
+  expect(wrapper.text()).toContain("so bad");
+  expect(wrapper.text()).toContain("Désolé, Vous avez perdu !");
+});
---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+/// <reference types="vitest" />
+
+import Vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [Vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
