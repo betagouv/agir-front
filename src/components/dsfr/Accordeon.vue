9753bdea57021907ace69bc672fcc0354bd384e1
octo-guic <guillaume.chauvet@octo.com>
2023-08-01 14:20:09 +0200

    [Tech] : creation du dossier dsfr et refacto du composant Accordeon (#000)

---
+++ src/components/dsfr/Accordeon.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="fr-accordion">
+    <h3 class="fr-accordion__title">
+      <button class="fr-accordion__btn fr-text-label--blue-france" aria-expanded="false" :aria-controls="nameId">Voir le détail</button>
+    </h3>
+    <div class="fr-collapse" :id="nameId">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    nameId: string
+  }>()
+</script>
