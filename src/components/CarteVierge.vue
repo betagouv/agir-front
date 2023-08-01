9952c89c4bdc927b1b2bd3d7af32dc21cf3e36e1
octo-guic <guillaume.chauvet@octo.com>
2023-08-01 14:20:41 +0200

    [Tech] : creation du composant CarteVierge (#000)

---
+++ src/components/CarteVierge.vue
@@ -0,0 +1,14 @@
+<template>
+  <aside class="fr-p-6v">
+    <slot />
+  </aside>
+</template>
+
+<style scoped>
+  aside {
+    background-color: #FFF;
+    text-align: center;
+    border-radius: 6px;
+    border: 1px solid rgba(0, 0, 0, 0.19);
+  }
+</style>
