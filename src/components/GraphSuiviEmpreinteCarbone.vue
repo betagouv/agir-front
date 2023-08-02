6fb0c1844e857cc059a1f8674c9f92a41b185a0c
octo-guic <guillaume.chauvet@octo.com>
2023-08-02 17:05:18 +0200

    [Tech] : formate les dates du graphique (#000)

--- src/components/GraphSuiviEmpreinteCarbone.vue
+++ src/components/GraphSuiviEmpreinteCarbone.vue
@@ -32,7 +32,7 @@ export default defineComponent({
       return this.datesDuGraph;
     },
 
-    getDonneesDuGraph() {      
+    getDonneesDuGraph() {
       return {
         labels: this.datesDuGraph,
         datasets: [
