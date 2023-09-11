e95685275afbb48e54dc3a28a54aef03bfb411bc
octo-guic <guillaume.chauvet@octo.com>
2023-09-11 15:51:10 +0200

    [tech] : ajout composant CarteInfo

---
+++ src/components/pages/PageAidesResultat.vue
@@ -0,0 +1,16 @@
+<template>
+  <FilDAriane
+    page-courante="Acheter un vélo"
+    :page-hierarchie="[{label: 'Aides fincancières', url: 'mes-aides'}]"
+  />
+  <AidesResultat />
+</template>
+
+<script setup lang="ts">
+  import FilDAriane from "@/components/dsfr/FilDAriane.vue";
+  import AidesResultat from "@/components/custom/AidesResultat.vue";
+
+  defineProps<{
+    titre: string
+  }>();
+</script>
