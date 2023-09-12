ef348d165594d9032e16f31901845b57f7291781
octo-guic <guillaume.chauvet@octo.com>
2023-09-12 14:35:43 +0200

    [tech] : creation du composant FormulaireAideVelo

--- src/components/custom/AidesResultat.vue
+++ src/components/custom/AidesResultat.vue
@@ -30,6 +30,7 @@
 </template>
 
 <script setup lang="ts">
+  import { ref } from 'vue';
   import Accordeon from '@/components/dsfr/Accordeon.vue';
   import AideDetail from '@/components/custom/AideDetail.vue';
   import { SimulationAideResultatViewModel } from '@/aides/ports/simulationAideResultat';
