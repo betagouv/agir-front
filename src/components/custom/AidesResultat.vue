96650c0b2b82d5bc1b2e2cd0e75d9e307c302cd7
octo-guic <guillaume.chauvet@octo.com>
2023-09-12 14:35:43 +0200

    [tech] : creation du composant FormulaireAideVelo

--- src/components/custom/AidesResultat.vue
+++ src/components/custom/AidesResultat.vue
@@ -2,33 +2,7 @@
   <h1 class="fr-h2">Acheter un vélo</h1>
   <div class="fr-grid-row fr-grid-row--gutters">
     <aside class="fr-col-lg-4 fr-col-12">
-      <div class="background--white border border-radius--md fr-p-3w fr-mb-4w">
-        <h2 class="fr-h5">Paramètres</h2>
-        <form @submit.prevent="submitForm">
-          <div class="fr-input-group">
-            <label class="fr-label" for="text-input-code-postal">
-              Votre code postal
-            </label>
-            <input class="fr-input" v-model="codePostal" name="code-postal" id="text-input-code-postal" type="text">
-          </div>
-          <div class="fr-input-group">
-            <label class="fr-label" for="text-input-rfr">
-              Revenu fiscal de référence
-            </label>
-            <input class="fr-input" v-model="revenuFiscal" name="code-postal" id="text-input-rfr" type="text">
-          </div>
-          <button class="fr-mt-2v fr-btn">Valider</button>
-        </form>
-      </div>
-      <CarteInfo>
-        <p class="fr-text--bold">
-          <span class="fr-icon-information-line" aria-hidden="true"></span>
-          Pouquoi ces questions ?
-        </p>
-        <p>Adapter les résultats au plus près de votre situation</p>
-        <p>Votre <strong>code postal</strong> permet de consulter les aides locales.</p>
-        <p>Votre <strong>revenu fiscal de référence</strong> et le <strong>nombre de parts</strong> permettent d’afficher les aides en fonction de vos ressources.</p>
-      </CarteInfo>
+      <FormulaireAideVelo @submit-simulation="submitSimulation" />
     </aside>
     <div class="fr-col-lg-8 fr-col-12">
       <div class="background--white border border-radius--md fr-p-3w">
@@ -56,27 +30,16 @@
 </template>
 
 <script setup lang="ts">
+  import { ref } from 'vue';
   import Accordeon from '@/components/dsfr/Accordeon.vue';
   import AideDetail from '@/components/custom/AideDetail.vue';
-  import CarteInfo from '@/components/custom/CarteInfo.vue';
-  import { ref } from 'vue';
+  import FormulaireAideVelo from './FormulaireAideVelo.vue';
   import { SimulationAidesVeloViewModel } from '@/aides/ports/simulerAideVelo.presenter';
-  import { SimulerAideVeloPresenterImpl } from '@/aides/adapters/simulerAideVelo.presenter.impl';
-  import { SimulerAideVeloRepositoryAxios } from '@/aides/adapters/simulerAideVelo.repository.axios';
-  import SimulerAideVeloUsecase from '@/aides/simulerAideVelo.usecase';
+  
+  const simulationAidesVeloViewModel = ref<SimulationAidesVeloViewModel | null>(null);
 
-  const codePostal = ref("");
-  const revenuFiscal = ref("");
-  const simulationAidesVeloViewModel = ref<SimulationAidesVeloViewModel>();
-
-  const submitForm = () => {
-    const useCase = new SimulerAideVeloUsecase(new SimulerAideVeloRepositoryAxios());
-
-    function mapValues(viewModels: SimulationAidesVeloViewModel) {
-      simulationAidesVeloViewModel.value = viewModels;
-    }
-
-    useCase.execute(codePostal.value, revenuFiscal.value, new SimulerAideVeloPresenterImpl(mapValues));
+  const submitSimulation = (data: SimulationAidesVeloViewModel) => {
+    simulationAidesVeloViewModel.value = data;
   };
 </script>
 
