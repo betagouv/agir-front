64bffb0b1ff032d65c7e7f3e0cc0db64f7353535
dlamande <contact@dorianlamande.com>
2023-09-13 15:12:13 +0200

    quizz cms

--- src/components/pages/PageQuiz.vue
+++ src/components/pages/PageQuiz.vue
@@ -1,8 +1,9 @@
 <template>
   <div v-if="isLoading">Chargement...</div>
-  <div v-else-if="(!quizViewModel || !store.interactionEnCours)">Une erreur est survenue</div>
+  <div v-else-if="!quizViewModel || !store.interactionEnCours">Une erreur est survenue</div>
   <PageQuizComposant
-    v-else :quiz-view-model="quizViewModel"
+    v-else
+    :quiz-view-model="quizViewModel"
     :nombreDePointsAGagner="store.interactionEnCours ? store.interactionEnCours.nombreDePointsAGagner : '0'"
     :valeurBilanCarbone="store.valeurBilanCarbone"
     :id-utilisateur="store.id"
@@ -11,30 +12,31 @@
 </template>
 
 <script setup lang="ts">
-  import { onMounted, ref } from "vue";
-  import { useRoute } from "vue-router";
-  import { ChargementQuizUsecase } from "@/quiz/chargementQuiz.usecase";
-  import { QuizRepositoryAxios } from "@/quiz/adapters/quizRepositoryAxios";
-  import { ChargementQuizPresenterImpl, QuizViewModel } from "@/quiz/adapters/chargementQuiz.presenter.impl";
-  import { utilisateurStore } from "@/store/utilisateur";
-  import PageQuizComposant from "@/components/custom/PageQuizComposant.vue";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import { ChargementQuizUsecase } from "@/quiz/chargementQuiz.usecase";
+import { ChargementQuizPresenterImpl, QuizViewModel } from "@/quiz/adapters/chargementQuiz.presenter.impl";
+import { utilisateurStore } from "@/store/utilisateur";
+import PageQuizComposant from "@/components/custom/PageQuizComposant.vue";
+import { QuizRepositoryCMSAxios } from "@/quiz/adapters/quizRepositoryCMSAxios";
 
-  const quizViewModel = ref<QuizViewModel>();
-  const store = utilisateurStore();
-  const route = useRoute();
-  const isLoading = ref<boolean>(false);  
-  
-  const mapValuesQuiz = (viewModel: QuizViewModel) => {
-    quizViewModel.value = viewModel;
-  }
-  
-  const chargementQuizz = async () => {
-    isLoading.value = true;
-    const idQuiz = Array.isArray(route.params.id) ? route.params.id[0] : route.params.id;
-    const chargementQuizzUsecase = new ChargementQuizUsecase(new QuizRepositoryAxios());
-    await chargementQuizzUsecase.execute(idQuiz, new ChargementQuizPresenterImpl(mapValuesQuiz));
-    isLoading.value = false;
-  };
+const quizViewModel = ref<QuizViewModel>();
+const store = utilisateurStore();
+const route = useRoute();
+const isLoading = ref<boolean>(false);
 
-  onMounted(chargementQuizz);
+const mapValuesQuiz = (viewModel: QuizViewModel) => {
+  quizViewModel.value = viewModel;
+  console.log(quizViewModel.value);
+};
+
+const chargementQuizz = async () => {
+  isLoading.value = true;
+  const idQuiz = Array.isArray(route.params.id) ? route.params.id[0] : route.params.id;
+  const chargementQuizzUsecase = new ChargementQuizUsecase(new QuizRepositoryCMSAxios());
+  await chargementQuizzUsecase.execute(idQuiz, new ChargementQuizPresenterImpl(mapValuesQuiz));
+  isLoading.value = false;
+};
+
+onMounted(chargementQuizz);
 </script>
