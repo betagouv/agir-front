17228feffc969767faf702769ca7bd402eec6de9
OCTO-GUIC <121949498+OCTO-GUIC@users.noreply.github.com>
2023-09-14 16:30:24 +0200

    [Feature] :creation etape simulation aides (#61)

--- src/components/custom/AidesResultat.vue
+++ src/components/custom/AidesResultat.vue
@@ -1,14 +1,16 @@
 <template>
   <h1 class="fr-h2">{{ titre }}</h1>
-  <div class="fr-grid-row fr-grid-row--gutters">
-    <aside class="fr-col-lg-4 fr-col-12">
-      <slot />
-    </aside>
+  <div v-if="!simulationAidesViewModel">
+    <slot name="formulaire"></slot>
+  </div>
+  <div v-else class="fr-grid-row fr-grid-row--gutters">
+    <div class="fr-col-lg-4 fr-col-12">
+      <slot name="asideResultatAides"></slot>
+    </div>
     <div class="fr-col-lg-8 fr-col-12">
       <div class="background--white border border-radius--md fr-p-3w">
         <h2 class="fr-h4">Vous pouvez bénéficier des aides vélo suivantes :</h2>
-        <p v-if="!simulationAidesViewModel">Veuillez remplir le formulaire pour accèder à vos aides.</p>
-        <div v-else v-for="(aides, index) in simulationAidesViewModel" :key="index">
+        <div v-for="(aides, index) in simulationAidesViewModel" :key="index">
           <Accordeon v-if="aides.length" :label="`Acheter un vélo ${index}`" :nameId="`aides-${index}`">
             <ul class="list-style-none fr-m-0 fr-p-0">
               <li v-for="(aide, index) in aides" :key="index">
@@ -33,12 +35,10 @@
   import Accordeon from '@/components/dsfr/Accordeon.vue';
   import AideDetail from '@/components/custom/AideDetail.vue';
   import { SimulationAideResultatViewModel } from '@/aides/ports/simulationAideResultat';
-
   defineProps<{
     titre: string
     simulationAidesViewModel: SimulationAideResultatViewModel | null
   }>();
- 
 </script>
 
 <style scoped>
--- src/components/pages/PageAidesRetrofit.vue
+++ src/components/pages/PageAidesRetrofit.vue
@@ -4,7 +4,12 @@
     :page-hierarchie="[{label: 'Aides fincancières', url: 'mes-aides'}]"
   />
   <AidesResultat :simulation-aides-view-model="simulationAidesRetrofitViewModel" :titre="titrePage">
-    <FormulaireAideRetrofit @submit-simulation="submitSimulation" />
+    <template v-slot:formulaire>
+      <FormulaireAideRetrofit @submit-simulation="submitSimulation" />     
+    </template>
+    <template v-slot:asideResultatAides>
+      <AsideAideRetrofit @reset-simulation="resetSimulation" :code-postal="codePostal" :revenu-fiscal="revenuFiscal" />
+    </template>
   </AidesResultat>
 </template>
 
@@ -13,14 +18,21 @@
   import FilDAriane from "@/components/dsfr/FilDAriane.vue";
   import AidesResultat from "@/components/custom/AidesResultat.vue";
   import FormulaireAideRetrofit from '@/components/custom/FormulaireAideRetrofit.vue';
+  import AsideAideRetrofit from '@/components/custom/AsideAideRetrofit.vue';
   import { SimulationAideResultatViewModel } from '@/aides/ports/simulationAideResultat';
   
   const titrePage = "Mes aides - Retrofit";
   const simulationAidesRetrofitViewModel = ref<SimulationAideResultatViewModel | null>(null);
+  const codePostal = ref<string>('');
+  const revenuFiscal = ref<string>('');
 
-  const submitSimulation = (data: SimulationAideResultatViewModel) => {
-    simulationAidesRetrofitViewModel.value = data;    
+  const submitSimulation = (data: SimulationAideResultatViewModel, dataCodePostal: string, dataRevenuFiscal: string) => {
+    codePostal.value = dataCodePostal;
+    revenuFiscal.value = dataRevenuFiscal;
+    simulationAidesRetrofitViewModel.value = data;
   };
+
+  const resetSimulation = () => simulationAidesRetrofitViewModel.value = null;
 
   defineProps<{
     titre: string
