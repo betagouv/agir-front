6a4a3e3324591a2e10c545f1b6d26fa29ce06be2
octo-guic <guillaume.chauvet@octo.com>
2023-09-14 10:41:05 +0200

    [feature] : découpe simulation aide en 2 étapes et ajout de slot a AideResultat

--- src/components/custom/FormulaireAideVelo.vue
+++ src/components/custom/FormulaireAideVelo.vue
@@ -1,31 +1,37 @@
 <template>
-  <div class="background--white border border-radius--md fr-p-3w fr-mb-4w">
-    <h2 class="fr-h5">Paramètres</h2>
-    <form @submit.prevent="submitForm">
-      <div class="fr-input-group">
-        <label class="fr-label" for="text-input-code-postal">
-          Votre code postal
-        </label>
-        <input class="fr-input" v-model="codePostal" name="code-postal" id="text-input-code-postal" type="text">
+  <div class="fr-grid-row fr-grid-row--gutters">
+    <div class="fr-col-lg-8">
+      <div class="background--white border border-radius--md fr-p-3w fr-mb-4w">
+        <h2 class="fr-h5">Quelques questions pour calculer les aides vélo adaptées</h2>
+        <form @submit.prevent="submitForm">
+          <div class="fr-input-group">
+            <label class="fr-label" for="text-input-code-postal">
+              Votre code postal
+            </label>
+            <input class="fr-input" v-model="codePostal" name="code-postal" id="text-input-code-postal" type="text">
+          </div>
+          <div class="fr-input-group">
+            <label class="fr-label" for="text-input-rfr">
+              Revenu fiscal de référence
+            </label>
+            <input class="fr-input" v-model="revenuFiscal" name="code-postal" id="text-input-rfr" type="text">
+          </div>
+          <button class="fr-mt-2v fr-btn">Valider</button>
+        </form>
       </div>
-      <div class="fr-input-group">
-        <label class="fr-label" for="text-input-rfr">
-          Revenu fiscal de référence
-        </label>
-        <input class="fr-input" v-model="revenuFiscal" name="code-postal" id="text-input-rfr" type="text">
-      </div>
-      <button class="fr-mt-2v fr-btn">Valider</button>
-    </form>
+    </div>
+    <div class="fr-col-lg-4">
+      <CarteInfo>
+        <p class="fr-text--bold">
+          <span class="fr-icon-information-line" aria-hidden="true"></span>
+          Pouquoi ces questions ?
+        </p>
+        <p>Adapter les résultats au plus près de votre situation</p>
+        <p>Votre <strong>code postal</strong> permet de consulter les aides locales.</p>
+        <p>Votre <strong>revenu fiscal de référence</strong> et le <strong>nombre de parts</strong> permettent d’afficher les aides en fonction de vos ressources.</p>
+      </CarteInfo>
+    </div>
   </div>
-  <CarteInfo>
-    <p class="fr-text--bold">
-      <span class="fr-icon-information-line" aria-hidden="true"></span>
-      Pouquoi ces questions ?
-    </p>
-    <p>Adapter les résultats au plus près de votre situation</p>
-    <p>Votre <strong>code postal</strong> permet de consulter les aides locales.</p>
-    <p>Votre <strong>revenu fiscal de référence</strong> et le <strong>nombre de parts</strong> permettent d’afficher les aides en fonction de vos ressources.</p>
-  </CarteInfo>
 </template>
 
 <script setup lang="ts">
@@ -34,7 +40,7 @@
   import { SimulerAideVeloPresenterImpl } from '@/aides/adapters/simulerAideVelo.presenter.impl';
   import { SimulerAideVeloRepositoryAxios } from '@/aides/adapters/simulerAideVelo.repository.axios';
   import SimulerAideVeloUsecase from '@/aides/simulerAideVelo.usecase';
-import { SimulationAideResultatViewModel } from '@/aides/ports/simulationAideResultat';
+  import { SimulationAideResultatViewModel } from '@/aides/ports/simulationAideResultat';
 
   const emit = defineEmits(['submit-simulation'])
   const codePostal = ref("");
@@ -44,7 +50,7 @@ import { SimulationAideResultatViewModel } from '@/aides/ports/simulationAideRes
     const useCase = new SimulerAideVeloUsecase(new SimulerAideVeloRepositoryAxios());
 
     function mapValues(viewModels: SimulationAideResultatViewModel) {
-      emit('submit-simulation', viewModels);
+      emit('submit-simulation', viewModels, codePostal.value, revenuFiscal.value);
     }
 
     useCase.execute(codePostal.value, revenuFiscal.value, new SimulerAideVeloPresenterImpl(mapValues));
--- src/components/pages/PageAidesVelo.vue
+++ src/components/pages/PageAidesVelo.vue
@@ -4,7 +4,12 @@
     :page-hierarchie="[{label: 'Aides fincancières', url: 'mes-aides'}]"
   />
   <AidesResultat :simulation-aides-view-model="simulationAidesVeloViewModel" :titre="titrePage">
-    <FormulaireAideVelo @submit-simulation="submitSimulation" />
+    <template v-slot:formulaire>
+      <FormulaireAideVelo @submit-simulation="submitSimulation" />
+    </template>
+    <template v-slot:asideResultatAides>
+      <AsideAideVelo @reset-simulation="resetSimulation" :code-postal="codePostal" :revenu-fiscal="revenuFiscal"/>
+    </template>
   </AidesResultat>
 </template>
 
@@ -13,14 +18,21 @@
   import FilDAriane from "@/components/dsfr/FilDAriane.vue";
   import AidesResultat from "@/components/custom/AidesResultat.vue";
   import FormulaireAideVelo from '@/components/custom/FormulaireAideVelo.vue';
+  import AsideAideVelo from '@/components/custom/AsideAideVelo.vue';
   import { SimulationAideResultatViewModel } from '@/aides/ports/simulationAideResultat';
   
   const titrePage = "Acheter un vélo";
   const simulationAidesVeloViewModel = ref<SimulationAideResultatViewModel | null>(null);
+  const codePostal = ref<string>('');
+  const revenuFiscal = ref<string>('');
 
-  const submitSimulation = (data: SimulationAideResultatViewModel) => {
+  const submitSimulation = (data: SimulationAideResultatViewModel, dataCodePostal: string, dataRevenuFiscal: string) => {
+    codePostal.value = dataCodePostal;
+    revenuFiscal.value = dataRevenuFiscal;    
     simulationAidesVeloViewModel.value = data;
   };
+
+  const resetSimulation = () => simulationAidesVeloViewModel.value = null;
 
   defineProps<{
     titre: string
