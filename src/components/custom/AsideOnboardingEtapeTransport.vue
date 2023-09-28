1bef8dd68aa9d9c16a127c0a9de8367f5b50c0d8
octo-guic <guillaume.chauvet@octo.com>
2023-09-28 14:39:12 +0200

    [feature] : cration composant custom Asideonboarding

---
+++ src/components/custom/AsideOnboardingEtapeTransport.vue
@@ -0,0 +1,16 @@
+<template>
+  <CarteInfo>
+    <p class="fr-text--bold">
+      <span class="fr-icon-information-line" aria-hidden="true"></span>
+      Pouquoi ces questions ?
+    </p>
+    <p class="fr-text--sm">Les transports sont un des principaux postes de notre empreinte écologique.</p>
+    <p class="fr-text--sm fr-mb-0">
+      Par exemple, ils représentent XX% des émissions de gaz à effet de serre moyenne des ménages français.
+    </p>
+  </CarteInfo>
+</template>
+
+<script setup lang="ts">
+  import CarteInfo from '@/components/custom/CarteInfo.vue';
+</script>
