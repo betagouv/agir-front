559ced098a7098a045145eb50dbb68560f681cb9
octo-guic <guillaume.chauvet@octo.com>
2023-09-28 16:31:34 +0200

    [tech] : creation composant dsfr inputCodePostal

---
+++ src/components/dsfr/InputCodePostal.vue
@@ -0,0 +1,5 @@
+<template>
+  <label class="fr-label" for="code-postal">Code postal</label>
+  <input class="fr-input" name="code-postal" id="code-postal" type="text" />
+</template>
+<script setup lang="ts"></script>
