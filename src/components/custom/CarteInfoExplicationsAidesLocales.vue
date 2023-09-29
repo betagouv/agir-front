abc603819449ed6337feaf16982a9303c02841a0
dlamande <contact@dorianlamande.com>
2023-09-29 13:34:10 +0200

    demande info aide retrofit

---
+++ src/components/custom/CarteInfoExplicationsAidesLocales.vue
@@ -0,0 +1,15 @@
+<template>
+  <CarteInfo>
+    <p class="fr-text--bold">
+      <span class="fr-icon-information-line" aria-hidden="true"></span>
+      Pouquoi ces questions ?
+    </p>
+    <p>Adapter les résultats au plus près de votre situation</p>
+    <p>Votre <strong>code postal</strong> permet de consulter les aides locales.</p>
+    <p>
+      Votre <strong>revenu fiscal de référence</strong> et le <strong>nombre de parts</strong> permettent d’afficher les
+      aides en fonction de vos ressources.
+    </p>
+  </CarteInfo>
+</template>
+<script setup lang="js"></script>
