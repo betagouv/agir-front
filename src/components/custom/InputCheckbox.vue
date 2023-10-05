47bae6c4ecafb096dbdbbb43302e5ab5b3bad126
OCTO-GUIC <121949498+OCTO-GUIC@users.noreply.github.com>
2023-10-05 17:50:35 +0200

    [fix] : couleur bordure radoi button et checkbox (#86)

--- src/components/custom/InputCheckbox.vue
+++ src/components/custom/InputCheckbox.vue
@@ -4,7 +4,11 @@
       Plusieurs réponses sont possibles
     </legend>
     <div class="fr-fieldset__element" v-for="option in options" :key="option.id">
-      <div class="fr-checkbox-group checkbox-group--custom fr-p-2w border">
+      <div
+        :class="`fr-checkbox-group checkbox-group--custom fr-p-2w border ${
+          checkedNames.includes(option.id) ? 'fr-text--bold border--bleu-dark' : ''
+        }`"
+      >
         <input :id="option.id" :value="option.id" type="checkbox" v-model="checkedNames" @change="updateValue" />
         <label class="fr-label" :for="option.id">{{ option.label }}</label>
       </div>
@@ -33,11 +37,3 @@
     emit('update:modelValue', checkedNames.value);
   };
 </script>
-
-<style scoped>
-  /* CSS semantic: non compatible Firefox -> to refacto */
-  .fr-checkbox-group:has(input:checked) {
-    border-color: var(--blue-france-sun-113-625);
-    font-weight: bold;
-  }
-</style>
