608ba98f0b4453576e5e3ee5103ab1472f6b4eda
octo-guic <guillaume.chauvet@octo.com>
2023-10-10 17:26:16 +0200

    fix background white

--- src/components/dsfr/Footer.vue
+++ src/components/dsfr/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer v-if="getUtilisateur" class="fr-footer" role="contentinfo">
+  <footer v-if="getUtilisateur" class="fr-footer background--white" role="contentinfo">
     <div class="fr-container">
       <div class="fr-footer__body">
         <div class="fr-footer__brand fr-enlarge-link">
