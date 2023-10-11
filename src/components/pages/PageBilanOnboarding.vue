ee49cf5b86258a8590e112973e96f778aac4e53c
octo-guic <guillaume.chauvet@octo.com>
2023-10-11 17:00:19 +0200

    [feat] : ajout section coach kesako

--- src/components/pages/PageBilanOnboarding.vue
+++ src/components/pages/PageBilanOnboarding.vue
@@ -34,6 +34,32 @@
     </div>
     <p class="fr-text--bold fr-mb-0">Répartition des efforts à engager dans le cadre de la planification écologique</p>
   </section>
+  <section class="fr-py-6w fr-px-2w">
+    <h2 class="fr-h2">Qu’est-ce que le coach a pour vous ?</h2>
+  </section>
+  <section class="background--white fr-py-6w fr-px-2w">
+    <h2 class="fr-h2">Ce qu’ils en disent</h2>
+    <div class="fr-grid-row fr-grid-row--gutters">
+      <div class="fr-grid-row fr-grid-row--middle fr-col-md-6">
+        <img src="/anneDeGap.png" class="img-object-fit-contain fr-mr-3w" alt="" />
+        <div class="fr-col fr-col-md-8">
+          <p class="fr-mb-0">
+            « <strong>Agir</strong> m’a proposé des <strong>solutions concrètes pour moi et près de chez moi.</strong> »
+          </p>
+          <span class="fr-text--bold fr-text--sm">Anne de Gap</span>
+        </div>
+      </div>
+      <div class="fr-grid-row fr-grid-row--middle fr-col-md-6">
+        <img src="/michelDeSoissons.png" class="img-object-fit-contain fr-mr-3w" alt="" />
+        <div class="fr-col fr-col-md-8">
+          <p class="fr-mb-0">
+            « Je ne savais pas qu’il y avait autant d’aides, j’ai réussi à <strong>faire des économies !</strong> »
+          </p>
+          <span class="fr-text--bold fr-text--sm">Michel de Soissons</span>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
