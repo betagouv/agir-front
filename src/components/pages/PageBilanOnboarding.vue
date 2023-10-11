a446a0268fb93c4827d9101b9cc35ea69e0b1320
octo-guic <guillaume.chauvet@octo.com>
2023-10-11 15:47:05 +0200

    [feat] : ajout section Où en est-on ?

--- src/components/pages/PageBilanOnboarding.vue
+++ src/components/pages/PageBilanOnboarding.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Votre 1er bilan</h1>
   <h2>Quels sont les impacts de vos usages ?</h2>
-  <div class="fr-grid-row fr-grid-row--gutters">
+  <section class="fr-grid-row fr-grid-row--gutters fr-pb-6w">
     <div class="fr-col-lg-6 fr-col-12">
       <CarteVierge>
         <div class="legende fr-text--sm fr-text--bold">
@@ -20,7 +20,20 @@
         S'inscrire
       </router-link>
     </div>
-  </div>
+  </section>
+  <section class="background--white text--center fr-py-6w fr-px-2w">
+    <h2 class="fr-h2">Où en est-on ?</h2>
+    <p class="fr-text--lg">La France s’engage à réduire ses émissions de gaz à effet de serre de XX% d’ici 2030</p>
+    <div class="fr-grid-row fr-grid-row--center fr-grid-row--middle fr-mb-2w fr-text--lg text--left">
+      <img src="/graph-bilan-onboarding.svg" class="fr-mr-3w" alt="" />
+      <div class="fr-grid-row flex-column graph-legend">
+        <span class="fr-text--bold text--bleu">25% Particuliers (c’est nous !)</span>
+        <span>50% Entreprises</span>
+        <span>25% État et collectivités territoriales</span>
+      </div>
+    </div>
+    <p class="fr-text--bold fr-mb-0">Répartition des efforts à engager dans le cadre de la planification écologique</p>
+  </section>
 </template>
 
 <script setup lang="ts">
@@ -73,5 +86,26 @@
   .legende span:nth-child(2)::before {
     right: 0;
     background-color: #e32416;
+  }
+
+  .graph-legend span::before {
+    content: '';
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 50%;
+    margin-right: 0.25rem;
+  }
+
+  .graph-legend span:first-child::before {
+    background: var(--blue-france-sun-113-625);
+  }
+
+  .graph-legend span:nth-child(2)::before {
+    background: #919191;
+  }
+
+  .graph-legend span:nth-child(3)::before {
+    background: #353535;
   }
 </style>
