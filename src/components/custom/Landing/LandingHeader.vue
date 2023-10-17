e844e4fa053fe9a26ff87b5b2102056fc3fa849f
octo-guic <guillaume.chauvet@octo.com>
2023-10-17 16:02:23 +0200

    feature: creation component LandingHeader

---
+++ src/components/custom/Landing/LandingHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--middle">
+    <div class="fr-col-md-7">
+      <h1>Réduire votre empreinte écologique selon vos moyens, vos lieux de vie et vos envies</h1>
+      <p class="fr-text--xl">
+        Faites un premier pas vers la transition écologique en estimant votre impact en 30 secondes chrono !
+      </p>
+      <router-link :to="{ name: 'pre-onboarding' }" class="fr-btn fr-btn--icon-right fr-icon-arrow-right-line">
+        Commencer
+      </router-link>
+    </div>
+    <div class="fr-col-md-5">
+      <img src="/landing-header.jpg" class="border-radius--full max-full-width" alt="" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
--- src/components/pages/PageAccueil.vue
+++ src/components/pages/PageAccueil.vue
@@ -1,34 +1,11 @@
 <template>
-  <div class="fr-container">
-    <div
-      class="background--white border-radius--md border fr-grid-row fr-grid-row--gutters fr-grid-row--center fr-grid-row--middle"
-    >
-      <div class="fr-col-10 fr-col-md-8 fr-p-3w fr-p-md-9w">
-        <h1>Agir !</h1>
-        <h2>L’accompagnement personnalisé pour réduire votre empreinte écologique</h2>
-        <ul>
-          <li>Mesurez les impacts de vos usages au quotidien</li>
-          <li>Simulez des aides pertinentes adaptées à votre situation</li>
-          <li>Testez vos connaissances</li>
-        </ul>
-
-        <div v-if="!isAuthentifie">
-          <router-link class="fr-link" :to="{ name: 'pre-onboarding' }">Commencer !</router-link>
-          <p class="fr-mt-3w">
-            Déjà un compte ?
-            <router-link class="fr-link" :to="{ name: 'authentification' }">Se connecter !</router-link>
-          </p>
-        </div>
-      </div>
+  <section class="background--white fr-py-10w">
+    <div class="fr-container">
+      <LandingHeader />
     </div>
-  </div>
+  </section>
 </template>
 
 <script setup lang="ts">
-  import { utilisateurStore } from '@/store/utilisateur';
-  import { computed } from 'vue';
-
-  const isAuthentifie = computed(() => {
-    return utilisateurStore().utilisateur.nom.length > 0;
-  });
+  import LandingHeader from '@/components/custom/Landing/LandingHeader.vue';
 </script>
