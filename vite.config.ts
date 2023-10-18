8310ecdf90f95298a1ab84813f16b47545956830
Denis B <denis.baudot@gmail.com>
2023-10-18 16:15:18 +0200

    try vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import * as path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export const viteConfig = {
   plugins: [vue()],
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
+    headers: {
+      'Content-Security-Policy': `style-src 'nonce-random' 'self'; default-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self'; connect-src 'self'; object-src 'self'; media-src 'self'; frame-src 'self' https://www.youtube.com`,
+    },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 };
