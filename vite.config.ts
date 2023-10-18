f0afd212a2d71d8513983deb850578a3f830444e
Denis B <denis.baudot@gmail.com>
2023-10-18 16:27:26 +0200

    add cloudinary to csp

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ export const viteConfig = {
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
-    headers: {
-      'Content-Security-Policy': `style-src 'nonce-random' 'self'; default-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self'; connect-src 'self'; object-src 'self'; media-src 'self'; frame-src 'self' https://www.youtube.com`,
-    },
   },
   resolve: {
     alias: {
