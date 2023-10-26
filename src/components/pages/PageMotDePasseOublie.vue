62e18fe7ecbab004b558c94ff554c293cefe2aff
dlamande <contact@dorianlamande.com>
2023-10-26 14:27:53 +0200

    retours mr

--- src/components/pages/PageMotDePasseOublie.vue
+++ src/components/pages/PageMotDePasseOublie.vue
@@ -4,13 +4,15 @@
       class="fr-col-12 fr-col-lg-6 fr-mx-auto fr-mb-0 background--white fr-p-4w border border-radius--md"
       @submit.prevent="recupererMotDePasse"
     >
-      <fieldset class="fr-mb-0 fr-fieldset">
-        <legend class="fr-fieldset__legend fr-px-0 fr-mx-0 text--center" id="identity-fieldset-legend">
-          <h2>Saisissez votre adresse électronique pour pouvoir redéfinir un mot de passe</h2>
+      <fieldset class="fr-mb-0 fr-fieldset" aria-labelledby="legend-email-forgot">
+        <legend class="fr-fieldset__legend fr-px-0 fr-mx-0 text--center" id="legend-email-forgot">
+          <h1 class="fr-text--xl">Saisissez votre adresse électronique pour pouvoir redéfinir un mot de passe</h1>
         </legend>
-        <div class="fr-col-12">
+        <div class="fr-fieldset__element">
           <InputMail label="Adresse électronique" v-model="email" name="email" />
-          <button class="fr-btn display-block text--center full-width" :disabled="!email" type="submit">Valider</button>
+        </div>
+        <div class="fr-fieldset__element">
+          <button class="fr-btn display-block text--center" :disabled="!email" type="submit">Valider</button>
         </div>
       </fieldset>
     </form>
--- src/components/pages/PageMotDePasseOublieRedefinirMotDePasse.vue
+++ src/components/pages/PageMotDePasseOublieRedefinirMotDePasse.vue
@@ -4,27 +4,33 @@
       class="fr-col-12 fr-col-lg-6 fr-mx-auto fr-mb-0 background--white fr-p-4w border border-radius--md"
       @submit.prevent="definirMotDePasse"
     >
-      <fieldset class="fr-mb-0 fr-fieldset">
-        <legend class="fr-fieldset__legend fr-px-0 fr-mx-0 text--center" id="identity-fieldset-legend">
-          <h2>Choisir un nouveau mot de passe</h2>
+      <fieldset class="fr-mb-0 fr-fieldset" aria-labelledby="legend-password-modify">
+        <legend class="fr-fieldset__legend fr-px-0 fr-mx-0 text--center" id="legend-password-modify">
+          <h1 class="fr-text--xl">Choisir un nouveau mot de passe</h1>
         </legend>
         <p>
           Choisissez un mot de passe et saisissez le code envoyé à l'adresse <strong>: {{ email }}</strong>
         </p>
-        <div class="fr-col-12">
+        <div class="fr-fieldset__element">
           <InputPassword
             class="fr-col-md-11 fr-mt-2w"
             v-model="motDePasse"
             @update:mot-de-passe-valide="onMotDePasseValideChanged"
           />
+        </div>
+        <div class="fr-fieldset__element">
           <InputText class="fr-col-md-5 fr-mt-2w" v-model="code" name="code" label="Code à usage unique" />
+        </div>
+        <div class="fr-fieldset__element">
           <button
-            class="fr-btn display-block text--center full-width"
+            class="fr-btn display-block text--center"
             :disabled="!motDePasseValide || code.length === 0"
             type="submit"
           >
             Valider
           </button>
+        </div>
+        <div class="fr-messages-group">
           <Alert
             v-if="erreur"
             class="fr-col-12 fr-mt-2w"
