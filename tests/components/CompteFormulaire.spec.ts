c94794a12dcb0cc72b1822e228fa8617ecf3bbbd
OCTO-GUIC <121949498+OCTO-GUIC@users.noreply.github.com>
2023-11-13 17:02:29 +0100

    [tech] : ajout test compte formulaire (#135)

--- tests/components/CompteFormulaire.spec.ts
+++ tests/components/CompteFormulaire.spec.ts
@@ -1,16 +1,17 @@
-import PageCompteFormulaire from '../../src/components/custom/PageCompteFormulaire.vue';
-import { SpyInstance, beforeEach, describe, it } from 'vitest';
+import { describe, it, beforeEach } from 'vitest';
 import { fireEvent, render } from '@testing-library/vue';
-import { CompteUtlisateurViewModel } from '../../src/compte/adapters/compteUtilisateur.presenter.impl';
-import { ChargementCommunesUsecase } from '../../src/communes/chargementCommunesUsecase';
+import PageCompteFormulaire from '@/components/custom/PageCompteFormulaire.vue';
+import { CompteUtlisateurViewModel } from '@/compte/adapters/compteUtilisateur.presenter.impl';
+import { ChargementCommunesUsecase } from '@/communes/chargementCommunesUsecase';
+import { MettreAJourCompteUtilisateurUsecase } from '@/compte/mettreAJourCompteUtilisateur.usecase';
 
 const compteUtlisateurViewModel: CompteUtlisateurViewModel = {
   id: 'idUser',
   nom: 'Claude',
   prenom: 'Dubois',
   mail: 'claud-dubois@exemple.com',
-  codePostal: '75001',
-  commune: 'PARIS 01',
+  codePostal: '75002',
+  commune: 'PARIS 02',
   revenuFiscal: '123456',
 };
 
@@ -19,51 +20,115 @@ const props = { compteUtlisateurViewModel };
 describe('Compte - Formulaire', () => {
   let titre: HTMLElement;
   let inputNom: HTMLInputElement;
+  let inputPrenom: HTMLInputElement;
   let inputEmail: HTMLInputElement;
   let inputCodePostal: HTMLInputElement;
+  let selectCommune: HTMLSelectElement;
+  let inputRevenuFiscal: HTMLInputElement;
   let submitBouton: HTMLInputElement;
 
   beforeEach(() => {
     // GIVEN
-    let chargementCommunesUsecaseMock: SpyInstance<[string], Promise<string[]>>;
-
-    chargementCommunesUsecaseMock = vi
-      .spyOn(ChargementCommunesUsecase.prototype, 'execute')
-      .mockImplementation((_codePostal: string) => Promise.resolve([]));
+    vi.spyOn(ChargementCommunesUsecase.prototype, 'execute').mockImplementation((_codePostal: string) =>
+      Promise.resolve(['PARIS 01', 'PARIS 02'])
+    );
 
     const { getByRole } = render(PageCompteFormulaire, { props });
 
     inputNom = getByRole('textbox', { name: 'Nom' });
+    inputPrenom = getByRole('textbox', { name: 'Prénom' });
     inputEmail = getByRole('textbox', { name: /Adresse électronique/i });
     inputCodePostal = getByRole('textbox', { name: /Code postal/i });
+    selectCommune = getByRole('combobox', { name: /Sélection d'une commune/i });
+    inputRevenuFiscal = getByRole('textbox', { name: 'Revenu fiscal de référence' });
     submitBouton = getByRole('button', { name: 'Mettre à jour' });
     titre = getByRole('heading', { level: 2, name: 'Identité personnelle' });
   });
 
-  it("pré-remplit et affiche les champs associés au compte de l'utilisateur", () => {
+  it("pré-rempli et affiche les champs associés au compte de l'utilisateur", () => {
     // WHEN
     // THEN
     expect(titre).toBeDefined();
     expect(inputNom).toBeDefined();
     expect(inputNom.value).toBe('Claude');
+    expect(inputPrenom).toBeDefined();
+    expect(inputPrenom.value).toBe('Dubois');
     expect(inputEmail).toBeDefined();
     expect(inputEmail.value).toBe('claud-dubois@exemple.com');
     expect(inputCodePostal).toBeDefined();
-    expect(inputCodePostal.value).toBe('75001');
+    expect(inputCodePostal.value).toBe('75002');
+    expect(selectCommune).toBeDefined();
+    expect(selectCommune.value).toBe('PARIS 02');
+    expect(inputRevenuFiscal).toBeDefined();
+    expect(inputRevenuFiscal.value).toBe('123456');
     expect(submitBouton).toBeDefined();
   });
 
   describe('quand je mets à jour mes informations', () => {
     it('la valeur des champs est modifiée', async () => {
       // WHEN
-      await fireEvent.change(inputNom, { target: { value: 'John' } });
-      await fireEvent.change(inputEmail, { target: { value: 'john-dubois@exemple.com' } });
-      await fireEvent.change(inputCodePostal, { target: { value: '75002' } });
+      await fireEvent.update(inputNom, 'John');
+      await fireEvent.update(inputPrenom, 'Smith');
+      await fireEvent.update(inputEmail, 'john-smith@exemple.com');
+      await fireEvent.update(inputRevenuFiscal, '654321');
+      await fireEvent.update(inputCodePostal, '75001');
+      await fireEvent.update(selectCommune, 'PARIS 01');
 
       // THEN
       expect(inputNom.value).toBe('John');
-      expect(inputEmail.value).toBe('john-dubois@exemple.com');
-      expect(inputCodePostal.value).toBe('75002');
+      expect(inputPrenom.value).toBe('Smith');
+      expect(inputEmail.value).toBe('john-smith@exemple.com');
+      expect(inputRevenuFiscal.value).toBe('654321');
+      expect(inputCodePostal.value).toBe('75001');
+      expect(selectCommune.value).toBe('PARIS 01');
+    });
+
+    describe("quand l'utilisateur soumet ses modifications", () => {
+      const mettreAJourCompteUtilisateurUsecaseMock = vi
+        .spyOn(MettreAJourCompteUtilisateurUsecase.prototype, 'execute')
+        .mockImplementation((_compteUtlisateurViewModel: CompteUtlisateurViewModel) => Promise.resolve(true));
+
+      describe('si les informations sont correctes', () => {
+        it('appele le usecase mettre à jour compte utilisateur avec les bonnes informations', async () => {
+          // WHEN
+          await fireEvent.update(inputNom, 'John');
+          await fireEvent.update(inputPrenom, 'Smith');
+          await fireEvent.update(inputEmail, 'john-smith@exemple.com');
+          await fireEvent.update(inputRevenuFiscal, '654321');
+          await fireEvent.update(inputCodePostal, '75001');
+          await fireEvent.update(selectCommune, 'PARIS 01');
+          await fireEvent.click(submitBouton);
+
+          expect(mettreAJourCompteUtilisateurUsecaseMock).toHaveBeenNthCalledWith(1, {
+            id: 'idUser',
+            nom: 'John',
+            prenom: 'Smith',
+            mail: 'john-smith@exemple.com',
+            revenuFiscal: '654321',
+            codePostal: '75001',
+            commune: 'PARIS 01',
+          });
+        });
+
+        it.todo('affiche un message de confirmation', async () => {
+          // getByRole('alert');
+        });
+      });
+
+      describe('quand les informations sont incorrectes', () => {
+        it("n'appele pas le usecase", async () => {
+          // WHEN
+          await fireEvent.update(inputCodePostal, '');
+          await fireEvent.update(selectCommune, '');
+          await fireEvent.click(submitBouton);
+
+          expect(mettreAJourCompteUtilisateurUsecaseMock).not.toHaveBeenCalled();
+        });
+
+        it.todo("affiche un message d'erreur", () => {
+          // getByRole('alert');
+        });
+      });
     });
   });
 });
