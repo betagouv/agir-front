893677cd0b5b6cf9145c3b49372ea5ad05e748ef
dlamande <contact@dorianlamande.com>
2023-11-20 17:01:57 +0100

    refacto interactin a ete cliquée

--- src/composables/interactionAEteCliquee.ts
+++ src/composables/interactionAEteCliquee.ts
@@ -2,19 +2,23 @@ import { utilisateurStore } from '@/store/utilisateur';
 import { CliquerInteractionUsecase } from '@/interactions/cliquerInteraction.usecase';
 import { InteractionsRepositoryAxios } from '@/interactions/adapters/interactionsRepository.axios';
 import { interactionEnCoursStore } from '@/store/interaction';
-import { RecommandationViewModel } from '@/recommandationsPersonnalisees/adapters/recommandationsPersonnalisees.presenter.impl';
 
-export function useInteraction(recommandation: RecommandationViewModel) {
+export function useInteraction(interaction: {
+  id: string;
+  type: string;
+  nombreDePointsAGagner: string;
+  idDuContenu: string;
+}) {
   function interactionAEteCliquee(): void {
     const store = utilisateurStore();
     const idUtilisateur = store.utilisateur.id;
     const useCase = new CliquerInteractionUsecase(new InteractionsRepositoryAxios());
-    useCase.execute(idUtilisateur, recommandation.id, recommandation.type).then(() => {});
+    useCase.execute(idUtilisateur, interaction.id, interaction.type).then(() => {});
     interactionEnCoursStore().setInteractionEnCours({
-      id: recommandation.id,
-      type: recommandation.type,
-      nombreDePointsAGagner: recommandation.nombreDePointsAGagner,
-      idDuContenu: recommandation.contentId,
+      id: interaction.id,
+      type: interaction.type,
+      nombreDePointsAGagner: interaction.nombreDePointsAGagner,
+      idDuContenu: interaction.idDuContenu,
     });
   }
 
