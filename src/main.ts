277c38d8fcb815ecd156edc05c32212691fbec9d
Denis B <denis.baudot@gmail.com>
2023-11-22 11:22:33 +0100

    add sentry

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import router from '@/router';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import VueMatomo from 'vue-matomo';
+import * as Sentry from '@sentry/vue';
 import '@gouvfr/dsfr/dist/dsfr.min.css';
 import '@gouvfr/dsfr/dist/utility/utility.min.css';
 import '@gouvfr/dsfr/dist/dsfr.module.min.js';
@@ -26,6 +27,31 @@ app.use(VueMatomo, {
   host: import.meta.env.VITE_MATOMO_URL,
   siteId: import.meta.env.VITE_MATOMO_SITE_ID,
 });
+
+Sentry.init({
+  app,
+  dsn: 'https://fbc3449644ae48659f66a87005cc31bd@sentry.incubateur.net/128',
+  integrations: [
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+    }),
+    new Sentry.Replay(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  //tracePropagationTargets: ['localhost', /^\//],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 app.mount('#app');
 
 window._paq.push(['trackPageView']);
