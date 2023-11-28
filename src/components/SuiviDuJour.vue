d5e2bc8fe1edf405bb3563dd35dc6e745302554c
octo-guic <guillaume.chauvet@octo.com>
2023-11-28 12:19:00 +0100

    [tech] : upate suiviDuJour setup

--- src/components/SuiviDuJour.vue
+++ src/components/SuiviDuJour.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="fr-grid-row fr-grid-row--gutters">
     <div class="fr-col-12 fr-col-lg-8">
-      <div class="follow-up-stepper-container">
-        <div class="follow-up-stepper-sub-container">
-          <h3 v-if="etapeCourante" class="step-title-container fr-mx-n1w">{{ getCurrentStepTitle }}</h3>
+      <div class="border border-radius--md background--white">
+        <div class="fr-p-4w">
+          <h3 v-if="etapeCourante" class="fr-mx-n1w">{{ currentStepTitle }}</h3>
           <form @submit.prevent="etapeSuivante">
             <div
               v-if="etapeCourante"
@@ -13,51 +13,46 @@
             />
             <br />
             <fieldset class="fr-fieldset" id="checkbox" aria-labelledby="checkbox-legend checkbox-messages">
-              <div class="selected-step-container fr-mx-n1w" v-if="etapeCourante == 1">
-                <SuiviDuJourPremiereEtape
-                  current-step-question="Comptez combien de repas vous avez consommé avec les aliments suivants :"
-                  :etape-courante="etapeCourante"
-                  @update:model-value="miseAjourReponseSuiviDuJourAlimentation"
-                  :model-value="suiviDuJourAlimentation"
-                  :dernier-suivi-du-jour-view-model="dernierSuiviDuJourAlimentationViewmodel"
-                />
+              <div class="fr-col-12" v-if="etapeCourante == 1">
+                <div class="fr-mb-2w">
+                  <SuiviDuJourPremiereEtape
+                    current-step-question="Comptez combien de repas vous avez consommé avec les aliments suivants :"
+                    :etape-courante="etapeCourante"
+                    @update:model-value="miseAjourReponseSuiviDuJourAlimentation"
+                    :model-value="suiviDuJourAlimentation"
+                    :dernier-suivi-du-jour-view-model="dernierSuiviDuJourAlimentationViewmodel"
+                  />
+                </div>
+                <button class="fr-btn" title="Suivant">Continuer</button>
               </div>
-              <div class="selected-step-container fr-mx-n1w" v-else-if="etapeCourante == 2">
-                <SuiviDuJourSecondeEtape
-                  current-step-question="Quels transports avez vous utilisé aujourd'hui ?"
-                  :etape-courante="etapeCourante"
-                  @update:model-value="miseAjourReponseSuiviDuJourTransport"
-                  :model-value="suiviDuJourTransport"
-                  :dernier-suivi-du-jour-view-model="dernierSuiviDuJourTransportViewmodel"
-                />
+              <div class="fr-col-12" v-else-if="etapeCourante == 2">
+                <div class="fr-mb-2w">
+                  <SuiviDuJourSecondeEtape
+                    current-step-question="Quels transports avez vous utilisé aujourd'hui ?"
+                    :etape-courante="etapeCourante"
+                    @update:model-value="miseAjourReponseSuiviDuJourTransport"
+                    :model-value="suiviDuJourTransport"
+                    :dernier-suivi-du-jour-view-model="dernierSuiviDuJourTransportViewmodel"
+                  />
+                </div>
+                <ul class="fr-btns-group fr-btns-group--inline fr-btns-group--icon-left">
+                  <li>
+                    <button
+                      @click="etapePrecedente"
+                      class="fr-btn fr-btn--tertiary-no-outline fr-btn--icon-left fr-icon-arrow-left-line fr-mb-0"
+                    >
+                      Précédent
+                    </button>
+                  </li>
+                  <li>
+                    <button class="fr-btn fr-mb-0" title="Suivant">Continuer</button>
+                  </li>
+                </ul>
               </div>
               <div class="last-step-container fr-container--fluid" v-else>
                 <SuiviDuJourResultats :suivi-du-jour-resultats="suiviDuJourResultatsViewModel" />
               </div>
             </fieldset>
-            <div style="text-align: left" class="fr-mx-n1w">
-              <span
-                v-if="etapeCourante <= 2 && etapeCourante > 1"
-                @click="etapePrecedente"
-                class="step-btn-actions margin-between-buttons"
-              >
-                <span class="fr-icon-arrow-left-line" aria-hidden="true"></span>
-                Précédent
-              </span>
-              <button v-if="etapeCourante < 3" class="fr-btn fr-btn-not-rounded margin-between-buttons" title="Suivant">
-                Continuer
-              </button>
-              <router-link
-                v-if="etapeCourante == 3"
-                class="fr-btn fr-btn-not-rounded redirect-coach-link"
-                id="button-2864"
-                title="Retourner à la page coach"
-                :to="{ name: 'coach' }"
-              >
-                Retour à mes actions
-              </router-link>
-            </div>
-            <br />
           </form>
         </div>
       </div>
@@ -73,8 +68,9 @@
   </div>
 </template>
 
-<script lang="ts">
-  import { defineComponent, onMounted, ref } from 'vue';
+<script setup lang="ts">
+  import { computed, onMounted, ref } from 'vue';
+  import BilanNosGestesClimat from '@/components/BilanNosGestesClimat.vue';
   import ImpactDuJour from '@/components/ImpactDuJour.vue';
   import SuiviDuJourResultats from '@/components/SuiviDuJourResultats.vue';
   import SuiviDuJourPremiereEtape from '@/components/SuiviDuJourPremiereEtape.vue';
@@ -91,174 +87,83 @@
   } from '@/suivi/adapters/dernierSuiviDuJour.presenter.impl';
   import { utilisateurStore } from '@/store/utilisateur';
 
-  export default defineComponent({
-    name: 'SuiviDuJour',
-    components: {
-      NombreDePointsDuJour,
-      SuiviDuJourSecondeEtape,
-      SuiviDuJourPremiereEtape,
-      SuiviDuJourResultats,
-      ImpactDuJour,
-    },
-    computed: {
-      getCurrentStepTitle() {
-        if (this.etapeCourante == 1) {
-          return '🥦 Alimentation';
-        } else if (this.etapeCourante == 2) {
-          return '🚗 Transport du quotidien';
-        }
-        return 'Résultat du suivi';
-      },
-    },
-    setup() {
-      let etapeCourante = ref<number>(1);
-      let suiviDuJourAlimentation = new Map<string, string>();
-      let suiviDuJourTransport = new Map<string, string>();
-      let dernierSuiviDuJourAlimentationViewmodel = ref<DernierSuiviDuJourViewModel>();
-      let dernierSuiviDuJourTransportViewmodel = ref<DernierSuiviDuJourViewModel>();
-      const suiviDuJourResultatsViewModel = ref<SuiviDuJourResultatsViewModel>({
-        impactCarbonDuJour: { valeur: '0', pictoSens: '', commentaire: '', variation: '0' },
-        suivisPrecedent: { valeursDesSuivis: [], datesDesSuivis: [], moyenneDesSuivis: [] },
-        additionCarbone: [],
-      });
-      const store = utilisateurStore();
-      onMounted(() => {
-        const idUtilisateur = store.utilisateur.id;
-
-        const chargerDernierSuiviAlimentation = new ObtenirDernierSuiviUsecase(new SuiviDuJourRepositoryAxios());
-
-        function mapSuiviAlimentation(dernierSuiviViewModel: DernierSuiviDuJourViewModel) {
-          dernierSuiviDuJourAlimentationViewmodel.value = dernierSuiviViewModel;
-        }
-        function mapSuiviTransport(dernierSuiviViewModel: DernierSuiviDuJourViewModel) {
-          dernierSuiviDuJourTransportViewmodel.value = dernierSuiviViewModel;
-        }
-
-        chargerDernierSuiviAlimentation.execute(
-          idUtilisateur,
-          'alimentation',
-          new DernierSuiviDuJourPresenterImpl(mapSuiviAlimentation, new DateTimeTypeScript())
-        );
-        chargerDernierSuiviAlimentation.execute(
-          idUtilisateur,
-          'transport',
-          new DernierSuiviDuJourPresenterImpl(mapSuiviTransport, new DateTimeTypeScript())
-        );
-      });
-
-      function etapeSuivante() {
-        if (etapeCourante.value == 2) {
-          calculEmpreinteDuJour();
-        }
-        etapeCourante.value = etapeCourante.value + 1;
-      }
-      function etapePrecedente() {
-        etapeCourante.value = etapeCourante.value - 1;
-      }
-      function sauterEtape() {
-        etapeCourante.value = etapeCourante.value + 1;
-      }
-      const calculEmpreinteDuJour = () => {
-        const idUtilisateur = store.utilisateur.id;
-        const envoyerSuiviDuJour = new EnvoyerSuiviDuJourUsecase(new SuiviDuJourRepositoryAxios());
-        envoyerSuiviDuJour.execute(
-          { valeurs: suiviDuJourAlimentation },
-          { valeurs: suiviDuJourTransport },
-          new SuiviDuJourPresenterImpl(mapImpactCarboneDuJour),
-          idUtilisateur
-        );
-      };
-
-      function miseAjourReponseSuiviDuJourAlimentation(map: Map<string, string>) {
-        suiviDuJourAlimentation = map;
-      }
-      function miseAjourReponseSuiviDuJourTransport(map: Map<string, string>) {
-        suiviDuJourTransport = map;
-      }
-
-      function mapImpactCarboneDuJour(impactDuJourViewModel: SuiviDuJourResultatsViewModel) {
-        suiviDuJourResultatsViewModel.value = impactDuJourViewModel;
-      }
-
-      return {
-        etapeCourante,
-        sauterEtape,
-        etapeSuivante,
-        etapePrecedente,
-        calculEmpreinteDuJour,
-        miseAjourReponseSuiviDuJourAlimentation,
-        miseAjourReponseSuiviDuJourTransport,
-        suiviDuJourAlimentation,
-        suiviDuJourTransport,
-        dernierSuiviDuJourAlimentationViewmodel,
-        dernierSuiviDuJourTransportViewmodel,
-        suiviDuJourResultatsViewModel,
-        store,
-      };
-    },
+  const etapeCourante = ref<number>(1);
+  let suiviDuJourAlimentation = new Map<string, string>();
+  let suiviDuJourTransport = new Map<string, string>();
+  const dernierSuiviDuJourAlimentationViewmodel = ref<DernierSuiviDuJourViewModel>();
+  const dernierSuiviDuJourTransportViewmodel = ref<DernierSuiviDuJourViewModel>();
+  const suiviDuJourResultatsViewModel = ref<SuiviDuJourResultatsViewModel>({
+    impactCarbonDuJour: { valeur: '0', pictoSens: '', commentaire: '', variation: '0' },
+    suivisPrecedent: { valeursDesSuivis: [], datesDesSuivis: [], moyenneDesSuivis: [] },
+    additionCarbone: [],
   });
+
+  const store = utilisateurStore();
+
+  const currentStepTitle = computed(() => {
+    if (etapeCourante.value === 1) {
+      return '🥦 Alimentation';
+    } else if (etapeCourante.value == 2) {
+      return '🚗 Transport du quotidien';
+    }
+    return 'Résultat du suivi';
+  });
+
+  onMounted(() => {
+    const idUtilisateur = store.utilisateur.id;
+
+    const chargerDernierSuiviAlimentation = new ObtenirDernierSuiviUsecase(new SuiviDuJourRepositoryAxios());
+
+    function mapSuiviAlimentation(dernierSuiviViewModel: DernierSuiviDuJourViewModel) {
+      dernierSuiviDuJourAlimentationViewmodel.value = dernierSuiviViewModel;
+    }
+    function mapSuiviTransport(dernierSuiviViewModel: DernierSuiviDuJourViewModel) {
+      dernierSuiviDuJourTransportViewmodel.value = dernierSuiviViewModel;
+    }
+
+    chargerDernierSuiviAlimentation.execute(
+      idUtilisateur,
+      'alimentation',
+      new DernierSuiviDuJourPresenterImpl(mapSuiviAlimentation, new DateTimeTypeScript())
+    );
+    chargerDernierSuiviAlimentation.execute(
+      idUtilisateur,
+      'transport',
+      new DernierSuiviDuJourPresenterImpl(mapSuiviTransport, new DateTimeTypeScript())
+    );
+  });
+
+  function etapeSuivante() {
+    if (etapeCourante.value == 2) {
+      calculEmpreinteDuJour();
+    }
+    etapeCourante.value = etapeCourante.value + 1;
+  }
+
+  function etapePrecedente() {
+    etapeCourante.value = etapeCourante.value - 1;
+  }
+
+  const calculEmpreinteDuJour = () => {
+    const idUtilisateur = store.utilisateur.id;
+    const envoyerSuiviDuJour = new EnvoyerSuiviDuJourUsecase(new SuiviDuJourRepositoryAxios());
+    envoyerSuiviDuJour.execute(
+      { valeurs: suiviDuJourAlimentation },
+      { valeurs: suiviDuJourTransport },
+      new SuiviDuJourPresenterImpl(mapImpactCarboneDuJour),
+      idUtilisateur
+    );
+  };
+
+  function miseAjourReponseSuiviDuJourAlimentation(map: Map<string, string>) {
+    suiviDuJourAlimentation = map;
+  }
+
+  function miseAjourReponseSuiviDuJourTransport(map: Map<string, string>) {
+    suiviDuJourTransport = map;
+  }
+
+  function mapImpactCarboneDuJour(impactDuJourViewModel: SuiviDuJourResultatsViewModel) {
+    suiviDuJourResultatsViewModel.value = impactDuJourViewModel;
+  }
 </script>
-
-<style scoped>
-  .follow-up-stepper-container {
-    border: 1px solid rgba(0, 0, 0, 0.19);
-    border-radius: 6px;
-    background-color: white;
-  }
-
-  .follow-up-stepper-sub-container {
-    margin: 2em 2em 0 2em;
-  }
-
-  .step-btn-actions {
-    color: #000091;
-    font-weight: bold;
-  }
-
-  .step-btn-actions:hover {
-    cursor: pointer;
-  }
-
-  .fill-response-checkbox-container .fr-checkbox-group input[type='checkbox'] + label:before {
-    margin: 10px;
-    width: 15px;
-    height: 15px;
-  }
-
-  .redirect-coach-link:hover {
-    color: white;
-  }
-
-  .step-title-container {
-    text-align: left;
-    font-size: 25px;
-  }
-
-  .last-step-container {
-    width: 100%;
-  }
-
-  .fr-btn-not-rounded {
-    border-radius: 0;
-  }
-
-  .share-btn-container {
-    margin: 10px auto;
-    background-color: white;
-    color: #000091;
-    border: 1px solid rgba(0, 0, 0, 0.19);
-  }
-
-  .fr-btn-not-rounded {
-    border-radius: 0;
-  }
-
-  .selected-step-container {
-    width: 100%;
-    text-align: left;
-  }
-
-  .margin-between-buttons {
-    margin: 0 1em 0 0;
-  }
-</style>
--- src/components/dsfr/Transcription.vue
+++ src/components/dsfr/Transcription.vue
@@ -2,42 +2,28 @@
   <div class="fr-transcription background--white">
     <button class="fr-transcription__btn" aria-expanded="false" :aria-controls="id">{{ titre }}</button>
     <div class="fr-collapse" :id="id">
-      <dialog :id="`${id}-modal`" class="fr-modal" role="dialog" :aria-labelledby="`${id}-transcription-title`">
-        <div class="fr-container fr-container--fluid fr-container-md">
-          <div class="fr-grid-row fr-grid-row--center">
-            <div class="fr-col-12 fr-col-md-10 fr-col-lg-8">
-              <div class="fr-modal__body">
-                <div class="fr-modal__header">
-                  <button class="fr-btn--close fr-btn" :aria-controls="`${id}-modal`" title="Fermer">Fermer</button>
-                </div>
-                <div class="fr-modal__content">
-                  <h1 :id="`${id}-transcription-title`" class="fr-modal__title">
-                    {{ titre }}
-                  </h1>
-                  <slot />
-                </div>
-                <div class="fr-transcription__footer">
-                  <div class="fr-transcription__actions-group">
-                    <button
-                      class="fr-btn fr-btn--fullscreen"
-                      :aria-controls="`${id}-modal`"
-                      data-fr-opened="false"
-                      title=""
-                    >
-                      Agrandir
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+      <Modale :titre="titre" :label="`${id}-transcription-title`" :id="`${id}-modal`">
+        <slot />
+      </Modale>
+      <div class="fr-transcription__footer">
+        <div class="fr-transcription__actions-group">
+          <button
+            class="fr-btn fr-btn--fullscreen"
+            :aria-controls="`${id}-modal`"
+            data-fr-opened="false"
+            :title="titre"
+          >
+            Agrandir
+          </button>
         </div>
-      </dialog>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  import Modale from '@/components/custom/Modale/Modale.vue';
+
   defineProps<{
     id: string;
     titre: string;
