146023e915a179cac81c9a1ba5734581917e48fb
octo-guic <guillaume.chauvet@octo.com>
2023-12-05 11:34:31 +0100

    tech: update class

--- src/components/custom/BoutonRadio.vue
+++ src/components/custom/BoutonRadio.vue
@@ -5,7 +5,8 @@
     aria-labelledby="radio-legend"
   >
     <legend
-      :class="`fr-fieldset__legend--regular fr-fieldset__legend fr-pb-0 ${legendeSize === 'l' ? 'fr-h4' : 'fr-mb-1w'}`"
+      class="fr-fieldset__legend--regular fr-fieldset__legend fr-pb-0"
+      :class="legendeSize === 'l' ? 'fr-h4' : 'fr-mb-1w'"
       id="radio-legend"
     >
       {{ legende }}
