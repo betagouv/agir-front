9f060a54e8b80607b94905caf17af17d16f33500
octo-guic <guillaume.chauvet@octo.com>
2023-12-06 17:23:54 +0100

    fix: test chargementScoreUsecase

--- tests/score/chargementScore.usecase.spec.ts
+++ tests/score/chargementScore.usecase.spec.ts
@@ -16,6 +16,24 @@ describe('Fichier de test du usecase de chargement du score', () => {
         niveau: 1,
         nombreDePointsDansLeNiveau: 25,
         nombreDePointsDuNiveau: 100,
+        celebration: null,
+      });
+    }
+  });
+
+  it("en donnant un utilisateur valide doit me retourner de le score et les célébrations s'il y en a", async () => {
+    // GIVEN
+    const chargementScoreUsecase = new ChargementScoreUsecase(new MockScoreRepository());
+    // WHEN
+    await chargementScoreUsecase.execute('userAvecCelebration', new ChargementScorePresenterImpl(expectation));
+    // THEN
+    function expectation(viewModel: ScoreViewModel) {
+      expect(viewModel).toStrictEqual<ScoreViewModel>({
+        points: 10,
+        niveau: 1,
+        nombreDePointsDansLeNiveau: 25,
+        nombreDePointsDuNiveau: 100,
+        celebration: { id: 'celebrationID', type: 'niveau', titre: 'celebrationTitre', new_niveau: 4 },
       });
     }
   });
--- tests/score/mockScoreRepository.ts
+++ tests/score/mockScoreRepository.ts
@@ -2,11 +2,17 @@ import { Score, ScoreRepository } from '@/score/ports/score.repository';
 
 export class MockScoreRepository implements ScoreRepository {
   async getScore(utilisateur: string): Promise<Score> {
+    const celebrationMock: Score['celebration'] =
+      utilisateur === 'userAvecCelebration'
+        ? { id: 'celebrationID', type: 'niveau', titre: 'celebrationTitre', new_niveau: 4 }
+        : null;
+
     return {
       niveau: 1,
       nombreDePointsDuNiveau: 100,
       nombreDePointsDansLeNiveau: 25,
       points: 10,
+      celebration: celebrationMock,
     };
   }
 }
