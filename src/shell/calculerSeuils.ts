345cb0e4c5cee5b2673b817ea3077448233740f6
OCTO-GUIC <121949498+OCTO-GUIC@users.noreply.github.com>
2023-12-12 13:53:20 +0100

    fix: formate les revenus fiscaux de référence (#196)

--- src/shell/calculerSeuils.ts
+++ src/shell/calculerSeuils.ts
@@ -4,15 +4,15 @@ export function calculerSeuils(nombreDeParts: number) {
 
   return [
     {
-      label: `Moins de ${revenuMin} €`,
+      label: `Moins de ${revenuMin.toLocaleString('fr')} €`,
       value: '0',
     },
     {
-      label: `De ${revenuMin} à ${revenuMax} €`,
+      label: `De ${revenuMin.toLocaleString('fr')} à ${revenuMax.toLocaleString('fr')} €`,
       value: `${revenuMin}`,
     },
     {
-      label: `Plus de ${revenuMax} €`,
+      label: `Plus de ${revenuMax.toLocaleString('fr')} €`,
       value: `${revenuMax}`,
     },
   ];
