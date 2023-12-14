b00197c7563a9dc5b1533dd52ede53825a4880dd
dlamande <contact@dorianlamande.com>
2023-12-14 14:59:17 +0100

    refacto: la validation de compte ne renvoie plus qu'un token

--- src/authentification/ports/utilisateur.repository.ts
+++ src/authentification/ports/utilisateur.repository.ts
@@ -11,10 +11,12 @@ export interface Utilisateur {
   fonctionnalitesDebloquees: string[];
 }
 
+export type IdUtilisateur = string;
+
 export interface UtilisateurRepository {
   authentifierUtilisateur(email: string, motDePasse: string): Promise<Utilisateur>;
   getUtilisateurAvecId(idUtilisateur: string): Promise<Utilisateur>;
-  validerCompteUtilisateur(email: string, code: string): Promise<Utilisateur>;
+  validerCompteUtilisateur(email: string, code: string): Promise<IdUtilisateur>;
   renvoyerCodeOTP(email: string): Promise<void>;
   commencerRedefinirMotDePasse(email: string): void;
   terminerRedefinirMotDePasse(email: string, motDePasse: string, code: string): Promise<void>;
