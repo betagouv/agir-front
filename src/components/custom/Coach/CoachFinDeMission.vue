b6188887bb7eaeafd88e4ba4b34240ebef62ecee
octo-guic <guillaume.chauvet@octo.com>
2023-12-18 11:16:50 +0100

    feat: modification ui de l'écran fin de mission

---
+++ src/components/custom/Coach/CoachFinDeMission.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card-fin-mission fr-grid-row fr-grid-row--middle flex-column fr-py-6w border-radius--lg">
+    <div class="card-fin-mission--picto text--bleu fr-mb-3w">
+      <span class="fr-icon-flag-fill" aria-hidden="true"></span>
+      <span class="fr-icon-flag-fill" aria-hidden="true"></span>
+      <span class="fr-icon-flag-fill" aria-hidden="true"></span>
+    </div>
+    <span class="text--bleu text--3xl">Votre 1ère mission</span>
+    <span class="fr-display--xs fr-mt-1w text--uppercase fr-mb-2w">Accomplie !</span>
+    <span class="fr-text--lead fr-text--bold">
+      Votre bonus : +25 <img src="/ic_score.svg" alt="points" width="24" />
+    </span>
+    <button
+      class="fr-btn fr-btn--lg fr-btn--primary fr-btn--icon-right fr-icon-arrow-right-line"
+      @click="recupererPointsTodo"
+    >
+      Continuer
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { defineEmits } from 'vue';
+
+  const emit = defineEmits(['recuperer-points-todo']);
+
+  const recupererPointsTodo = () => {
+    emit('recuperer-points-todo');
+  };
+</script>
+
+<style scoped>
+  .card-fin-mission {
+    background-image: url('/background-fin-de-mission.jpg');
+    background-size: cover;
+    background-position: center;
+  }
+
+  .card-fin-mission--picto {
+    display: flex;
+    align-items: end;
+  }
+
+  .card-fin-mission--picto span:nth-child(2)::before {
+    --icon-size: 3rem;
+  }
+</style>
