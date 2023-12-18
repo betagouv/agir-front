d2bc36b12733593597c7c4916f78d744a612544e
OCTO-GUIC <121949498+OCTO-GUIC@users.noreply.github.com>
2023-12-18 17:13:03 +0100

    feat: ajouter un écran quand toutes les missions sont terminées (#216)

--- src/components/custom/Coach/CoachToDo.vue
+++ src/components/custom/Coach/CoachToDo.vue
@@ -1,60 +1,63 @@
 <template>
-  <div v-if="!bonusFinalRecupere">
-    <h2 class="fr-mb-0">{{ todoList.titre }}</h2>
-    <p class="fr-text--xl">Un pas après l’autre</p>
-    <div v-if="todoList.fait.length > 0">
-      <h3 class="text--uppercase fr-mb-0 fr-text--xs text-disabled-grey fr-mb-1w">déjà fait</h3>
-      <ul class="list-style-none fr-p-0 fr-m-0">
-        <li v-for="todo in todoList.fait" :key="todo.titre" class="fr-mb-2w">
-          <CoachCardDone
-            :titre="todo.titre"
-            :value="todo.progession.etapeCourante"
-            :nombre-points="todo.nombreDePointsAGagner"
-            :point-a-ete-recolte="todo.pointAEteRecolte"
-            :element-id="todo.id"
-          />
-        </li>
-      </ul>
-    </div>
-    <div v-if="todoList.aFaire.length > 0">
-      <h3 class="text--uppercase fr-mb-0 fr-text--xs text-disabled-grey fr-mb-1w">à faire</h3>
-      <ul class="list-style-none fr-p-0 fr-m-0">
-        <li v-for="todo in todoList.aFaire" :key="todo.titre" class="fr-mb-2w">
-          <CoachCardToDo
-            :interactionId="todo.interactionId"
-            :id-du-contenu="todo.contentId"
-            :type="todo.type"
-            :nombre-de-points-a-gagner="todo.nombreDePointsAGagner"
-            :titre="todo.titre"
-            :value="todo.progession.etapeCourante"
-            :value-max="todo.progession.etapeTotal"
-            :url="todo.url"
-          />
-        </li>
-      </ul>
-    </div>
-    <div class="fr-py-1w">
-      <h3 class="text--uppercase fr-mb-0 fr-text--xs text-disabled-grey fr-mb-1w before--path">
-        <span class="fr-icon-gift-fill" aria-hidden="true"></span>
-        Bonus de fin de mission
-      </h3>
-      <div v-if="!isDisableBonusFinDeToDo()">
-        <button
-          class="fr-btn fr-btn--secondary fr-btn--icon-left fr-icon-gift-fill fr-text--lg background--white"
-          @click="showBonus"
-        >
-          Découvrir le bonus
-        </button>
-        <div id="container-survey"></div>
+  <CoachFinDesMissions v-if="todoList.derniere" />
+  <div v-else>
+    <div v-if="!bonusFinalRecupere">
+      <h2 class="fr-mb-0">{{ todoList.titre }}</h2>
+      <p class="fr-text--xl">Un pas après l’autre</p>
+      <div v-if="todoList.fait.length > 0">
+        <h3 class="text--uppercase fr-mb-0 fr-text--xs text-disabled-grey fr-mb-1w">déjà fait</h3>
+        <ul class="list-style-none fr-p-0 fr-m-0">
+          <li v-for="todo in todoList.fait" :key="todo.titre" class="fr-mb-2w">
+            <CoachCardDone
+              :titre="todo.titre"
+              :value="todo.progession.etapeCourante"
+              :nombre-points="todo.nombreDePointsAGagner"
+              :point-a-ete-recolte="todo.pointAEteRecolte"
+              :element-id="todo.id"
+            />
+          </li>
+        </ul>
+      </div>
+      <div v-if="todoList.aFaire.length > 0">
+        <h3 class="text--uppercase fr-mb-0 fr-text--xs text-disabled-grey fr-mb-1w">à faire</h3>
+        <ul class="list-style-none fr-p-0 fr-m-0">
+          <li v-for="todo in todoList.aFaire" :key="todo.titre" class="fr-mb-2w">
+            <CoachCardToDo
+              :interactionId="todo.interactionId"
+              :id-du-contenu="todo.contentId"
+              :type="todo.type"
+              :nombre-de-points-a-gagner="todo.nombreDePointsAGagner"
+              :titre="todo.titre"
+              :value="todo.progession.etapeCourante"
+              :value-max="todo.progession.etapeTotal"
+              :url="todo.url"
+            />
+          </li>
+        </ul>
+      </div>
+      <div class="fr-py-1w">
+        <h3 class="text--uppercase fr-mb-0 fr-text--xs text-disabled-grey fr-mb-1w before--path">
+          <span class="fr-icon-gift-fill" aria-hidden="true"></span>
+          Bonus de fin de mission
+        </h3>
+        <div v-if="!isDisableBonusFinDeToDo()">
+          <button
+            class="fr-btn fr-btn--secondary fr-btn--icon-left fr-icon-gift-fill fr-text--lg background--white"
+            @click="showBonus"
+          >
+            Découvrir le bonus
+          </button>
+          <div id="container-survey"></div>
+        </div>
       </div>
     </div>
+    <CoachFinDeMission
+      v-else
+      @recuperer-points-todo="recupererPointsTodo"
+      :nombre-de-points-a-gagner="todoList.pointFinDeMission"
+      :titre-de-mission="todoList.titre"
+    />
   </div>
-  <CoachFinDeMission
-    v-else
-    @recuperer-points-todo="recupererPointsTodo"
-    :nombre-de-points-a-gagner="todoList.pointFinDeMission"
-    :titre-de-mission="todoList.titre"
-  />
 </template>
 
 <script setup lang="ts">
@@ -62,26 +65,27 @@
   import CoachCardDone from '@/components/custom/Coach/CoachCardDone.vue';
   import CoachCardToDo from '@/components/custom/Coach/CoachCardToDo.vue';
   import CoachFinDeMission from '@/components/custom/Coach/CoachFinDeMission.vue';
+  import CoachFinDesMissions from '@/components/custom/Coach/CoachFinDesMissions.vue';
   import { TodoListViewModel } from '@/toDoList/adapters/toDoList.presenter.impl';
   import { TerminerToDoListUsecase } from '@/toDoList/terminerToDoList.usecase';
   import { utilisateurStore } from '@/store/utilisateur';
   import { ToDoListRepositoryAxios } from '@/toDoList/adapters/toDoList.repository.axios';
   import { ToDoListEventBusImpl } from '@/toDoList/toDoListEventBusImpl';
-  import {publierEvenementHotjar, HotjarEvenement} from '@/shell/publierEvenementHotjar';
+  import { publierEvenementHotjar, HotjarEvenement } from '@/shell/publierEvenementHotjar';
   import { inject } from 'vue';
-  
+
   const hotjar = inject('Hotjar') as {
     event: (eventName: string) => void;
   };
-  
+
   const props = defineProps<{ todoList: TodoListViewModel }>();
 
   const bonusFinalRecupere = ref(false);
 
   const showBonus = () => {
     bonusFinalRecupere.value = true;
-    if(hotjar && props.todoList && props.todoList.derniere){
-      publierEvenementHotjar(hotjar, HotjarEvenement.DEBRIEF)
+    if (hotjar && props.todoList && props.todoList.derniere) {
+      publierEvenementHotjar(hotjar, HotjarEvenement.DEBRIEF);
     }
   };
 
