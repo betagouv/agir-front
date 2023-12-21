74f3b18ef55e7f7ed758b83ea54c0b3c7503ce1a
dlamande <contact@dorianlamande.com>
2023-12-21 14:22:18 +0100

    fix test unitaire

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default mergeConfig(viteConfig, {
     globals: true,
     environment: 'jsdom',
     include: ['tests/**/*.spec.ts'],
+    exclude: ['tests/e2e/*.spec.ts'],
     sequence: { shuffle: true },
   },
   resolve: {
