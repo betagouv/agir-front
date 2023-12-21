99109ec2222e240d91073bfc4f530ac7ca4498ef
dlamande <contact@dorianlamande.com>
2023-12-21 14:30:57 +0100

    try to fix CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,8 +70,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn dev',
+    command: 'vite --port 5173',
     url: 'http://localhost:5173',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: false,
   },
 });
