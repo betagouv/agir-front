90f6f9f3194f5a4648a820382a148789e55d58f5
octo-guic <guillaume.chauvet@octo.com>
2024-01-10 15:11:56 +0100

    tech: creation function connecterUtilisateur pour utilisation dans les beforeeach des tests e2e

--- tests/e2e/kyc.spec.ts
+++ tests/e2e/kyc.spec.ts
@@ -1,70 +1,10 @@
-import { test, expect, chromium } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
+import { connecterUtilisateur } from './utils/connecterUtilisateurs';
 
-test('reponse a un kyc doit afficher "Merci pour votre réponse"', async () => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
+let page: Page;
 
-  await page.route('https://agir-back-dev.osc-fr1.scalingo.io/utilisateurs/login', route => {
-    route.fulfill({
-      status: 200,
-      contentType: 'application/json',
-      body: JSON.stringify({
-        utilisateur: {
-          id: 'wojtek',
-          nom: 'WOJCIK',
-          prenom: 'Wojtek',
-          email: 'ww@w.com',
-          code_postal: '91120',
-          commune: 'PALAISEAU',
-          revenu_fiscal: null,
-          nombre_de_parts_fiscales: 2.5,
-          quizzProfile: {
-            climat: {
-              level: 1,
-              isCompleted: false,
-            },
-            dechet: {
-              level: 1,
-              isCompleted: false,
-            },
-            loisir: {
-              level: 1,
-              isCompleted: false,
-            },
-            logement: {
-              level: 2,
-              isCompleted: false,
-            },
-            transport: {
-              level: 1,
-              isCompleted: false,
-            },
-            alimentation: {
-              level: 1,
-              isCompleted: false,
-            },
-            consommation: {
-              level: 1,
-              isCompleted: false,
-            },
-          },
-          created_at: '2023-12-20T16:28:07.766Z',
-          fonctionnalites_debloquees: ['aides', 'services', 'recommandations'],
-        },
-        token:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dGlsaXNhdGV1cklkIjoid29qdGVrIiwiaWF0IjoxNzAzMTU1MzM2LCJleHAiOjE3MDU3NDczMzZ9.bJOxx98NUoYDq3e0mXKgv-YjAq-ZfSRc4S05bgwvKdA',
-      }),
-    });
-  });
-
-  await page.goto('/authentification');
-
-  await page.fill('#email', 'exemple@domaine.com');
-  await page.fill('#password-input', '');
-
-  await page.getByRole('button', { name: 'Se connecter' }).click();
-  await page.waitForURL('/coach/');
+test.beforeAll(async () => {
+  page = await connecterUtilisateur();
 
   await page.route('https://agir-back-dev.osc-fr1.scalingo.io/kyc/1', route => {
     route.fulfill({
@@ -80,8 +20,16 @@ test('reponse a un kyc doit afficher "Merci pour votre réponse"', async () => {
       }),
     });
   });
-  await page.goto('/kyc/1');
 
+  await page.goto('/kyc/1');
+});
+
+test('doit afficher le bon title et titre', async () => {
+  await expect(page).toHaveTitle('Agir !');
+  await expect(page.getByRole('heading', { level: 1 })).toHaveText('Question pour mieux vous connaître');
+});
+
+test('reponse a un kyc doit afficher "Merci pour votre réponse"', async () => {
   await page.fill('#reponse', 'Ma réponse, lorem ipsum dolor');
   await page.getByRole('button', { name: 'Valider' }).click();
 
