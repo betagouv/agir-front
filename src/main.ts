7e809337731ef9c99ffc78f6fc16b6ba4c36dd50
Denis B <denis.baudot@gmail.com>
2024-01-11 09:41:55 +0100

    tech: try to add app version in vite build

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,8 @@ declare global {
     };
   }
 }
-
+// eslint-disable-next-line no-console
+console.log('coucou', __APP_VERSION__);
 const app = createApp(App);
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import * as path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export const viteConfig = {
   plugins: [vue()],
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },
 };
 export default defineConfig(viteConfig);
