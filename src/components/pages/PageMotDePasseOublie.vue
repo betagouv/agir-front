7edac63a74346c39db14f3146df93e16741b0884
octo-guic <guillaume.chauvet@octo.com>
2024-01-15 14:02:34 +0100

    feat: ajout du bouton retour

--- src/components/pages/PageMotDePasseOublie.vue
+++ src/components/pages/PageMotDePasseOublie.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="fr-container fr-py-6w">
     <div class="fr-col-12 fr-col-lg-5 fr-mx-auto fr-mb-0 background--white fr-p-4w border border-radius--md">
+      <button
+        v-if="etape === Etapes.ETAPE_REDEFINIR"
+        class="fr-btn fr-btn--tertiary-no-outline fr-btn--icon-left fr-icon-arrow-left-line fr-pl-0"
+        @click="retourEtapeFormulaire"
+      >
+        Retour
+      </button>
       <h1 class="fr-h4">Mot de passe oublié - {{ etape }}/2</h1>
       <MotDePasseOublieFormulaire v-if="etape === Etapes.ETAPE_FORMULAIRE" @update:email="submitFormulaire" />
       <MotDePasseOublieRedefinir v-if="etape === Etapes.ETAPE_REDEFINIR" :email="email" />
@@ -25,4 +32,6 @@
     etape.value = Etapes.ETAPE_REDEFINIR;
     email.value = emailValue;
   };
+
+  const retourEtapeFormulaire = () => (etape.value = Etapes.ETAPE_FORMULAIRE);
 </script>
