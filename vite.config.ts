8e979503c6914fb4eb9096e833dec81dd2f8c566
dlamande <contact@dorianlamande.com>
2024-01-16 16:04:57 +0100

    tech: optimisation du chargement de l'application

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,21 @@ export const viteConfig = {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#outputmanualchunks
+      output: {
+        manualChunks: {
+          aides: ['./src/components/pages/PageAides.vue'],
+          services: ['./src/components/pages/PageCatalogueServices.vue'],
+          quiz: ['./src/components/pages/PageQuiz.vue'],
+          compte: ['./src/components/pages/PageCompte.vue'],
+          onboarding: ['./src/components/pages/PagePreOnboarding.vue', './src/components/pages/PageOnboarding.vue'],
+          accueil: ['./src/components/pages/PageAccueil.vue'],
+        },
+      },
+    },
+  },
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },
