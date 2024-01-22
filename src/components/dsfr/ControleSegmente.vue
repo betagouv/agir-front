f248b5536821698f32a4ab0dbd6b50d1f9e2b4eb
octo-guic <guillaume.chauvet@octo.com>
2024-01-22 16:21:28 +0100

    tech: creation du composant dsfr ControleSegmente

---
+++ src/components/dsfr/ControleSegmente.vue
@@ -0,0 +1,29 @@
+<template>
+  <fieldset class="fr-segmented fr-segmented--no-legend">
+    <legend class="fr-segmented__legend">{{ legende }}</legend>
+    <div class="fr-segmented__elements">
+      <div v-for="(segment, index) in segments" :key="index" class="fr-segmented__element">
+        <input
+          :value="segment.value"
+          type="radio"
+          :id="`${segment.value}-id`"
+          :name="name"
+          :checked="segment.checked"
+        />
+        <label class="fr-label" :for="`${segment.value}-id`">{{ segment.libelle }}</label>
+      </div>
+    </div>
+  </fieldset>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    legende: string;
+    name: string;
+    segments: {
+      libelle: string;
+      value: string;
+      checked?: boolean;
+    }[];
+  }>();
+</script>
