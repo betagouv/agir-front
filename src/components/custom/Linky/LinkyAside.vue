f4face7c218ae9e902c6d422692168dd1402e720
octo-guic <guillaume.chauvet@octo.com>
2024-01-22 16:21:58 +0100

    tech: création du composant LinkyAside

---
+++ src/components/custom/Linky/LinkyAside.vue
@@ -0,0 +1,15 @@
+<template>
+  <CarteVierge class="fr-mb-2w">
+    <h2 class="fr-h4">Paramétrage</h2>
+    <p class="fr-mb-0">Pour plus d'informations ou paramètrer son compteur: lorem lien</p>
+  </CarteVierge>
+  <CarteExplication>
+    <LinkyExplicationAleatoire class="fr-mt-2w" />
+  </CarteExplication>
+</template>
+
+<script setup lang="ts">
+  import CarteVierge from '@/components/CarteVierge.vue';
+  import CarteExplication from '@/components/custom/CarteExplication.vue';
+  import LinkyExplicationAleatoire from '@/components/custom/Linky/LinkyExplicationAleatoire.vue';
+</script>
