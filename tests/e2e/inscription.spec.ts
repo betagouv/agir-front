5e2a0d640518b5f3542fba7926857b766d172f00
Denis B <denis.baudot@gmail.com>
2024-01-29 19:07:34 +0100

    remove headless false

--- tests/e2e/inscription.spec.ts
+++ tests/e2e/inscription.spec.ts
@@ -4,7 +4,7 @@ import { chromium, BrowserContext } from '@playwright/test';
 
 let page: Page;
 const browser = await chromium.launch({
-  headless: false,
+  headless: true,
 });
 const context = await browser.newContext();
 
