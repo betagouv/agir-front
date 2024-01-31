f4ba0d3b17585c3d4e9876fe11cbd8a422926163
dlamande <contact@dorianlamande.com>
2024-01-31 11:15:34 +0100

    tech: suppression de la route interactions remplacer par recommandations (#276)

--- src/article/passerUnArticleCommeLu.usecase.ts
+++ src/article/passerUnArticleCommeLu.usecase.ts
@@ -8,8 +8,8 @@ export class PasserUnArticleCommeLuUsecase {
     private readonly todoListEventBus: EventBus<ToDoListEvent>
   ) {}
 
-  async execute(interactionId: string, utilisateurId: string): Promise<void> {
-    await this.articleRepository.marquerCommeLu(interactionId, utilisateurId);
+  async execute(articleId: string, utilisateurId: string): Promise<void> {
+    await this.articleRepository.marquerCommeLu(articleId, utilisateurId);
     this.todoListEventBus.publish(ToDoListEvent.TODO_ARTICLE_A_ETE_LU);
   }
 }
