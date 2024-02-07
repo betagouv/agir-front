c995a6da7c6363502dd0d32e3a329232ac4ef495
Denis B <denis.baudot@gmail.com>
2024-02-07 10:05:16 +0100

    change config

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
+    /*{
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
@@ -46,7 +46,7 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },*/
 
     /* Test against mobile viewports. */
     // {
