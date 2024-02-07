3b78bdf51f7437f7e02dd20e4b224a8a0b92b7f8
Denis B <denis.baudot@gmail.com>
2024-02-07 15:54:07 +0100

    re-add chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,10 +33,10 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    /*{
+    {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },*/
+    },
 
     {
       name: 'firefox',
