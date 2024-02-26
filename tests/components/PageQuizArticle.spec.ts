eed78955158fdc8bdedc6ec3351590af00361e19
octo-guic <guillaume.chauvet@octo.com>
2024-02-26 15:58:00 +0100

    tech: refacto tests

--- tests/components/PageQuizArticle.spec.ts
+++ tests/components/PageQuizArticle.spec.ts
@@ -35,77 +35,88 @@ const pageQuizComposantprops: {
   articleAssocie: null,
 };
 
-describe('Page Quiz Article', () => {
-  it("affiche le titre, une question, ses réponses possibles et un bouton 'Valider' disable", () => {
-    const { getByRole, getAllByRole } = render(PageQuizComposant, { props: pageQuizComposantprops });
-    const boutonValider = getByRole<HTMLButtonElement>('button', { name: 'Valider' });
+let page;
+let boutonValider: HTMLButtonElement;
+let bonneReponse: HTMLInputElement;
+let mauvaiseReponse: HTMLInputElement;
 
-    expect(getByRole('heading', { name: 'Une question sur la thématique Climat', level: 1 })).toBeDefined();
-    expect(getByRole('group', { name: "Quel est le principal avantage de l'agriculture biologique ?" })).toBeDefined();
-    expect(getByRole('radio', { name: '1' })).toBeDefined();
-    expect(getByRole('radio', { name: '2' })).toBeDefined();
-    expect(getByRole('radio', { name: '3' })).toBeDefined();
-    expect(getByRole('radio', { name: '4' })).toBeDefined();
-    expect(getAllByRole('radio')).toHaveLength(4);
+describe('Page Quiz Article', () => {
+  beforeEach(() => {
+    page = render(PageQuizComposant, { props: pageQuizComposantprops });
+    boutonValider = page.getByRole('button', { name: 'Valider' });
+    bonneReponse = page.getByRole('radio', { name: '1' });
+    mauvaiseReponse = page.getByRole('radio', { name: '2' });
+  });
+
+  it("affiche le titre, une question, ses réponses possibles et un bouton 'Valider' disable", () => {
+    // WHEN THEN
+    // GIVEN
+    expect(page.getByRole('heading', { name: 'Une question sur la thématique Climat', level: 1 })).toBeDefined();
+    expect(
+      page.getByRole('group', { name: "Quel est le principal avantage de l'agriculture biologique ?" })
+    ).toBeDefined();
+    expect(page.getByRole('radio', { name: '1' })).toBeDefined();
+    expect(page.getByRole('radio', { name: '2' })).toBeDefined();
+    expect(page.getByRole('radio', { name: '3' })).toBeDefined();
+    expect(page.getByRole('radio', { name: '4' })).toBeDefined();
+    expect(page.getAllByRole('radio')).toHaveLength(4);
     expect(boutonValider.disabled).toBeTruthy();
   });
 
   describe("quand l'utilisateur répond à une question", () => {
     it("le bouton 'Valider' devient enable", async () => {
-      const { getByRole } = render(PageQuizComposant, { props: pageQuizComposantprops });
-      const boutonValider = getByRole<HTMLButtonElement>('button', { name: 'Valider' });
-      const reponseUn = getByRole('radio', { name: '1' });
+      // WHEN
+      // THEN
+      await fireEvent.click(bonneReponse);
 
-      await fireEvent.click(reponseUn);
+      // GIVEN
       expect(boutonValider.disabled).toBeFalsy();
     });
 
     describe("quand je clique sur le bouton 'Valider'", () => {
       it("les radios deviennent disable et affiche un bouton pour retourner à l'accueil", async () => {
-        const { getAllByRole, getByRole, getByText } = render(PageQuizComposant, { props: pageQuizComposantprops });
-        const radiosButton = getAllByRole<HTMLInputElement>('radio');
-        const boutonValider = getByRole<HTMLButtonElement>('button', { name: 'Valider' });
-        const reponseUn = getByRole('radio', { name: '1' });
+        // WHEN
+        // THEN
+        const radiosButton = page.getAllByRole('radio');
 
-        await fireEvent.click(reponseUn);
+        await fireEvent.click(bonneReponse);
         await fireEvent.click(boutonValider);
 
+        // GIVEN
         radiosButton.forEach(radio => {
           expect(radio.disabled).toBeTruthy();
         });
 
-        expect(getByText("Revenir à l'accueil")).toBeDefined();
+        expect(page.getByText("Revenir à l'accueil")).toBeDefined();
       });
 
       describe('quand la réponse est correcte', () => {
         it('affiche un message de félicitations, le gain de points et la description reponse OK', async () => {
-          const { getByRole, getByText, getByAltText } = render(PageQuizComposant, { props: pageQuizComposantprops });
-          const boutonValider = getByRole<HTMLButtonElement>('button', { name: 'Valider' });
-          const reponseUn = getByRole('radio', { name: '1' });
-
-          await fireEvent.click(reponseUn);
+          // WHEN
+          // THEN
+          await fireEvent.click(bonneReponse);
           await fireEvent.click(boutonValider);
 
-          expect(getByText('Bien joué !')).toBeDefined();
-          expect(getByText('Vous récoltez')).toBeDefined();
-          expect(getByText('+10')).toBeDefined();
-          expect(getByAltText('points')).toBeDefined();
-          expect(getByRole('heading', { name: 'Pourquoi ?' })).toBeDefined();
-          expect(getByText('Lorem texte explication OK')).toBeDefined();
+          // GIVEN
+          expect(page.getByText('Bien joué !')).toBeDefined();
+          expect(page.getByText('Vous récoltez')).toBeDefined();
+          expect(page.getByText('+10')).toBeDefined();
+          expect(page.getByAltText('points')).toBeDefined();
+          expect(page.getByRole('heading', { name: 'Pourquoi ?' })).toBeDefined();
+          expect(page.getByText('Lorem texte explication OK')).toBeDefined();
         });
 
         it('appel le usecase EnvoyerDonneesQuizInteractionUsecase avec les bonnes données', async () => {
+          // WHEN
           const envoyerDonneesQuizInteractionUsecaseMock = vi
             .spyOn(EnvoyerDonneesQuizInteractionUsecase.prototype, 'execute')
             .mockImplementation(() => Promise.resolve());
 
-          const { getByRole } = render(PageQuizComposant, { props: pageQuizComposantprops });
-          const boutonValider = getByRole<HTMLButtonElement>('button', { name: 'Valider' });
-          const reponseUn = getByRole('radio', { name: '1' });
-
-          await fireEvent.click(reponseUn);
+          // THEN
+          await fireEvent.click(bonneReponse);
           await fireEvent.click(boutonValider);
 
+          // GIVEN
           expect(envoyerDonneesQuizInteractionUsecaseMock).toHaveBeenNthCalledWith(
             1,
             'idUtilisateur',
@@ -118,29 +129,27 @@ describe('Page Quiz Article', () => {
 
       describe('quand la réponse est incorrecte', () => {
         it('affiche la description reponse KO', async () => {
-          const { getByRole, getByText } = render(PageQuizComposant, { props: pageQuizComposantprops });
-          const boutonValider = getByRole<HTMLButtonElement>('button', { name: 'Valider' });
-          const reponseUn = getByRole('radio', { name: '2' });
-
-          await fireEvent.click(reponseUn);
+          // WHEN
+          // THEN
+          await fireEvent.click(mauvaiseReponse);
           await fireEvent.click(boutonValider);
 
-          expect(getByRole('heading', { name: 'Pourquoi ?' })).toBeDefined();
-          expect(getByText('Lorem texte explication KO')).toBeDefined();
+          // GIVEN
+          expect(page.getByRole('heading', { name: 'Pourquoi ?' })).toBeDefined();
+          expect(page.getByText('Lorem texte explication KO')).toBeDefined();
         });
 
         it('appel le usecase EnvoyerDonneesQuizInteractionUsecase avec les bonnes données', async () => {
+          // WHEN
           const envoyerDonneesQuizInteractionUsecaseMock = vi
             .spyOn(EnvoyerDonneesQuizInteractionUsecase.prototype, 'execute')
             .mockImplementation(() => Promise.resolve());
 
-          const { getByRole } = render(PageQuizComposant, { props: pageQuizComposantprops });
-          const boutonValider = getByRole<HTMLButtonElement>('button', { name: 'Valider' });
-          const reponseUn = getByRole('radio', { name: '2' });
-
-          await fireEvent.click(reponseUn);
+          // THEN
+          await fireEvent.click(mauvaiseReponse);
           await fireEvent.click(boutonValider);
 
+          // GIVEN
           expect(envoyerDonneesQuizInteractionUsecaseMock).toHaveBeenNthCalledWith(
             1,
             'idUtilisateur',
