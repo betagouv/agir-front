c143c2d4dc710ab48641b38f9c71527781ce9e69
dlamande <contact@dorianlamande.com>
2024-02-26 10:35:23 +0100

    fix: wording et contenu aléatoire des actions quotidiennes (#311)

--- src/components/custom/Linky/LinkyExplicationAleatoire.vue
+++ src/components/custom/Linky/LinkyExplicationAleatoire.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <p>{{ info }}</p>
+    <Transition name="fade">
+      <p v-if="afficherAction">{{ infoActionQuotidienneAffichee }}</p>
+    </Transition>
     <button
       @click="selectionnerInfoAleatoire"
       class="fr-btn fr-btn--tertiary-no-outline fr-btn--icon-left fr-icon-refresh-line"
@@ -12,20 +14,38 @@
 
 <script setup lang="ts">
   import { ref } from 'vue';
+  const afficherAction = ref(true);
 
   const infosActionsQuotidiennes = [
-    "Le lavage à 30°C au lieu de 60°C peut réduire la consommation d'énergie du lave-linge jusqu'à 40%.",
-    "Utiliser le lave-linge à pleine charge peut économiser jusqu'à 3 400 litres d'eau par an.",
-    "Les appareils en mode veille peuvent représenter jusqu'à 10% de la consommation énergétique d'un foyer.",
-    "Utiliser des ampoules LED peut réduire la consommation d'énergie pour l'éclairage de 75% par rapport aux ampoules traditionnelles.",
-    "Utiliser un couvercle sur les casseroles peut réduire l'énergie nécessaire à la cuisson de jusqu'à 90%.",
-    "Chaque degré plus bas dans le réfrigérateur peut augmenter la consommation d'énergie de 5%.",
+    "Laver son linge à 30°C consomme trois fois moins d'énergie qu'à 90°C.",
+    "Le programme Eco du lave-vaisselle utilise 45% d'électricité de mois qu'un programme intensif.",
+    "Couvrir les casseroles permet de raccourcir le temps de cuisson et d'économiser 25% d'électricité ou de gaz.",
+    "Éteindre complètement les appareils plutôt que de les laisser en veille peut représenter jusqu'à 10% d'économies.",
+    "Installer un thermostat programmable pour réguler la température des pièces selon les besoins permet de réaliser jusqu'à 15% d'économies sur le chauffage.",
+    'Réduire la température du logement de 1°C permet de réduire la consommation d’énergie de 7%.',
   ];
 
-  const info = ref(infosActionsQuotidiennes[0]);
+  const infoActionQuotidienneAffichee = ref(infosActionsQuotidiennes[0]);
 
   const selectionnerInfoAleatoire = () => {
-    const randomIndex = Math.floor(Math.random() * infosActionsQuotidiennes.length);
-    info.value = infosActionsQuotidiennes[randomIndex];
+    afficherAction.value = false;
+    const infoActionsQuotidienneSansCelleAffichee = infosActionsQuotidiennes.filter(
+      i => i !== infoActionQuotidienneAffichee.value
+    );
+    const randomIndex = Math.floor(Math.random() * infoActionsQuotidienneSansCelleAffichee.length);
+    infoActionQuotidienneAffichee.value = infoActionsQuotidienneSansCelleAffichee[randomIndex];
+    setTimeout(() => {
+      afficherAction.value = true;
+    }, 500);
   };
 </script>
+<style scoped>
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
