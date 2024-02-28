fb40d7f622fc9e134d06f28bb4236eb27b3b1614
dlamande <contact@dorianlamande.com>
2024-02-28 11:51:42 +0100

    build: mise à jour des dépendances

--- src/authentification/authentifierUtilisateurFranceConnect.usecase.ts
+++ src/authentification/authentifierUtilisateurFranceConnect.usecase.ts
@@ -1,5 +1,5 @@
 import { Utilisateur, UtilisateurRepository } from '@/authentification/ports/utilisateur.repository';
-import jwt_decode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
 export interface SessionRepository {
   sauvegarderUtilisateur(utilisateur: Utilisateur);
@@ -15,7 +15,7 @@ export class AuthentifierUtilisateurFranceConnectUsecase {
   async execute(token: string): Promise<void> {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    const utilisateurId = jwt_decode(token).utilisateurId as string;
+    const utilisateurId = jwtDecode(token).utilisateurId as string;
     const utilisateur = await this._utilisateurRepository.getUtilisateurAvecId(utilisateurId);
     this._sessionRepository.sauvegarderUtilisateur(utilisateur);
   }
