9d03eb0bb61182f2e1ac1025642d5d4815276870
dlamande <contact@dorianlamande.com>
2024-03-28 16:33:41 +0100

    style du tableau

--- src/router/conformite/routes.ts
+++ src/router/conformite/routes.ts
@@ -12,21 +12,33 @@ const conformiteRoutes: RouteRecordRaw[] = [
     path: '/mentions-legales',
     name: RouteConformiteName.MENTIONS_LEGALES,
     component: () => import('@/components/PageMentionsLegales.vue'),
+    meta: {
+      title: 'Mentions légales',
+    },
   },
   {
     path: '/accessibilite',
     name: RouteConformiteName.ACCESSIBILITE,
     component: () => import('@/components/PageAccessibilite.vue'),
+    meta: {
+      title: 'Accessibilité',
+    },
   },
   {
     path: '/cgu',
     name: RouteConformiteName.CGU,
     component: () => import('@/components/PageCGU.vue'),
+    meta: {
+      title: "Conditions générales d'utilisation",
+    },
   },
   {
     path: '/politiques-de-confidentialite',
     name: RouteConformiteName.POLITIQUES_DE_CONFIDENTIALITE,
     component: () => import('@/components/PagePolitiquesDeConfidentialite.vue'),
+    meta: {
+      title: 'Politiques de confidentialité',
+    },
   },
 ];
 
