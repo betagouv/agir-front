a2e1a2135dfa7cee76b3970ac3271f207abaeb54
dlamande <contact@dorianlamande.com>
2024-03-28 11:03:23 +0100

    feat: ajout des pages de conformité

---
+++ src/conformites/adapters/conformite.repository.cms.ts
@@ -0,0 +1,14 @@
+import { ConformiteRepository } from '@/conformites/ports/conformite.repository';
+import { PageConformiteType, PageConformite } from '@/conformites/recupererPageConformite.usecase';
+import { AxiosFactory } from '@/axios.factory';
+
+export class ConformiteRepositoryCms implements ConformiteRepository {
+  async recupererPageConformite(pageConformiteType: PageConformiteType): Promise<PageConformite> {
+    const axiosCMS = AxiosFactory.getCMSAxios();
+    const pageConformiteResponse = await axiosCMS.get(`/conformites/${pageConformiteType.valueOf()}`);
+    return {
+      texte: pageConformiteResponse.data.data.attributes.contenu,
+      titre: pageConformiteResponse.data.data.attributes.titre,
+    };
+  }
+}
