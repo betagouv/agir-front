bb43e5690ee33cd0cc7a1cf6fc84a4642a86e6ea
octo-guic <guillaume.chauvet@octo.com>
2024-04-18 11:06:13 +0200

    feat: ajout du composant lien d'évitement

---
+++ src/components/dsfr/LienDEvitement.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="fr-skiplinks">
+    <nav class="fr-container" role="navigation" aria-label="Accès rapide">
+      <ul class="fr-skiplinks__list">
+        <li><a class="fr-link" href="#contenu">Contenu</a></li>
+        <li><a class="fr-link" href="#header-navigation">Menu</a></li>
+        <li><a class="fr-link" href="#footer">Pied de page</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import '@gouvfr/dsfr/dist/component/skiplink/skiplink.min.css';
+</script>
