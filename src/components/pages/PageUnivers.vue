e43ebb01d49f0a07f8d1ed37d76395dc14801f34
dlamande <contact@dorianlamande.com>
2024-04-29 11:46:55 +0200

    ajout des reco / defis par univers

--- src/components/pages/PageUnivers.vue
+++ src/components/pages/PageUnivers.vue
@@ -1,5 +1,88 @@
 <template>
-  <h1>Univers</h1>
+  <div class="fr-container">
+    <h1 class="fr-h1 fr-m-0 fr-mt-4w">Univers</h1>
+  </div>
+
+  <section
+    class="fr-py-6w background--white"
+    id="defis"
+    v-if="
+      utilisateurStore().utilisateur.fonctionnalitesDebloquees.includes('defis') &&
+      recommandationsPersonnaliseesViewModel
+    "
+  >
+    <div class="fr-container">
+      <CoachActions :recommandations="recommandationsPersonnaliseesViewModel.defis" />
+    </div>
+  </section>
+
+  <section
+    id="recommandations"
+    v-if="store.utilisateur.fonctionnalitesDebloquees.includes(Fonctionnalites.RECOMMANDATIONS)"
+    class="fr-py-6w fr-background-contrast--grey"
+  >
+    <div class="fr-container" v-if="!isLoading">
+      <h2 class="fr-h2 fr-mb-0">Recommandé, pour vous</h2>
+      <p class="fr-text--xl">Une sélection d’articles et de services, pour vous, selon vos préférences !</p>
+      <CoachRecommandations
+        v-if="recommandationsPersonnaliseesViewModel"
+        :recommandations="recommandationsPersonnaliseesViewModel.autresRecommandations"
+      />
+    </div>
+    <div class="fr-container" v-else>
+      <CarteSkeleton />
+    </div>
+  </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { onMounted, ref } from 'vue';
+  import { useRoute } from 'vue-router';
+  import CarteSkeleton from '@/components/CarteSkeleton.vue';
+  import CoachActions from '@/components/custom/Coach/CoachActions.vue';
+  import CoachRecommandations from '@/components/custom/Coach/CoachRecommandations.vue';
+  import {
+    RecommandationPersonnaliseeViewModel,
+    RecommandationsPersonnaliseesPresenterImpl,
+  } from '@/recommandationsPersonnalisees/adapters/recommandationsPersonnalisees.presenter.impl';
+  import { RecommandationsPersonnaliseesRepositoryAxios } from '@/recommandationsPersonnalisees/adapters/recommandationsPersonnalisees.repository.axios';
+  import { RecupererRecommandationsPersonnaliseesUniversUsecase } from '@/recommandationsPersonnalisees/recupererRecommandationsPersonnaliseesUnivers.usecase';
+  import { Fonctionnalites } from '@/shell/fonctionnalitesEnum';
+  import { utilisateurStore } from '@/store/utilisateur';
+
+  const store = utilisateurStore();
+  const route = useRoute();
+  const isLoading = ref<boolean>(true);
+
+  const universId = Array.isArray(route.params.id) ? route.params.id[0] : route.params.id;
+  const recommandationsPersonnaliseesViewModel = ref<RecommandationPersonnaliseeViewModel>();
+
+  function onRecommandationsPretesAAfficher(viewModel: RecommandationPersonnaliseeViewModel) {
+    recommandationsPersonnaliseesViewModel.value = viewModel;
+  }
+
+  const lancerChargementDesDonnees = () => {
+    const idUtilisateur = store.utilisateur.id;
+    const chargerRecommandationsPersonnaliseesUsecase = new RecupererRecommandationsPersonnaliseesUniversUsecase(
+      new RecommandationsPersonnaliseesRepositoryAxios(),
+    );
+
+    Promise.all([
+      chargerRecommandationsPersonnaliseesUsecase.execute(
+        universId,
+        idUtilisateur,
+        new RecommandationsPersonnaliseesPresenterImpl(onRecommandationsPretesAAfficher),
+      ),
+    ])
+      .then(() => {
+        isLoading.value = false;
+      })
+      .catch(() => {
+        isLoading.value = false;
+      });
+  };
+
+  onMounted(() => {
+    lancerChargementDesDonnees();
+  });
+</script>
--- src/recommandationsPersonnalisees/adapters/recommandationsPersonnalisees.repository.axios.ts
+++ src/recommandationsPersonnalisees/adapters/recommandationsPersonnalisees.repository.axios.ts
@@ -12,8 +12,35 @@ export interface RecommandationApiModel {
   image_url: string;
   points: number;
   content_id: string;
+  thematique_principale: string;
 }
 export class RecommandationsPersonnaliseesRepositoryAxios implements RecommandationsPersonnaliseesRepository {
+  async chargerRecommandationsPersonnaliseesUnivers(
+    idUnivers: string,
+    idUtilisateur: string,
+  ): Promise<RecommandationPersonnalisee[]> {
+    const axiosInstance = AxiosFactory.getAxios();
+    const response = await axiosInstance.get<RecommandationApiModel[]>(
+      `/utilisateurs/${idUtilisateur}/recommandations`,
+    );
+
+    return response.data
+      .filter(model => model.thematique_principale === idUnivers)
+      .map((apiModel: RecommandationApiModel) => {
+        const recommandationPersonnalisee: RecommandationPersonnalisee = {
+          type: apiModel.type as InteractionType,
+          titre: apiModel.titre,
+          thematique: apiModel.thematique_gamification,
+          nombreDePointsAGagner: apiModel.points.toString(),
+          illustrationURL: apiModel.image_url,
+          idDuContenu: apiModel.content_id,
+          joursRestants: apiModel.jours_restants,
+          points: apiModel.points,
+        };
+
+        return recommandationPersonnalisee;
+      });
+  }
   @intercept401()
   async recommandationAEteCliquee(idUtilisateur: string): Promise<void> {
     await axios.post(`/utilisateurs/${idUtilisateur}/events`, {
