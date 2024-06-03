2842b12a816bce67b1a44aed7ddf62be56ed0d60
octo-guic <guillaume.chauvet@octo.com>
2024-06-03 10:25:27 +0200

    tech: création composant ThematiqueQuestionKyc

---
+++ src/components/custom/Thematiques/ThematiqueQuestionsKyc.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-for="(questionViewModel, index) in questionsViewModel" :key="index">
+    <div v-show="index === etapeCourante">
+      <p class="text--bleu fr-grid-row align-items--center fr-py-2w">
+        <button
+          v-if="index !== 0"
+          class="fr-btn fr-btn--tertiary-no-outline fr-icon-arrow-left-line"
+          :title="`Retour à l'étape ${index}`"
+          @click="etapeCourante--"
+        >
+          Retour à l'étape précédente
+        </button>
+        <span class="fr-text--bold">Question {{ index + 1 }}</span>
+        &nbsp; sur {{ questionsViewModel.length }}
+      </p>
+      <KYCForm
+        :question-view-model="questionViewModel"
+        wording-bouton="Continuer"
+        @update:soumission-kyc="passerEtapeSuivante"
+      />
+    </div>
+  </div>
+  <KYCFin v-if="afficherFinKyc" :a-deja-repondu="false" phrasePointAGagner="toto" />
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import KYCFin from '@/components/custom/KYC/KYCFin.vue';
+  import KYCForm from '@/components/custom/KYC/KYCForm.vue';
+  import { QuestionViewModel } from '@/domaines/kyc/adapters/question.presenter.impl';
+
+  const props = defineProps<{ questionsViewModel: QuestionViewModel[] }>();
+
+  const premiereKycNonRepondu = () => {
+    const indexQuestion = props.questionsViewModel.findIndex(
+      question => question.reponses.length === 0 && !question.aDejaEteRepondu,
+    );
+    return indexQuestion !== -1 ? indexQuestion : 0;
+  };
+
+  const etapeCourante = ref<number>(premiereKycNonRepondu());
+  const afficherFinKyc = ref<boolean>(false);
+
+  const passerEtapeSuivante = () => {
+    etapeCourante.value++;
+    afficherFinKyc.value = etapeCourante.value === props.questionsViewModel.length;
+  };
+</script>
