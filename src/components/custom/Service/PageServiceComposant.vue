2990b68d70881c89fa70411c563763c3c60bbd92
octo-guic <guillaume.chauvet@octo.com>
2024-07-01 17:08:58 +0200

    tech: ajout loader et refacto composant

---
+++ src/components/custom/Service/PageServiceComposant.vue
@@ -0,0 +1,31 @@
+<template>
+  <FilDAriane
+    :page-courante="`Service : ${serviceRechercheViewModel.titre}`"
+    :page-hierarchie="[{ label: 'Vos services', url: RouteCoachName.SERVICES }]"
+  />
+  <h1 class="fr-h2">{{ serviceRechercheViewModel.titre }}</h1>
+  <div class="fr-grid-row fr-grid-row--gutters">
+    <div class="fr-col-12 fr-col-md-8">
+      <section v-if="serviceRechercheViewModel.favoris">
+        <ServiceCarousel :services-recherche-favoris-view-model="serviceRechercheViewModel.favoris" />
+      </section>
+      <section v-if="serviceRechercheViewModel.suggestions" class="fr-py-6w">
+        <ServiceListeCarte :suggestions-service-view-model="serviceRechercheViewModel.suggestions" />
+      </section>
+    </div>
+    <div class="fr-col-12 fr-col-md-4">
+      <ServiceAside :service-footer-view-model="serviceRechercheViewModel.aside" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import ServiceAside from '@/components/custom/Service/ServiceAside.vue';
+  import ServiceCarousel from '@/components/custom/Service/ServiceCarousel.vue';
+  import ServiceListeCarte from '@/components/custom/Service/ServiceListeCarte.vue';
+  import FilDAriane from '@/components/dsfr/FilDAriane.vue';
+  import { ServiceRechercheViewModel } from '@/domaines/serviceRecherche/adapters/serviceRecherche.presenter.impl';
+  import { RouteCoachName } from '@/router/coach/routeCoachName';
+
+  defineProps<{ serviceRechercheViewModel: ServiceRechercheViewModel }>();
+</script>
