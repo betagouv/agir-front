6595368554165b436f2b0d31627afa165406f427
octo-guic <guillaume.chauvet@octo.com>
2024-07-01 17:08:58 +0200

    tech: ajout loader et refacto composant

--- src/components/pages/PageService.vue
+++ src/components/pages/PageService.vue
@@ -1,46 +1,33 @@
 <template>
-  <div class="fr-container" v-if="serviceRechercheViewModel">
-    <FilDAriane
-      :page-courante="`Service : ${serviceRechercheViewModel.titre}`"
-      :page-hierarchie="[{ label: 'Vos services', url: RouteCoachName.SERVICES }]"
-    />
-    <h1 class="fr-h2">{{ serviceRechercheViewModel.titre }}</h1>
-    <div class="fr-grid-row fr-grid-row--gutters">
-      <div class="fr-col-12 fr-col-md-8">
-        <section v-if="serviceRechercheViewModel.favoris">
-          <ServiceCarousel :services-recherche-favoris-view-model="serviceRechercheViewModel.favoris" />
-        </section>
-        <section v-if="serviceRechercheViewModel.suggestions" class="fr-py-6w">
-          <ServiceListeCarte :suggestions-service-view-model="serviceRechercheViewModel.suggestions" />
-        </section>
-      </div>
-      <div class="fr-col-12 fr-col-md-4">
-        <ServiceAside :service-footer-view-model="serviceRechercheViewModel.aside" />
-      </div>
-    </div>
+  <div class="fr-container">
+    <div v-if="isLoading">Chargement en cours ...</div>
+    <p v-else-if="!serviceRechercheViewModel">Problème de chargement de donées</p>
+    <PageServiceComposant v-else :service-recherche-view-model="serviceRechercheViewModel" />
   </div>
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue';
-  import ServiceAside from '@/components/custom/Service/ServiceAside.vue';
-  import ServiceCarousel from '@/components/custom/Service/ServiceCarousel.vue';
-  import ServiceListeCarte from '@/components/custom/Service/ServiceListeCarte.vue';
-  import FilDAriane from '@/components/dsfr/FilDAriane.vue';
+  import { onMounted, ref } from 'vue';
+  import PageServiceComposant from '@/components/custom/Service/PageServiceComposant.vue';
   import {
     ServiceRecherchePresenterImpl,
     ServiceRechercheViewModel,
   } from '@/domaines/serviceRecherche/adapters/serviceRecherche.presenter.impl';
   import { ServiceRechercheAxios } from '@/domaines/serviceRecherche/adapters/serviceRecherche.repository.axios';
   import { RecupererServiceRechercheUsecase } from '@/domaines/serviceRecherche/recupererServiceRecherche.usecase';
-  import { RouteCoachName } from '@/router/coach/routeCoachName';
   import { utilisateurStore } from '@/store/utilisateur';
 
+  const isLoading = ref<boolean>(true);
   const serviceRechercheViewModel = ref<ServiceRechercheViewModel>();
-  const usecase = new RecupererServiceRechercheUsecase(new ServiceRechercheAxios());
-  usecase.execute(
-    utilisateurStore().utilisateur.id,
-    'proximite',
-    new ServiceRecherchePresenterImpl(vm => (serviceRechercheViewModel.value = vm)),
-  );
+
+  onMounted(async () => {
+    const usecase = new RecupererServiceRechercheUsecase(new ServiceRechercheAxios());
+    await usecase.execute(
+      utilisateurStore().utilisateur.id,
+      'proximite',
+      new ServiceRecherchePresenterImpl(vm => (serviceRechercheViewModel.value = vm)),
+    );
+
+    isLoading.value = false;
+  });
 </script>
