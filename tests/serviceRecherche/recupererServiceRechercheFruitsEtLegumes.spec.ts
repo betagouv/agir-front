4d5cf3b96dc866d986c8b9557e31453e13e39dc2
octo-guic <guillaume.chauvet@octo.com>
2024-07-03 17:07:05 +0200

    fix: test recupererServiceFruitsEtLegume

--- tests/serviceRecherche/recupererServiceRechercheFruitsEtLegumes.spec.ts
+++ tests/serviceRecherche/recupererServiceRechercheFruitsEtLegumes.spec.ts
@@ -9,28 +9,47 @@ describe('Fichier de tests concernant la récuperation du service Fruits et Lég
   it("en donnant l'id d'un utilisateur, renvoie la liste des fruits et légumes du mois triès", () => {
     // GIVEN
     const usecase = new RecupererServiceFruitsEtLegumesUsecase(
-      new ServiceRechercheFruitsEtLegumesRepositoryMock([
-        {
-          titre: 'Haricot',
-          impactCarboneKg: 0.4130619719,
-        },
-        {
-          titre: 'Aubergine',
-          impactCarboneKg: 0.4571093429,
-        },
-        {
-          titre: 'Ananas',
-          impactCarboneKg: 1.292282106,
-        },
-        {
-          titre: 'Cerise',
-          impactCarboneKg: 1.3353255069,
-        },
-        {
-          titre: 'Mangue',
-          impactCarboneKg: 10.641545366,
-        },
-      ]),
+      new ServiceRechercheFruitsEtLegumesRepositoryMock({
+        listeFruitsEtLegumes: [
+          {
+            titre: 'Haricot',
+            impactCarboneKg: 0.4130619719,
+          },
+          {
+            titre: 'Aubergine',
+            impactCarboneKg: 0.4571093429,
+          },
+          {
+            titre: 'Ananas',
+            impactCarboneKg: 1.292282106,
+          },
+          {
+            titre: 'Cerise',
+            impactCarboneKg: 1.3353255069,
+          },
+          {
+            titre: 'Mangue',
+            impactCarboneKg: 10.641545366,
+          },
+        ],
+        categories: [
+          {
+            code: 'mai',
+            label: 'mai',
+            estLaCategorieParDefaut: false,
+          },
+          {
+            code: 'juin',
+            label: 'juin',
+            estLaCategorieParDefaut: false,
+          },
+          {
+            code: 'juillet',
+            label: 'juillet',
+            estLaCategorieParDefaut: true,
+          },
+        ],
+      }),
     );
 
     // WHEN
@@ -49,6 +68,23 @@ describe('Fichier de tests concernant la récuperation du service Fruits et Lég
           logo: '',
           screenshot: '',
         },
+        categories: [
+          {
+            code: 'mai',
+            label: 'mai',
+            estLaCategorieParDefaut: false,
+          },
+          {
+            code: 'juin',
+            label: 'juin',
+            estLaCategorieParDefaut: false,
+          },
+          {
+            code: 'juillet',
+            label: 'juillet',
+            estLaCategorieParDefaut: true,
+          },
+        ],
       });
     }
   });
