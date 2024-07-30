0e00b7e40af5d15b8041fe475e43e65d049c5c51
octo-guic <guillaume.chauvet@octo.com>
2024-07-30 15:46:05 +0200

    tech: ajout de tests

---
+++ tests/serviceRecherche/adapters/serviceRecherche.repository.mock.ts
@@ -0,0 +1,10 @@
+import { ServiceRechercheRepository } from '@/domaines/serviceRecherche/ports/serviceRecherche.repository';
+import { ServicesRecherche } from '@/domaines/serviceRecherche/recupererServicesRechercheParUnivers.usecase';
+
+export class ServiceRechercheRepositoryMock implements ServiceRechercheRepository {
+  constructor(private servicesRechercheARetourner: ServicesRecherche) {}
+
+  recupererServicesParUnivers(_idUtilisateur: string): Promise<ServicesRecherche> {
+    return Promise.resolve(this.servicesRechercheARetourner);
+  }
+}
