e1392df94b40c2adfe4087e58502e3ef1bad38ad
dlamande <contact@dorianlamande.com>
2024-08-01 11:20:09 +0200

    feat: affichage d'un disclaimer à la création du compte (#540)

---
+++ src/components/pages/PagePostCreationCompte/PagePostCreationCompteDisclaimer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="fr-container fr-py-6w">
+    <div class="fr-col-12 fr-col-md-6 fr-mx-auto fr-mb-0 background--white fr-p-4w border border-radius--md">
+      <img src="/bg_creation_compte_disclaimer.png" alt="" />
+      <h1 class="fr-h4 fr-mb-3w">Agir est encore en expérimentation&nbsp;!</h1>
+      <p class="fr-text--regular fr-text--lg">Nous ajoutons progressivement les aides et contenus locaux.</p>
+      <p class="fr-text--regular fr-text--lg">Revenez régulièrement pour profiter de nos dernières nouveautés</p>
+
+      <router-link :to="{ name: RouteCompteName.POST_CREATION_COMPTE_ETAPE_1 }" class="fr-btn fr-btn--lg fr-mt-1v"
+        >J'ai compris
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { RouteCompteName } from '@/router/compte/routeCompteName';
+</script>
