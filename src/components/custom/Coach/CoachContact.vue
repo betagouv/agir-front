d6d2cf25b5aedffecee3668211bdcceb69f4f80f
OCTO-GUIC <121949498+OCTO-GUIC@users.noreply.github.com>
2024-08-07 11:54:51 +0200

    feat: modification section contact page accueil (#554)

---
+++ src/components/custom/Coach/CoachContact.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--middle">
+    <div class="fr-col-md-8">
+      <h2 class="fr-h4 text--white">Agir est un nouveau service qui continue de s’améliorer</h2>
+      <p class="text--white fr-text--xl fr-mb-0">
+        N’hésitez pas à nous contacter par mail pour nous partager vos retours, poser vos questions et suggestions de
+        fonctionnalité que vous aimeriez voir sur Agir.
+      </p>
+    </div>
+    <div class="fr-col-md-4">
+      <a href="mailto:contact@agir.beta.gouv.fr" class="fr-btn fr-btn--icon-left fr-icon-mail-line border--white">
+        Suggérer une amélioration
+      </a>
+    </div>
+  </div>
+</template>
