9c3e01fd0ce3b51a97b09af036da52b61504a55b
dlamande <contact@dorianlamande.com>
2024-09-05 10:15:26 +0200

    suppression erreur lint

--- src/components/validations/validationsChampsFormulaire.ts
+++ src/components/validations/validationsChampsFormulaire.ts
@@ -1,6 +1,5 @@
 export function validationPrenom(prenom: string): boolean {
-  // eslint-disable-next-line no-useless-escape
-  const regex = /^[A-Za-zÀ-ÿ\-]+$/;
+  const regex = /^[A-Za-zÀ-ÿ\\-]+$/;
   return regex.test(prenom);
 }
 
@@ -8,7 +7,6 @@ export function validationNom(nom: string): boolean {
   if (nom == '') {
     return true;
   }
-  // eslint-disable-next-line no-useless-escape
-  const regex = /^[A-Za-zÀ-ÿ\-]*$/;
+  const regex = /^[A-Za-zÀ-ÿ\\-]*$/;
   return regex.test(nom);
 }
