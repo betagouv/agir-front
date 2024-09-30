0f21c1e746eb43c6939cdec2fe870bd5f10b5957
octo-guic <guillaume.chauvet@octo.com>
2024-09-30 17:35:07 +0200

    feat: creation composant RappelCTA

--- src/components/pages/PageAccueil.vue
+++ src/components/pages/PageAccueil.vue
@@ -14,9 +14,9 @@
       <LandingNotreApproche />
     </div>
   </section>
-  <section class="background--bleu-dark fr-py-8w">
+  <section class="fr-py-8w rappel-cta">
     <div class="fr-container">
-      <LandingPourQui />
+      <LandingRappelCTA />
     </div>
   </section>
 </template>
@@ -24,6 +24,17 @@
 <script setup lang="ts">
   import LandingHeader from '@/components/custom/Landing/LandingHeader.vue';
   import LandingNotreApproche from '@/components/custom/Landing/LandingNotreApproche.vue';
-  import LandingPourQui from '@/components/custom/Landing/LandingPourQui.vue';
   import LandingPourquoi from '@/components/custom/Landing/LandingPourquoi.vue';
+  import LandingRappelCTA from '@/components/custom/Landing/LandingRappelCTA.vue';
 </script>
+
+<style scoped>
+  .rappel-cta {
+    position: relative;
+    background-color: var(--blue-france-sun-113-625);
+    background-image: url('/hp-background-rappel-cta.svg');
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+</style>
