bede3a958e33c6a20746f8388ae4f52f9b8a7002
dlamande <contact@dorianlamande.com>
2024-09-30 10:36:38 +0200

    fix: saisie de rfr invalide (#633)

--- src/components/custom/Compte/CompteFormulaireRevenuFiscal.vue
+++ src/components/custom/Compte/CompteFormulaireRevenuFiscal.vue
@@ -19,17 +19,44 @@
     <div class="fr-col-lg-6 fr-col-12">
       <InputTextAvecIcon
         name="revenu"
-        @update:model-value="value => (revenuFiscalDeReferenceModel = Number(value))"
+        @update:model-value="
+          value => {
+            const numberValue = Number(value);
+            if (isNaN(numberValue)) {
+              revenuFiscalDeReferenceModel = 0;
+            } else {
+              revenuFiscalDeReferenceModel = numberValue;
+            }
+          }
+        "
         :model-value="revenuFiscalDeReferenceModel?.toString() || ''"
         label="Revenu fiscal de référence de votre foyer"
         icon="fr-icon-money-euro-circle-line"
+        :erreur="champsRFRStatus"
+        @blur="onValidationRFR"
       />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  import { ref } from 'vue';
   import InputTextAvecIcon from '@/components/dsfr/InputTextAvecIcon.vue';
   const nombreDePartsModel = defineModel<number>('nombreDeParts', { default: 1 });
-  const revenuFiscalDeReferenceModel = defineModel<number | null>('revenuFiscalDeReference', { default: 0 });
+  const revenuFiscalDeReferenceModel = defineModel<number | null>('revenuFiscalDeReference');
+  const champsRFRStatus = ref<{ message: string; afficher: boolean }>({ message: '', afficher: false });
+
+  const emit = defineEmits<{
+    (e: 'update:isRFREnErreur', value: boolean): void;
+  }>();
+
+  const onValidationRFR = () => {
+    if (revenuFiscalDeReferenceModel.value && revenuFiscalDeReferenceModel.value < 0) {
+      champsRFRStatus.value = { message: 'Le revenu fiscal de référence doit être positif', afficher: true };
+      emit('update:isRFREnErreur', true);
+    } else {
+      champsRFRStatus.value = { message: '', afficher: false };
+      emit('update:isRFREnErreur', false);
+    }
+  };
 </script>
