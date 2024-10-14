91360e74b46d5f37c97fe62b1afaf5c884f83259
octo-guic <guillaume.chauvet@octo.com>
2024-10-14 14:24:17 +0200

    feat: update image

--- src/components/custom/Landing/LandingNotreApproche.vue
+++ src/components/custom/Landing/LandingNotreApproche.vue
@@ -46,7 +46,7 @@
       picto: 'fr-icon-award-line',
       titre: 'Économies mesurables',
       description: 'Suivez vos actions et mesurer leurs impacts grâce au bilan environnemental',
-      image: '/hp-approche-3.webp',
+      image: '/hp-approche-3.png',
     },
   ];
 </script>
