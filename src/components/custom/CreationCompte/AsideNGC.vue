d06af9bb21cfa2cc7012004a5818cf12c55bbdd3
octo-guic <guillaume.chauvet@octo.com>
2024-10-15 14:49:06 +0200

    feat: stylise bloc AsideNgv

---
+++ src/components/custom/CreationCompte/AsideNGC.vue
@@ -0,0 +1,15 @@
+<template>
+  <aside class="full-width fr-p-2w fr-pb-5w border-radius--md background--white position--relative fr-mt-6w shadow">
+    <span class="fr-text--xl text--bold fr-mb-2w display-block">Envie d'en savoir plus ?</span>
+    <router-link class="fr-btn fr-btn--secondary" :to="{ path: '/' }">Découvrir</router-link>
+    <img src="/aside_inscription_ngc.png" class="image-aside-ngc" height="200px" alt="" />
+  </aside>
+</template>
+
+<style scoped>
+  .image-aside-ngc {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+  }
+</style>
