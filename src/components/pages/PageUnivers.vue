5817e8f032eebef7f8e4d64d129eade9d358bea7
dlamande <contact@dorianlamande.com>
2024-10-16 17:13:52 +0200

    feat: passage des univers dans le header

--- src/components/pages/PageUnivers.vue
+++ src/components/pages/PageUnivers.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="fr-container">
+  <div class="fr-container fr-mt-4w" v-if="isLoading">
+    <CarteSkeleton />
+  </div>
+
+  <div v-else class="fr-container">
     <FilDAriane :page-courante="`Univers - ${universViewModel?.nom}`" />
     <div class="fr-grid-row align-items--center fr-mb-4w">
       <img
@@ -13,62 +17,62 @@
     </div>
   </div>
 
-  <section id="thematiques" v-if="thematiquesViewModel">
-    <div class="fr-container">
-      <ThematiquesListe :univers-id="universId" :thematiques="thematiquesViewModel" />
-    </div>
-  </section>
+  <div v-if="!isLoading">
+    <section id="thematiques" v-if="thematiquesViewModel">
+      <div class="fr-container">
+        <ThematiquesListe :univers-id="useRoute().params.id" :thematiques="thematiquesViewModel" />
+      </div>
+    </section>
 
-  <section class="fr-py-6w background--white" id="defis" v-if="defisViewModel">
-    <div class="fr-container">
-      <ActionListe :defis="defisViewModel" />
-    </div>
-  </section>
-  <section class="fr-py-6w" v-if="servicesViewModel?.services">
-    <div class="fr-container">
-      <h2 class="fr-h2">Mes services</h2>
-      <div class="fr-grid-row fr-grid-row--gutters">
-        <div v-for="service in servicesViewModel.services" :key="service.label" class="fr-col-6 fr-col-md-3">
-          <ServiceLinkExterne
-            v-if="service.estServiceExterne"
-            :url="service.url"
-            :titre="service.label"
-            :sous-titre="service.legende"
-          />
-          <ServiceLink
-            v-else
-            :url="service.url"
-            :label="service.label"
-            :picto="service.picto"
-            :legende="service.legende"
-          />
+    <section class="fr-py-6w background--white" id="defis" v-if="defisViewModel">
+      <div class="fr-container">
+        <ActionListe :defis="defisViewModel" />
+      </div>
+    </section>
+
+    <section class="fr-py-6w" v-if="servicesViewModel?.services">
+      <div class="fr-container">
+        <h2 class="fr-h2">Mes services</h2>
+        <div class="fr-grid-row fr-grid-row--gutters">
+          <div v-for="service in servicesViewModel.services" :key="service.label" class="fr-col-6 fr-col-md-3">
+            <ServiceLinkExterne
+              v-if="service.estServiceExterne"
+              :url="service.url"
+              :titre="service.label"
+              :sous-titre="service.legende"
+            />
+            <ServiceLink
+              v-else
+              :url="service.url"
+              :label="service.label"
+              :picto="service.picto"
+              :legende="service.legende"
+            />
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-  <section
-    id="recommandations"
-    v-if="store.utilisateur.fonctionnalitesDebloquees.includes(Fonctionnalites.RECOMMANDATIONS)"
-    class="fr-py-6w fr-background-contrast--grey"
-  >
-    <div class="fr-container" v-if="!isLoading">
-      <h2 class="fr-h2 fr-mb-0">Recommandé, pour vous</h2>
-      <p class="fr-text--xl">Une sélection d’articles et de services, pour vous, selon vos préférences !</p>
-      <CoachRecommandations
-        v-if="recommandationsPersonnaliseesViewModel"
-        :recommandations="recommandationsPersonnaliseesViewModel.autresRecommandations"
-      />
-    </div>
-    <div class="fr-container" v-else>
-      <CarteSkeleton />
-    </div>
-  </section>
+    <section
+      id="recommandations"
+      v-if="store.utilisateur.fonctionnalitesDebloquees.includes(Fonctionnalites.RECOMMANDATIONS)"
+      class="fr-py-6w fr-background-contrast--grey"
+    >
+      <div class="fr-container" v-if="!isLoading">
+        <h2 class="fr-h2 fr-mb-0">Recommandé, pour vous</h2>
+        <p class="fr-text--xl">Une sélection d’articles et de services, pour vous, selon vos préférences !</p>
+        <CoachRecommandations
+          v-if="recommandationsPersonnaliseesViewModel"
+          :recommandations="recommandationsPersonnaliseesViewModel.autresRecommandations"
+        />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
   import { onMounted, ref } from 'vue';
-  import { useRoute } from 'vue-router';
+  import { onBeforeRouteUpdate, useRoute } from 'vue-router';
   import ActionListe from '../custom/Action/ActionListe.vue';
   import CarteSkeleton from '@/components/CarteSkeleton.vue';
   import CoachRecommandations from '@/components/custom/Coach/CoachRecommandations.vue';
@@ -108,17 +112,15 @@
   import { utilisateurStore } from '@/store/utilisateur';
 
   const store = utilisateurStore();
-  const route = useRoute();
   const isLoading = ref<boolean>(true);
-
-  const universId = Array.isArray(route.params.id) ? route.params.id[0] : route.params.id;
   const recommandationsPersonnaliseesViewModel = ref<RecommandationPersonnaliseeViewModel>();
   const thematiquesViewModel = ref<ThematiqueViewModel[]>();
   const universViewModel = ref<UniversViewModel>();
   const defisViewModel = ref<DefiDescriptionViewModel[]>();
   const servicesViewModel = ref<ServicesRechercheViewModel>();
-
+  let universId = useRoute().params.id;
   const lancerChargementDesDonnees = () => {
+    isLoading.value = true;
     const idUtilisateur = store.utilisateur.id;
     const recupererUniversUsecase = new RecupererUniversUsecase(new UniversRepositoryAxios());
     const chargerRecommandationsPersonnaliseesUsecase = new RecupererRecommandationsPersonnaliseesUniversUsecase(
@@ -163,7 +165,11 @@
         isLoading.value = false;
       });
   };
-
+  onBeforeRouteUpdate((to, from, next) => {
+    next();
+    universId = to.params.id;
+    lancerChargementDesDonnees();
+  });
   onMounted(() => {
     lancerChargementDesDonnees();
   });
