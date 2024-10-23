ee2fc46f1799245e721d74f746131288a158fd80
Matteo Leclercq <matteo.leclercq@octo.com>
2024-10-23 16:05:49 +0200

    tech: nouveau wording dans l'accueil

--- src/components/custom/Coach/CoachBilanCarbonePartiel.vue
+++ src/components/custom/Coach/CoachBilanCarbonePartiel.vue
@@ -1,10 +1,10 @@
 <template>
-  <h2 class="fr-h2">Estimez votre <span class="text--bleu">bilan environnemental</span></h2>
+  <h2 class="fr-h2">Affiner mon <span class="text--bleu">bilan environnemental</span></h2>
   <p>
-    Et obtenez des <span class="text--bold">recommandations</span> et
+    Pour obtenir des <span class="text--bold">recommandations</span> et
     <span class="text--bold">aides</span> personnalisées
   </p>
-  <ul class="fr-grid-row fr-grid-row--gutters list-style-none fr-mb-4w">
+  <ul class="fr-grid-row fr-grid-row--gutters list-style-none fr-mb-2w">
     <li class="fr-col-md-2 fr-col-6" v-for="univers in universBilan" :key="univers.contentId">
       <BilanUniversCarte
         :content-id="univers.contentId"
@@ -17,15 +17,17 @@
       />
     </li>
   </ul>
-  <p class="fr-mb-0 background--bleu-info fr-p-2w border border-radius--md display-inline-block border--bleu">
+  <p class="background--bleu-info fr-p-2w border border-radius--md width--fit-content border--bleu">
     ✨ Estimation complète à
     <span class="text--bleu fr-text--bold">{{ pourcentageCompletion }}%</span>
   </p>
+  <router-link :to="{ name: RouteBilanCarboneName.BILAN_CARBONE }" class="fr-link">Voir mon bilan</router-link>
 </template>
 
 <script setup lang="ts">
   import BilanUniversCarte from '@/components/custom/BilanCarbone/BilanUniversCarte.vue';
   import { BilanCarbonePartielViewModel } from '@/domaines/bilanCarbone/adapters/bilanCarbone.presenter.impl';
+  import { RouteBilanCarboneName } from '@/router/bilanCarbone/routes';
 
   defineProps<{
     pourcentageCompletion: BilanCarbonePartielViewModel['pourcentageCompletionTotal'];
