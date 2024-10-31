52fe36c1fefc6846b3784990ebd570f6ccd1d7b6
octo-guic <guillaume.chauvet@octo.com>
2024-10-31 13:52:37 +0100

    tech: test e2e meta donne bilan carbone

--- tests/e2e/bilanCarbone.spec.ts
+++ tests/e2e/bilanCarbone.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect, chromium, Page } from '@playwright/test';
 import { InjectUtilisateur } from './utils/injectUtilisateur';
 import { InjectGamification } from './utils/injectGamification';
+import { getByText } from '@testing-library/vue';
 
 let page: Page;
 
@@ -25,22 +26,27 @@ test.beforeAll(async () => {
     });
   });
 
-  await page.route(`${process.env.VITE_API_URL}/utilisateur/dorian/bilans/last`, route => {
-    route.fulfill({
-      status: 200,
-      contentType: 'application/json',
-      body: JSON.stringify({}),
-    });
-  });
+  await page.route(`${process.env.VITE_API_URL}/utilisateur/dorian/bilans/last`, async _route => {});
 });
 
-test.describe('bilan carbone', () => {
-  test("doit afficher le bon fil d'ariane et meta-titre", async () => {
-    await page.goto('/bilan-environnemental');
+test.describe('Bilan carbone', () => {
+  test.describe('quand le bilan carbone est en cours de chargement', async () => {
+    test('doit afficher le meta titre associé, le breadcrumb et le loader', async () => {
+      await page.goto('/bilan-environnemental');
 
-    await expect(page).toHaveTitle("J'agis - Mon bilan environnemental");
+      await expect(page).toHaveTitle("J'agis - Mon bilan environnemental");
 
-    const breadcrumb = page.getByRole('navigation', { name: 'Vous êtes ici :', exact: true });
-    await expect(breadcrumb).toBeVisible();
+      const breadcrumb = page.getByRole('navigation', { name: 'Vous êtes ici :', exact: true });
+      await expect(breadcrumb).toBeVisible();
+
+      const nombreElementsBreadcrumb = await breadcrumb.getByRole('list').getByRole('listitem').count();
+      const premierElementDuBreadcrumb = breadcrumb.getByRole('list').getByRole('listitem').first();
+      const lienAccueil = premierElementDuBreadcrumb.getByRole('link', { name: 'Accueil', exact: true });
+      expect(nombreElementsBreadcrumb).toEqual(2);
+      await expect(lienAccueil).toBeVisible();
+
+      const loader = page.getByText('Chargement en cours ...', { exact: true });
+      await expect(loader).toBeVisible();
+    });
   });
 });
