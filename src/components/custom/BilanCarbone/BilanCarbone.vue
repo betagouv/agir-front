17c4c564ee7017e58cdbf69ea0664f4def02c02d
Matteo Leclercq <matteo.leclercq@octo.com>
2024-11-06 15:49:27 +0100

    tech: wip test composant bilan carbone

---
+++ src/components/custom/BilanCarbone/BilanCarbone.vue
@@ -0,0 +1,18 @@
+<template>
+  <BilanCarboneComplet v-if="bilanCarboneComplet" :bilan-carbone-view-model="bilanCarboneComplet" />
+  <BilanCarbonePartiel v-else-if="bilanCarbonePartiel" :bilan-carbone-partiel-view-model="bilanCarbonePartiel" />
+</template>
+
+<script setup lang="ts">
+  import BilanCarboneComplet from '@/components/pages/BilanCarboneComplet.vue';
+  import BilanCarbonePartiel from '@/components/pages/BilanCarbonePartiel.vue';
+  import {
+    BilanCarboneCompletViewModel,
+    BilanCarbonePartielViewModel,
+  } from '@/domaines/bilanCarbone/adapters/bilanCarbone.presenter.impl';
+
+  defineProps<{
+    bilanCarboneComplet?: BilanCarboneCompletViewModel;
+    bilanCarbonePartiel?: BilanCarbonePartielViewModel;
+  }>();
+</script>
---
+++ tests/components/BilanCarbone.spec.ts
@@ -0,0 +1,268 @@
+import { render } from '@testing-library/vue';
+import BilanCarbone from '@/components/custom/BilanCarbone/BilanCarbone.vue';
+import {
+  BilanCarboneCompletViewModel,
+  BilanCarbonePartielViewModel,
+} from '@/domaines/bilanCarbone/adapters/bilanCarbone.presenter.impl';
+
+describe('BilanCarbone', () => {
+  describe('Quand le bilan est incomplet', () => {
+    it('affiche le titre du bilan partiel', () => {
+      const bilanCarboneProps: {
+        bilanCarbonePartiel?: BilanCarbonePartielViewModel;
+        bilanCarboneComplet?: BilanCarboneCompletViewModel;
+      } = {
+        bilanCarbonePartiel: {
+          titre: 'Estimez mon <span class="text--bleu">bilan environnemental</span>',
+          pourcentageCompletionTotal: 89,
+          categories: [
+            {
+              label: '🚙 Transports',
+              tag: { wording: 'Fort', classes: 'tag-impact-fort' },
+              progressBarStyle: 'progress-bar-impact-fort',
+            },
+            {
+              label: '🥘 Alimentation',
+              tag: { wording: 'Très fort', classes: 'tag-impact-tres-fort' },
+              progressBarStyle: 'progress-bar-impact-tres-fort',
+            },
+            {
+              label: '🏡 Logement',
+              tag: { wording: 'Fort', classes: 'tag-impact-fort' },
+              progressBarStyle: 'progress-bar-impact-fort',
+            },
+            {
+              label: '🛍 Consommation',
+              tag: { wording: 'Fort', classes: 'tag-impact-fort' },
+              progressBarStyle: 'progress-bar-impact-fort',
+            },
+          ],
+          thematiquesBilan: [
+            {
+              clefUnivers: 'transport',
+              contentId: 'ENCHAINEMENT_KYC_bilan_transport',
+              label: '🚗 Transports',
+              urlImage: 'https://res.cloudinary.com/dq023imd8/image/upload/v1728466903/Mobilite_df75aefd09.svg',
+              estTermine: true,
+              pourcentageProgression: 100,
+              nombreTotalDeQuestion: 9,
+            },
+            {
+              clefUnivers: 'alimentation',
+              contentId: 'ENCHAINEMENT_KYC_bilan_alimentation',
+              label: '🥦 Alimentation',
+              urlImage: 'https://res.cloudinary.com/dq023imd8/image/upload/v1728466523/cuisine_da54797693.svg',
+              estTermine: false,
+              pourcentageProgression: 33,
+              nombreTotalDeQuestion: 6,
+            },
+            {
+              clefUnivers: 'consommation',
+              contentId: 'ENCHAINEMENT_KYC_bilan_consommation',
+              label: '🛒 Consommation durable',
+              urlImage: 'https://res.cloudinary.com/dq023imd8/image/upload/v1728468852/conso_7522b1950d.svg',
+              estTermine: true,
+              pourcentageProgression: 100,
+              nombreTotalDeQuestion: 8,
+            },
+            {
+              clefUnivers: 'logement',
+              contentId: 'ENCHAINEMENT_KYC_bilan_logement',
+              label: '🏡 Logement',
+              urlImage: 'https://res.cloudinary.com/dq023imd8/image/upload/v1728468978/maison_80242d91f3.svg',
+              estTermine: true,
+              pourcentageProgression: 100,
+              nombreTotalDeQuestion: 8,
+            },
+          ],
+        },
+        bilanCarboneComplet: undefined,
+      };
+      const page = render(BilanCarbone, {
+        props: bilanCarboneProps,
+      });
+      expect(page.getByRole('heading', { level: 2, name: 'Ma première estimation' })).toBeDefined();
+    });
+  });
+
+  describe('Quand le bilan est complet', () => {
+    it('affiche le titre du bilan complet', () => {
+      const bilanCarboneProps: {
+        bilanCarbonePartiel?: BilanCarbonePartielViewModel;
+        bilanCarboneComplet?: BilanCarboneCompletViewModel;
+      } = {
+        bilanCarbonePartiel: undefined,
+        bilanCarboneComplet: {
+          titre: 'Mon bilan <span class="text--bleu">environnemental</span>',
+          pourcentageProgressBar: 52.18743032269549,
+          nombreDeTonnesAnnuel: '6,3',
+          impactKgAnnuel: { valeur: '6.3', unite: 'tonnes' },
+          univers: [
+            {
+              label: '🥦 Alimentation',
+              impactKgAnnuel: { valeur: '3.3', unite: 'tonnes' },
+              pourcentage: 52,
+              emoji: '🍴',
+              details: [
+                { emoji: '🥩', label: 'Viandes', impactKgAnnuel: { valeur: '2.2', unite: 'tonnes' }, pourcentage: 68 },
+                { emoji: '🥤', label: 'Boissons', impactKgAnnuel: { valeur: '336 ', unite: 'kg' }, pourcentage: 10 },
+                { emoji: '🐟', label: 'Poissons', impactKgAnnuel: { valeur: '182 ', unite: 'kg' }, pourcentage: 6 },
+                {
+                  emoji: '🥐',
+                  label: 'Petit déjeuner',
+                  impactKgAnnuel: { valeur: '113 ', unite: 'kg' },
+                  pourcentage: 3,
+                },
+                {
+                  emoji: '🥦',
+                  label: 'Fruits & Légumes',
+                  impactKgAnnuel: { valeur: '0 ', unite: 'kg' },
+                  pourcentage: 0,
+                },
+              ],
+            },
+            {
+              label: '🏛️ Services sociétaux',
+              impactKgAnnuel: { valeur: '1.5', unite: 'tonnes' },
+              pourcentage: 23,
+              emoji: '🏛️',
+              details: [
+                {
+                  emoji: '🏛',
+                  label: 'Services publics',
+                  impactKgAnnuel: { valeur: '1.3', unite: 'tonnes' },
+                  pourcentage: 87,
+                },
+                {
+                  emoji: '✉️',
+                  label: 'Services marchands',
+                  impactKgAnnuel: { valeur: '191 ', unite: 'kg' },
+                  pourcentage: 13,
+                },
+              ],
+            },
+            {
+              label: '🛒 Consommation durable',
+              impactKgAnnuel: { valeur: '766 ', unite: 'kg' },
+              pourcentage: 12,
+              emoji: '📦',
+              details: [
+                { emoji: '🐶', label: 'Animaux', impactKgAnnuel: { valeur: '358 ', unite: 'kg' }, pourcentage: 47 },
+                {
+                  emoji: '📦',
+                  label: 'Autres produits',
+                  impactKgAnnuel: { valeur: '123 ', unite: 'kg' },
+                  pourcentage: 16,
+                },
+                { emoji: '🎭', label: 'Loisirs', impactKgAnnuel: { valeur: '119 ', unite: 'kg' }, pourcentage: 16 },
+                {
+                  emoji: '🔌',
+                  label: 'Electroménager',
+                  impactKgAnnuel: { valeur: '49 ', unite: 'kg' },
+                  pourcentage: 6,
+                },
+                { emoji: '🛋️', label: 'Ameublement', impactKgAnnuel: { valeur: '47 ', unite: 'kg' }, pourcentage: 6 },
+                { emoji: '👕', label: 'Textile', impactKgAnnuel: { valeur: '18 ', unite: 'kg' }, pourcentage: 2 },
+                { emoji: '📺', label: 'Numérique', impactKgAnnuel: { valeur: '18 ', unite: 'kg' }, pourcentage: 2 },
+                { emoji: '🚬', label: 'Tabac', impactKgAnnuel: { valeur: '7 ', unite: 'kg' }, pourcentage: 1 },
+              ],
+            },
+            {
+              label: '🏡 Logement',
+              impactKgAnnuel: { valeur: '514 ', unite: 'kg' },
+              pourcentage: 8,
+              emoji: '🏠',
+              details: [
+                {
+                  emoji: '🧱',
+                  label: 'Construction',
+                  impactKgAnnuel: { valeur: '312 ', unite: 'kg' },
+                  pourcentage: 61,
+                },
+                {
+                  emoji: '❄️',
+                  label: 'Climatisation',
+                  impactKgAnnuel: { valeur: '169 ', unite: 'kg' },
+                  pourcentage: 33,
+                },
+                { emoji: '⚡', label: 'Electricité', impactKgAnnuel: { valeur: '24 ', unite: 'kg' }, pourcentage: 5 },
+                { emoji: '☘️', label: 'Extérieur', impactKgAnnuel: { valeur: '7 ', unite: 'kg' }, pourcentage: 1 },
+                { emoji: '🏖', label: 'Vacances', impactKgAnnuel: { valeur: '2 ', unite: 'kg' }, pourcentage: 0 },
+                { emoji: '🔥', label: 'Chauffage', impactKgAnnuel: { valeur: '0 ', unite: 'kg' }, pourcentage: 0 },
+                { emoji: '🏊', label: 'Piscine', impactKgAnnuel: { valeur: '0 ', unite: 'kg' }, pourcentage: 0 },
+              ],
+            },
+            {
+              label: '🚗 Transports',
+              impactKgAnnuel: { valeur: '262 ', unite: 'kg' },
+              pourcentage: 4,
+              emoji: '🚦',
+              details: [
+                { emoji: '✈️', label: 'Avion', impactKgAnnuel: { valeur: '207 ', unite: 'kg' }, pourcentage: 79 },
+                {
+                  emoji: '🚌',
+                  label: 'Transports en commun',
+                  impactKgAnnuel: { valeur: '34 ', unite: 'kg' },
+                  pourcentage: 13,
+                },
+                { emoji: '⛴', label: 'Ferry', impactKgAnnuel: { valeur: '12 ', unite: 'kg' }, pourcentage: 5 },
+                { emoji: '🚋', label: 'Train', impactKgAnnuel: { valeur: '9 ', unite: 'kg' }, pourcentage: 3 },
+                { emoji: '🚘️', label: 'Voiture', impactKgAnnuel: { valeur: '0 ', unite: 'kg' }, pourcentage: 0 },
+                { emoji: '🛵', label: '2 roues', impactKgAnnuel: { valeur: '0 ', unite: 'kg' }, pourcentage: 0 },
+                { emoji: '🚲', label: 'Mobilité douce', impactKgAnnuel: { valeur: '0 ', unite: 'kg' }, pourcentage: 0 },
+                { emoji: '🏖️', label: 'Vacances', impactKgAnnuel: { valeur: '0 ', unite: 'kg' }, pourcentage: 0 },
+              ],
+            },
+          ],
+          top3: [
+            { emoji: '🥩', label: 'Viandes', pourcentage: '36' },
+            { emoji: '🐶', label: 'Animaux', pourcentage: '6' },
+            { emoji: '🧱', label: 'Construction', pourcentage: '5' },
+          ],
+          thematiquesBilan: [
+            {
+              clefUnivers: 'transport',
+              contentId: 'ENCHAINEMENT_KYC_bilan_transport',
+              label: '🚗 Transports',
+              urlImage: 'https://res.cloudinary.com/dq023imd8/image/upload/v1728466903/Mobilite_df75aefd09.svg',
+              estTermine: true,
+              pourcentageProgression: 100,
+              nombreTotalDeQuestion: 9,
+            },
+            {
+              clefUnivers: 'alimentation',
+              contentId: 'ENCHAINEMENT_KYC_bilan_alimentation',
+              label: '🥦 Alimentation',
+              urlImage: 'https://res.cloudinary.com/dq023imd8/image/upload/v1728466523/cuisine_da54797693.svg',
+              estTermine: true,
+              pourcentageProgression: 100,
+              nombreTotalDeQuestion: 6,
+            },
+            {
+              clefUnivers: 'consommation',
+              contentId: 'ENCHAINEMENT_KYC_bilan_consommation',
+              label: '🛒 Consommation durable',
+              urlImage: 'https://res.cloudinary.com/dq023imd8/image/upload/v1728468852/conso_7522b1950d.svg',
+              estTermine: true,
+              pourcentageProgression: 100,
+              nombreTotalDeQuestion: 8,
+            },
+            {
+              clefUnivers: 'logement',
+              contentId: 'ENCHAINEMENT_KYC_bilan_logement',
+              label: '🏡 Logement',
+              urlImage: 'https://res.cloudinary.com/dq023imd8/image/upload/v1728468978/maison_80242d91f3.svg',
+              estTermine: true,
+              pourcentageProgression: 100,
+              nombreTotalDeQuestion: 8,
+            },
+          ],
+        },
+      };
+      const page = render(BilanCarbone, {
+        props: bilanCarboneProps,
+      });
+      expect(page.getByRole('heading', { level: 2, name: "Mes principaux postes d'émission" })).toBeDefined();
+    });
+  });
+});
