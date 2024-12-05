03fc1192500c7febe161ef274f65240ad460af0a
Matteo Leclercq <matteo.leclercq@octo.com>
2024-12-05 10:59:21 +0100

    tech: create Stepper component

--- src/components/custom/Mission/MissionNavigation.vue
+++ src/components/custom/Mission/MissionNavigation.vue
@@ -1,24 +1,17 @@
 <template>
   <div class="flex flex-column">
-    <div class="fr-stepper fr-mb-2w">
-      <h1 class="fr-stepper__title">
-        {{ titre }}
-        <span class="fr-stepper__state">
-          <span class="fr-text--bold"> Étape {{ etapeActuelle }} sur {{ etapeTotale }}</span>
-        </span>
-      </h1>
-      <div class="fr-stepper__steps fr-mb-1w" :data-fr-current-step="etapeActuelle" :data-fr-steps="etapeTotale"></div>
-      <button
-        class="fr-btn fr-btn--icon-left fr-btn--tertiary-no-outline fr-icon-arrow-left-line fr-pl-0"
-        @click="onClickRevenirEtapePrecedente"
-      >
-        Retour
-      </button>
-    </div>
+    <Stepper
+      :titre="titre"
+      :etape-actuelle="etapeActuelle"
+      :etape-totale="etapeTotale"
+      :fonction-retour="onClickRevenirEtapePrecedente"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+  import Stepper from '@/components/dsfr/Stepper.vue';
+
   defineProps<{
     titre: string;
     etapeTotale: number;
