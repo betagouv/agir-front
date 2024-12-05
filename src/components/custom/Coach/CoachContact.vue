1b89158187d865d8718adf2f2f93513d2e9693ba
Eric Boucher <eric.p.boucher@gmail.com>
2024-12-05 09:57:55 +0100

    feat: mise à jour du texte de feedback (#808)

--- src/components/custom/Coach/CoachContact.vue
+++ src/components/custom/Coach/CoachContact.vue
@@ -3,8 +3,8 @@
     <div class="fr-col-md-8">
       <h2 class="fr-h4 text--white">J'agis est un nouveau service qui continue de s’améliorer</h2>
       <p class="text--white fr-text--xl fr-mb-0">
-        N’hésitez pas à nous contacter par mail pour nous partager vos retours, poser vos questions et suggestions de
-        fonctionnalité que vous aimeriez voir sur <span class="text--italic">J'agis</span>.
+        Posez vos questions, partagez vos retours et suggérez de nouvelles fonctionnalités que vous aimeriez
+        voir sur <span class="text--italic">J'agis</span>.
       </p>
     </div>
     <div class="fr-col-md-4">
