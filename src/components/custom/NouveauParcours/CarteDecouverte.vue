b85bb49c2f392eece1be9205915db1edc4e42ffd
Matteo Leclercq <matteo.leclercq@octo.com>
2024-12-13 15:48:57 +0100

    tech: css

--- src/components/custom/NouveauParcours/CarteDecouverte.vue
+++ src/components/custom/NouveauParcours/CarteDecouverte.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="fr-col-12 fr-col-md-4">
-    <div class="shadow border-radius--md text--left fr-p-3w full-height flex flex-column flex-space-between">
+    <div
+      class="shadow border-radius--md text--left fr-p-3w full-height flex flex-column flex-space-between background--white"
+    >
       <div class="fr-mb-3v">
         <h3 class="fr-h4 fr-pb-2w fr-mb-2w fr-text--md">
           <span aria-hidden="true" class="fr-pr-1w">{{ titreEmoji }}</span> {{ titreTexte }}
