83271af206a3bde5eea9a4b99a3922e089359b60
Matteo Leclercq <matteo.leclercq@octo.com>
2024-12-17 15:24:43 +0100

    feat: input code postal

--- src/components/pages/nouveauParcours/PageParcoursPerso.vue
+++ src/components/pages/nouveauParcours/PageParcoursPerso.vue
@@ -1,67 +1,87 @@
+<!-- TODO: changer l'url -->
+<!-- TODO: articles pour dechets, climat, loisir et services_societaux ET nombre_aides_dechet, nombre_aides_climat -->
+<!-- TODO: dernières propriétés : nombreInscrits, nombrePointsMoyen, nombreArticlesLocaux, nombreArticlesTotal, nombreDefiEnCours, nombreDefiRealises -->
+<!-- TODO: où vous les chiffres des aides ? -->
+<!-- TODO: mettre le gras -->
+<!-- TODO: skeleton -->
+
 <template>
   <section class="fr-container fr-my-6w">
-    <CarteSkeleton v-if="isLoading" />
+    <form
+      @submit.prevent="lancerRecherche"
+      class="flex flex-column flex-center gap--small fr-mb-8w text--center width--fit-content"
+    >
+      <InputText
+        class="fr-mb-0"
+        name="codePostal"
+        label="Code postal"
+        description="Saisissez un code postal afin d'obtenir des statistiques"
+        v-model="codePostal"
+      />
+      <button type="submit" class="fr-btn full-width text--center">Valider</button>
+    </form>
 
-    <template v-else-if="nouveauParcoursViewmodel">
-      <section class="propositions text--center">
-        <h1 class="fr-h2 fr-mb-1w">Par où souhaitez-vous commencer ?</h1>
-        <p class="fr-mb-5w fr-mt-0">
-          D’après nos calculs 🧮, dans le
-          <span class="codePostal">{{ codePostal }}</span
-          >, voici ce que nous pouvons vous proposer :
-        </p>
+    <div>
+      <CarteSkeleton v-if="isLoading" />
+      <template v-else-if="nouveauParcoursViewmodel">
+        <section class="propositions text--center">
+          <h1 class="fr-h2 fr-mb-5w">
+            <span class="text--italic">J'agis</span> en quelques chiffres pour le code postal
+            <span class="codePostal">{{ codePostal }}</span>
+          </h1>
 
-        <div class="fr-grid-row fr-grid-row--gutters">
-          <CarteDecouverte
-            v-for="proposition in nouveauParcoursViewmodel.propositions"
-            :key="proposition.titre"
-            :lien-bouton="proposition.lien"
-            :titre-emoji="proposition.emoji"
-            :titre-texte="proposition.titre"
-          >
-            <ul>
-              <li v-for="item in proposition.contenu" :key="item" v-html="item"></li>
-            </ul>
-
-            <div v-if="proposition.articles.length > 0">
-              <p class="fr-m-0">Les articles :</p>
+          <div class="fr-grid-row fr-grid-row--gutters">
+            <CarteDecouverte
+              v-for="proposition in nouveauParcoursViewmodel.propositions"
+              :key="proposition.titre"
+              :lien-bouton="proposition.lien"
+              :titre-emoji="proposition.emoji"
+              :titre-texte="proposition.titre"
+            >
               <ul>
-                <li v-for="article in proposition.articles" :key="article.id">
-                  <router-link :to="`/article/previsualisation/${article.id}`">{{ article.titre }}</router-link>
-                </li>
+                <li v-for="item in proposition.contenu" :key="item" v-html="item"></li>
               </ul>
-            </div>
-          </CarteDecouverte>
-        </div>
-      </section>
-    </template>
+
+              <div v-if="proposition.articles.length > 0">
+                <p class="fr-m-0">Les articles :</p>
+                <ul>
+                  <li v-for="article in proposition.articles" :key="article.id">
+                    <router-link :to="`/article/previsualisation/${article.id}`">{{ article.titre }}</router-link>
+                  </li>
+                </ul>
+              </div>
+            </CarteDecouverte>
+          </div>
+        </section>
+      </template>
+    </div>
   </section>
 </template>
 
 <script lang="ts" setup>
-  import { onMounted, ref } from 'vue';
-  import { useRoute } from 'vue-router';
+  import { ref } from 'vue';
   import CarteDecouverte from '@/components/custom/NouveauParcours/CarteDecouverte.vue';
   import CarteSkeleton from '@/components/custom/Skeleton/CarteSkeleton.vue';
+  import InputText from '@/components/dsfr/InputText.vue';
   import { NouveauParcoursPresenterImpl } from '@/domaines/nouveauParcours/adapters/nouveauParcours.presenter.impl';
   import { NouveauParcoursRepositoryAxios } from '@/domaines/nouveauParcours/adapters/nouveauParcours.repository.axios';
   import { NouveauParcoursViewModel } from '@/domaines/nouveauParcours/ports/nouveauParcours.presenter';
   import { RecuperationDonneesNouveauParcoursUsecase } from '@/domaines/nouveauParcours/recuperationDonneesNouveauParcours.usecase';
 
-  const isLoading = ref<boolean>(true);
+  const isLoading = ref<boolean>(false);
   const nouveauParcoursViewmodel = ref<NouveauParcoursViewModel>();
 
-  const route = useRoute();
-  const codePostal = route.query.codePostal as string;
+  const codePostal = ref<string>('');
 
-  onMounted(() => {
+  function lancerRecherche() {
+    isLoading.value = true;
     const usecase = new RecuperationDonneesNouveauParcoursUsecase(new NouveauParcoursRepositoryAxios());
     usecase
-      .execute(codePostal, new NouveauParcoursPresenterImpl(vm => (nouveauParcoursViewmodel.value = vm)))
+      .execute(codePostal.value, new NouveauParcoursPresenterImpl(vm => (nouveauParcoursViewmodel.value = vm)))
       .finally(() => {
         isLoading.value = false;
       });
-  });
+  }
 </script>
 
 <style scoped>
@@ -73,5 +93,9 @@
 
   .propositions li:first-letter {
     text-transform: capitalize;
+  }
+
+  form {
+    justify-self: center;
   }
 </style>
