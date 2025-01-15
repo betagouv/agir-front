07f618d90e717eb74f5f4257f89f38a9871d3226
dlamande <contact@dorianlamande.com>
2025-01-15 11:01:34 +0100

    tech: amelioration de la gestion des erreurs (POC)

---
+++ src/components/custom/ErreurDeChargement.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <button class="fr-btn" @click="action">Réessayer</button>
+  </div>
+</template>
+<script lang="ts" setup>
+  defineProps<{
+    action: () => void;
+    message: string;
+  }>();
+</script>
--- src/components/pages/PageAides.vue
+++ src/components/pages/PageAides.vue
@@ -2,7 +2,12 @@
   <div class="fr-container fr-pb-3w">
     <FilDAriane page-courante="Mes aides" />
     <div v-if="isLoading">Chargement ...</div>
-    <div v-else-if="!aides">Une erreur est survenue</div>
+    <div v-else-if="!aides">
+      <ErreurDeChargement
+        :action="chargerLesAides"
+        message="Une erreur inattendue est survenue pendant le chargement des aides. Veuillez re-essayer"
+      />
+    </div>
     <div v-else>
       <div v-if="!aides.utilisateurEstCouvert" class="fr-alert fr-alert--info fr-mb-4w">
         <h3 class="fr-alert__title">Votre ville n’est pas encore couverte par le service J'agis.</h3>
@@ -20,6 +25,7 @@
   import '@gouvfr/dsfr/dist/component/alert/alert.min.css';
   import { onMounted, ref } from 'vue';
   import Aides from '@/components/custom/Aides/Aides.vue';
+  import ErreurDeChargement from '@/components/custom/ErreurDeChargement.vue';
   import FilDAriane from '@/components/dsfr/FilDAriane.vue';
   import { ChargementAidesPresenterImpl } from '@/domaines/aides/adapters/chargementAides.presenter.impl';
   import { ChargementAidesAxiosRepository } from '@/domaines/aides/adapters/chargementAidesAxiosRepository';
@@ -31,12 +37,20 @@
   const isLoading = ref<boolean>(true);
 
   onMounted(async () => {
+    chargerLesAides();
+  });
+
+  function chargerLesAides() {
     const { id: utilisateurId } = utilisateurStore().utilisateur;
     const usecase = new ChargementAidesUsecase(new ChargementAidesAxiosRepository());
-    await usecase.execute(
-      utilisateurId,
-      new ChargementAidesPresenterImpl(aidesViewModel => (aides.value = aidesViewModel)),
-    );
-    isLoading.value = false;
-  });
+    isLoading.value = true;
+    usecase
+      .execute(utilisateurId, new ChargementAidesPresenterImpl(aidesViewModel => (aides.value = aidesViewModel)))
+      .catch(() => {
+        aides.value = undefined;
+      })
+      .finally(() => {
+        isLoading.value = false;
+      });
+  }
 </script>
