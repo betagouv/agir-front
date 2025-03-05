7e2c6e61b3e78b0ae62a439c14e8c2f9f13bd4ba
dlamande <contact@dorianlamande.com>
2025-03-05 10:55:03 +0100

    ajout des tests pour le widget

--- tests/serviceRecherche/fruitsEtLegumes/recupererServiceFruitsEtLegumes.spec.ts
+++ tests/serviceRecherche/fruitsEtLegumes/recupererServiceFruitsEtLegumes.spec.ts
@@ -1,9 +1,13 @@
-import { RecupererServiceFruitsEtLegumesUsecase } from '../../../src/domaines/serviceRecherche/fruitsEtLegumes/recupererServiceFruitsEtLegumes.usecase';
+import { RecupererServiceFruitsEtLegumesUsecase } from '@/domaines/serviceRecherche/fruitsEtLegumes/recupererServiceFruitsEtLegumes.usecase';
 import {
   ServiceFruitsEtLegumesViewModel,
   ServiceRechercheFruitsEtLegumesPresenterImpl,
-} from '../../../src/domaines/serviceRecherche/fruitsEtLegumes/adapters/serviceRechercheFruitsEtLegumes.presenter.impl';
+} from '@/domaines/serviceRecherche/fruitsEtLegumes/adapters/serviceRechercheFruitsEtLegumes.presenter.impl';
 import { ServiceRechercheFruitsEtLegumesRepositoryMock } from './adapters/serviceRechercheFruitsEtLegumes.repository.mock';
+import {
+  ServiceFruitsEtLegumesDetailWidgetViewModel,
+  ServiceRechercheFruitsEtLegumesWidgetPresenterImpl,
+} from '@/domaines/serviceRecherche/fruitsEtLegumes/adapters/serviceRechercheFruitsEtLegumesWidget.presenter.impl';
 
 describe('Fichier de tests concernant la récuperation du service Fruits et Légumes', () => {
   it("en donnant l'id d'un utilisateur, renvoie la liste des fruits et légumes du mois triès par odre alphabétique", async () => {
@@ -125,6 +129,105 @@ describe('Fichier de tests concernant la récuperation du service Fruits et Lég
           },
         ],
       });
+    }
+  });
+  it('Cas de la presentation en Widget doit presenter seulement une liste', async () => {
+    // GIVEN
+    const usecase = new RecupererServiceFruitsEtLegumesUsecase(
+      new ServiceRechercheFruitsEtLegumesRepositoryMock({
+        listeFruitsEtLegumes: [
+          {
+            titre: 'Haricot',
+            impactCarboneKg: 0.4130619719,
+            urlImage: 'urlImage',
+            type: 'legume',
+          },
+          {
+            titre: 'Aubergine',
+            impactCarboneKg: 0.4571093429,
+            urlImage: 'urlImage',
+            type: 'legume',
+          },
+          {
+            titre: 'Ananas',
+            impactCarboneKg: 1.292282106,
+            urlImage: 'urlImage',
+            type: 'fruit',
+          },
+          {
+            titre: 'Cerise',
+            impactCarboneKg: 1.3353255069,
+            urlImage: 'urlImage',
+            type: 'fruit',
+          },
+          {
+            titre: 'Mangue',
+            impactCarboneKg: 10.641545366,
+            urlImage: 'urlImage',
+            type: 'fruit',
+          },
+          {
+            titre: 'Tomate',
+            impactCarboneKg: 0.581556477,
+            urlImage: 'urlImage',
+            type: 'fruit_et_legume',
+          },
+        ],
+        categories: [
+          {
+            code: 'mai',
+            label: 'mai',
+            estLaCategorieParDefaut: false,
+          },
+          {
+            code: 'juin',
+            label: 'juin',
+            estLaCategorieParDefaut: false,
+          },
+          {
+            code: 'juillet',
+            label: 'juillet',
+            estLaCategorieParDefaut: true,
+          },
+        ],
+      }),
+    );
+
+    // WHEN
+    await usecase.execute(
+      'idUtilisateur',
+      'juillet',
+      new ServiceRechercheFruitsEtLegumesWidgetPresenterImpl(expectation),
+    );
+
+    // THEN
+    function expectation(catalogueViewModel: ServiceFruitsEtLegumesDetailWidgetViewModel[]) {
+      expect(catalogueViewModel).toStrictEqual<ServiceFruitsEtLegumesDetailWidgetViewModel[]>([
+        {
+          nom: 'Haricot',
+          urlImage: 'urlImage',
+        },
+        {
+          nom: 'Aubergine',
+          urlImage: 'urlImage',
+        },
+        {
+          nom: 'Ananas',
+          urlImage: 'urlImage',
+        },
+        {
+          nom: 'Cerise',
+          urlImage: 'urlImage',
+        },
+        {
+          nom: 'Mangue',
+          urlImage: 'urlImage',
+        },
+        {
+          nom: 'Tomate',
+          urlImage: 'urlImage',
+        },
+      ]);
     }
   });
 });
