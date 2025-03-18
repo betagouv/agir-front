a0b19c5f836cdaef5967ca6317c53ebb4d154054
Matteo Leclercq <matteo.leclercq@octo.com>
2025-03-18 13:59:50 +0100

    tech: TU chargerActionBilan

---
+++ tests/actions/chargerActionBilan.usecase.spec.ts
@@ -0,0 +1,208 @@
+import { ChargerActionStrategyFactory, ChargerActionUsecase } from '@/domaines/actions/chargerAction.usecase';
+import { ActionsRepositoryMock } from './adapters/actions.repository.mock';
+import { ActionPresenterImpl } from '@/domaines/actions/adapters/action.presenter.impl';
+import { ActionBilanViewModel } from '@/domaines/actions/ports/action.presenter';
+import { ActionDetail, TypeAction } from '@/domaines/actions/ports/actions.repository';
+import { ChargerActionClassiqueUsecase } from '@/domaines/actions/chargerActionClassique.usecase';
+import { ChargerActionQuizUsecase } from '@/domaines/actions/chargerActionQuiz.usecase';
+import { ChargerActionSimulateurUsecase } from '@/domaines/actions/chargerActionSimulateur.usecase';
+import { ReponseKYCSimple, ThematiqueQuestion } from '@/domaines/kyc/recupererQuestion.usecase';
+import { ChargerActionBilanUsecase } from '@/domaines/actions/chargerActionBilan.usecase';
+import { ClefThematiqueAPI } from '@/domaines/thematiques/MenuThematiques';
+
+describe("Fichier de tests concernant la récupération d'une action de type bilan", () => {
+  it("En donnant l'id d'une action, on devrait pouvoir récupérer son entiereté", async () => {
+    const action: ActionDetail = {
+      thematique: ClefThematiqueAPI.alimentation,
+      realisee: false,
+      points: 30,
+      consigne: 'Consigne',
+      labelCompteur: '100 bilans réalisés',
+      kycs: [
+        {
+          id: 'questionId',
+          libelle: 'Une question',
+          type: 'libre',
+          points: 10,
+          thematique: ThematiqueQuestion.ALIMENTATION,
+          reponses: {
+            reponses_possibles: [],
+            reponse: [],
+          } as ReponseKYCSimple,
+          aEteRepondu: false,
+        },
+        {
+          id: 'questionId2',
+          libelle: 'Une question 2 ?',
+          type: 'decimal',
+          points: 20,
+          thematique: ThematiqueQuestion.TRANSPORT,
+          reponses: {
+            reponses_possibles: [],
+            reponse: [],
+          } as ReponseKYCSimple,
+          aEteRepondu: false,
+        },
+      ],
+      aides: [
+        {
+          titre: 'Titre aide 1',
+          id: 'id-aide-1',
+          partenaireNom: 'Partenaire',
+          partenaireImg: 'partenaire_img.png',
+          montantMaximum: 1200,
+          estGratuit: false,
+        },
+        {
+          titre: 'Titre aide 2',
+          id: 'id-aide-2',
+          partenaireNom: 'Partenaire2',
+          partenaireImg: 'partenaire_img2.png',
+          montantMaximum: 1200,
+          estGratuit: false,
+        },
+        {
+          titre: 'Titre aide 2',
+          id: 'id-aide-2',
+          partenaireNom: 'Partenaire2',
+          partenaireImg: 'partenaire_img2.png',
+          montantMaximum: undefined,
+          estGratuit: true,
+        },
+      ],
+      quizzFelicitations: '',
+      code: 'id-action-bilan-test',
+      nombreDeRealisations: 40,
+      nombreAidesDisponibles: 0,
+      type: TypeAction.BILAN,
+      titre: '**Bilan logement**',
+      quizzes: [],
+      sousTitre: 'Bilan logement sous titre',
+      commune: 'Noisiel',
+      corps: {
+        introduction: '## En **quelques mots**\n\n-, Pourquoi est-ce important de faire son bilan ?',
+        astuces: '',
+      },
+      recommandations: [
+        {
+          titre: 'Les bases de l’alimentation végétarienne : par où commencer ?',
+          image: '/temp_les_bases_alim_vege.png',
+          url: '',
+        },
+        {
+          titre: 'Comment répondre à ses besoins nutritionnels sans viande ?',
+          image: '/temp_comment_repondre_besoins_nutri.png',
+          url: '',
+        },
+        {
+          titre: '10 recettes végétariennes pour les fêtes',
+          image: '/temp_10_recettes_pour_fetes.png',
+          url: '',
+        },
+      ],
+      services: [],
+      faq: [],
+    };
+    const usecase = new ChargerActionUsecase(
+      new ChargerActionStrategyFactory(
+        new ChargerActionClassiqueUsecase(),
+        new ChargerActionQuizUsecase(),
+        new ChargerActionSimulateurUsecase(),
+        new ChargerActionBilanUsecase(),
+      ),
+      ActionsRepositoryMock.avecActionDetail(action),
+      new ActionPresenterImpl(
+        () => {},
+        () => {},
+        () => {},
+        expected,
+      ),
+    );
+    await usecase.execute('id-utilisateur', 'id-action', TypeAction.BILAN);
+
+    function expected(viewModel: ActionBilanViewModel): void {
+      expect(viewModel).toStrictEqual<ActionBilanViewModel>({
+        realisee: false,
+        points: 30,
+        consigne: 'Consigne',
+        labelCompteur: '100 bilans réalisés',
+        nombreDeRealisations: 40,
+        titre: '<span class="text--bold">Bilan logement</span>',
+        titreAffiche: '<span class="text--bold">Bilan logement</span>',
+        sousTitre: 'Bilan logement sous titre',
+        introduction: `<h2>En <span class="text--bold">quelques mots</span></h2><p>-, Pourquoi est-ce important de faire son bilan ?</p>
+`,
+        recommandations: [
+          {
+            image: '/temp_les_bases_alim_vege.png',
+            titre: 'Les bases de l’alimentation végétarienne : par où commencer ?',
+            url: '',
+          },
+          {
+            image: '/temp_comment_repondre_besoins_nutri.png',
+            titre: 'Comment répondre à ses besoins nutritionnels sans viande ?',
+            url: '',
+          },
+          {
+            image: '/temp_10_recettes_pour_fetes.png',
+            titre: '10 recettes végétariennes pour les fêtes',
+            url: '',
+          },
+        ],
+        aides: [
+          {
+            titre: 'Titre aide 1',
+            id: 'id-aide-1',
+            partenaireNom: 'Partenaire',
+            partenaireImg: 'partenaire_img.png',
+            titreUrl: 'titre-aide-1',
+            montantMaximum: '1200€',
+            estGratuit: false,
+          },
+          {
+            titre: 'Titre aide 2',
+            id: 'id-aide-2',
+            partenaireNom: 'Partenaire2',
+            partenaireImg: 'partenaire_img2.png',
+            titreUrl: 'titre-aide-2',
+            montantMaximum: '1200€',
+            estGratuit: false,
+          },
+          {
+            titre: 'Titre aide 2',
+            id: 'id-aide-2',
+            partenaireNom: 'Partenaire2',
+            partenaireImg: 'partenaire_img2.png',
+            titreUrl: 'titre-aide-2',
+            montantMaximum: undefined,
+            estGratuit: true,
+          },
+        ],
+        kycs: [
+          {
+            id: 'questionId',
+            libelle: 'Une question',
+            type: 'libre',
+            points: 'Récoltez vos + 10 points',
+            reponses_possibles: [],
+            aDejaEteRepondu: false,
+            description:
+              "Ces informations permettent à <span class='text--italic'>J'agis</span> de mieux comprendre vos habitudes alimentaires",
+          },
+          {
+            id: 'questionId2',
+            libelle: 'Une question 2 ?',
+            type: 'decimal',
+            points: 'Récoltez vos + 20 points',
+            reponses_possibles: [],
+            aDejaEteRepondu: false,
+            description:
+              "Ces informations permettent à <span class='text--italic'>J'agis</span> de mieux vous conseiller en matière de mobilité",
+          },
+        ],
+        actionId: 'id-action-bilan-test',
+        thematique: ClefThematiqueAPI.alimentation,
+      });
+    }
+  });
+});
