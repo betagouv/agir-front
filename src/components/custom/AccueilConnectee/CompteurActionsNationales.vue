fbb4ef21870d89297b5532fee70256da2eac2f4a
Matteo Leclercq <matteo.leclercq@octo.com>
2025-03-20 15:48:41 +0100

    feat: compteur dynamique

--- src/components/custom/AccueilConnectee/CompteurActionsNationales.vue
+++ src/components/custom/AccueilConnectee/CompteurActionsNationales.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="root fr-container flex flex-column flex-center fr-pt-10w fr-pb-16w">
     <h2 class="fr-h2 text--white text--center flex flex-column flex-center align-items--center">
-      <Compteur :compte="aidesNationalesRealisees ?? 0" />
+      <CompteurDynamique :valeur="aidesNationalesRealisees ?? 0" />
       <span class="fr-mt-4w">Actions réalisées ensemble partout en France</span>
     </h2>
   </div>
 </template>
 
 <script setup lang="ts">
-  import Compteur from '@/components/custom/AccueilConnectee/Compteur.vue';
+  import CompteurDynamique from '@/components/custom/AccueilConnectee/CompteurDynamique.vue';
 
   defineProps<{ aidesNationalesRealisees?: number }>();
 </script>
