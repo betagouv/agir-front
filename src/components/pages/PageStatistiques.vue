1517b36f4362d938b6868860fa0dcaafb2a4f7d3
dlamande <contact@dorianlamande.com>
2025-03-31 15:53:26 +0200

    feat: nouveau lien page statistiques

--- src/components/pages/PageStatistiques.vue
+++ src/components/pages/PageStatistiques.vue
@@ -3,16 +3,16 @@
     <h1>Statistiques d'utilisation de J'agis</h1>
     <div>
       <IframeResizer
-        src="https://agir-metabase-prod.osc-secnum-fr1.scalingo.io/public/dashboard/ee7ed49c-eb83-48ce-a548-ff74b6ff99c7"
-        title="Statistiques d'utilisation de J'agis"
         allowtransparency
         license="GPLv3"
+        src="https://agir-metabase-prod.osc-secnum-fr1.scalingo.io/public/dashboard/365a0702-4f9d-45b7-b8f5-7a07a4d48e16"
+        title="Statistiques d'utilisation de J'agis"
       />
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
   import IframeResizer from '@iframe-resizer/vue/iframe-resizer.vue';
 </script>
 
