cf0467e4f5e0e0bf326360d9c209d7c611e25ff1
Matteo Leclercq <matteo.leclercq@octo.com>
2025-03-31 14:05:06 +0200

    feat: wip pourquoi agir

---
+++ src/components/custom/Landing/LandingPourquoiAgir.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="text--center text--white">
+    <h2 class="fr-h2 fr-mb-6w text--white">Pourquoi agir ?</h2>
+
+    <ul class="fr-grid-row fr-grid-row--gutters list-style-none">
+      <li class="fr-col-lg-3 fr-col-md-6 fr-col-12">
+        <span
+          class="fr-icon-money-euro-circle-line fr-mb-1w fr-p-2w display-inline-block background--bleu-picto border-radius--full fr-mb-3w"
+          aria-hidden="true"
+        />
+        <h3 class="fr-h4 text--white fr-mb-1w">Faire des économies</h3>
+        <p>Alléger le budget et les factures liées à votre consommation</p>
+      </li>
+
+      <li class="fr-col-lg-3 fr-col-md-6 fr-col-12">
+        <span
+          class="fr-icon-lungs-line fr-mb-1w fr-p-2w display-inline-block background--bleu-picto border-radius--full fr-mb-3w"
+          aria-hidden="true"
+        />
+        <h3 class="fr-h4 text--white fr-mb-1w">Être en bonne santé</h3>
+        <p>Qualité de l'air, pollutions, impacts actuels</p>
+      </li>
+
+      <li class="fr-col-lg-3 fr-col-md-6 fr-col-12">
+        <span
+          class="fr-icon-parent-line fr-mb-1w fr-p-2w display-inline-block background--bleu-picto border-radius--full fr-mb-3w"
+          aria-hidden="true"
+        />
+        <h3 class="fr-h4 text--white fr-mb-1w">Protéger sa famille</h3>
+        <p>Faire des choix éclairés pour les générations futures</p>
+      </li>
+
+      <li class="fr-col-lg-3 fr-col-md-6 fr-col-12">
+        <span
+          class="fr-icon-heart-line fr-mb-1w fr-p-2w display-inline-block background--bleu-picto border-radius--full fr-mb-3w"
+          aria-hidden="true"
+        />
+        <h3 class="fr-h4 text--white fr-mb-1w">Préserver la nature</h3>
+        <p>Limiter les conséquences des changements déjà visibles</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+  .background--bleu-picto {
+    background-color: #1212ab;
+  }
+</style>
--- src/components/pages/PageAccueil.vue
+++ src/components/pages/PageAccueil.vue
@@ -9,14 +9,10 @@
       </div>
     </div>
   </section>
-  <section class="fr-py-8w background--white">
+
+  <section class="fr-py-12w rappel-cta">
     <div class="fr-container">
-      <LandingNotreApproche />
-    </div>
-  </section>
-  <section class="fr-py-12w background--vert rappel-app-cta">
-    <div class="fr-container">
-      <LandingRappelAppCTA />
+      <LandingPourquoiAgir />
     </div>
   </section>
   <section class="fr-py-12w">
@@ -24,21 +20,9 @@
       <LandingOnglet />
     </div>
   </section>
-  <section class="fr-py-12w rappel-cta">
+  <section class="fr-py-12w background--vert rappel-app-cta">
     <div class="fr-container">
-      <LandingRappelCTA />
-    </div>
-  </section>
-  <section class="fr-py-8w background--beige">
-    <div class="fr-container">
-      <div class="fr-col-md-10 fr-mx-auto">
-        <LandingEnsemble />
-      </div>
-    </div>
-  </section>
-  <section class="fr-py-16w rappel-app">
-    <div class="fr-container">
-      <LandingApp />
+      <LandingRappelAppCTA />
     </div>
   </section>
   <section class="fr-py-8w fr-pb-3w background--white">
@@ -52,16 +36,13 @@
 </template>
 
 <script setup lang="ts">
-  import LandingApp from '@/components/custom/Landing/LandingApp.vue';
   import LandingCitations from '@/components/custom/Landing/LandingCitations.vue';
   import LandingCollectivite from '@/components/custom/Landing/LandingCollectivite.vue';
-  import LandingEnsemble from '@/components/custom/Landing/LandingEnsemble.vue';
   import LandingHeader from '@/components/custom/Landing/LandingHeader.vue';
   import LandingNosSolutions from '@/components/custom/Landing/LandingNosSolutions.vue';
-  import LandingNotreApproche from '@/components/custom/Landing/LandingNotreApproche.vue';
   import LandingOnglet from '@/components/custom/Landing/LandingOnglet.vue';
+  import LandingPourquoiAgir from '@/components/custom/Landing/LandingPourquoiAgir.vue';
   import LandingRappelAppCTA from '@/components/custom/Landing/LandingRappelAppCTA.vue';
-  import LandingRappelCTA from '@/components/custom/Landing/LandingRappelCTA.vue';
 </script>
 
 <style scoped>
