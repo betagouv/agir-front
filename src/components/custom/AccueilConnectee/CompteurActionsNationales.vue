2e954f4cbc985026250f6647a32e16de6ee2cbba
Matteo Leclercq <matteo.leclercq@octo.com>
2025-04-04 17:26:35 +0200

    feat: meilleur gestion des labels sur la HP Connectée

--- src/components/custom/AccueilConnectee/CompteurActionsNationales.vue
+++ src/components/custom/AccueilConnectee/CompteurActionsNationales.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="root fr-container flex flex-column flex-center fr-pt-10w fr-pb-16w">
-    <h2 class="fr-h2 text--white text--center flex flex-column flex-center align-items--center">
+    <h2
+      class="fr-h2 text--white text--center flex flex-column flex-center align-items--center"
+      :aria-label="`${aidesNationalesRealisees ?? 0} actions réalisées ensemble partout en France`"
+    >
       <CompteurDynamique :valeur="aidesNationalesRealisees ?? 0" />
       <span class="fr-mt-4w">Actions réalisées ensemble partout en France</span>
     </h2>
