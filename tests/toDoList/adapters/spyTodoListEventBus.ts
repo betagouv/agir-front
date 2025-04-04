d9f24337802ab3a05fb139527c94bd042a7aa54c
dlamande <contact@dorianlamande.com>
2025-04-04 11:39:12 +0200

    feat: les articles lus ne rapportent plus de points

--- tests/toDoList/adapters/spyTodoListEventBus.ts
+++ tests/toDoList/adapters/spyTodoListEventBus.ts
@@ -2,22 +2,22 @@ import { ToDoListEvent } from '@/domaines/toDoList/toDoListEventBusImpl';
 import { EventBus } from '@/shell/eventBus';
 
 export class SpyToDoListEventBus extends EventBus<ToDoListEvent> {
-  get eventName(): ToDoListEvent | null {
-    return this._eventName;
-  }
-  private _eventName: ToDoListEvent | null = null;
-
-  publish(eventName: ToDoListEvent): void {
-    super.publish(eventName);
-    this._eventName = eventName;
-  }
-
   eventSubscribers: Record<ToDoListEvent, { subscriberName: string; callback: () => void }[]> = {
     [ToDoListEvent.TODO_POINTS_ONT_ETE_RECUPERE]: [],
-    [ToDoListEvent.TODO_ARTICLE_A_ETE_LU]: [],
     [ToDoListEvent.TODO_A_ETE_TERMINEE]: [],
     [ToDoListEvent.TODO_QUIZ_ETE_TERMINE]: [],
     [ToDoListEvent.TODO_KYC_A_ETE_REPONDU]: [],
     [ToDoListEvent.TODO_RECOMMANDATION_A_ETE_CLIQUEE]: [],
   };
+
+  private _eventName: ToDoListEvent | null = null;
+
+  get eventName(): ToDoListEvent | null {
+    return this._eventName;
+  }
+
+  publish(eventName: ToDoListEvent): void {
+    super.publish(eventName);
+    this._eventName = eventName;
+  }
 }
