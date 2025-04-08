c2b90cc54fe85a6cb3091ce4211efa0c3969336e
dlamande <contact@dorianlamande.com>
2025-04-08 11:04:31 +0200

    branch name

--- .validate-branch-namerc.cjs
+++ .validate-branch-namerc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  pattern: '^(tech|feature|fix)/.+$',
+  pattern: '^(tech|feature|fix|dependabot)/.+$',
   errorMsg:
-    '🤨 La branche que tu essaies de pusher ne respecte pas nos conventions (tech/, feature/, fix/), tu peux la renommer avec `git branch -m <nom-actuel> <nouveau-nom>`',
+    '🤨 La branche que tu essaies de pusher ne respecte pas nos conventions (tech/, feature/, fix/, dependabot/), tu peux la renommer avec `git branch -m <nom-actuel> <nouveau-nom>`',
 };
