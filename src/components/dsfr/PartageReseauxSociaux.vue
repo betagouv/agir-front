5618ef24f973f92f809e7ad0c387d3a23d184c58
dlamande <contact@dorianlamande.com>
2025-04-09 11:47:35 +0200

    feat: partage d'un article par mail ou lien direct (#1016)

---
+++ src/components/dsfr/PartageReseauxSociaux.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="share-7093" class="fr-share">
+    <p class="fr-share__title text--center">Partager la page</p>
+    <ul class="fr-btns-group">
+      <li>
+        <a
+          id="share-7097"
+          :href="`mailto:?subject=&body=${url}`"
+          class="fr-btn--mail fr-btn"
+          rel="noopener external"
+          target="_blank"
+          title="Partager par email - nouvelle fenêtre"
+        >
+          Partager par email
+        </a>
+      </li>
+      <li>
+        <button
+          id="share-7098"
+          class="fr-btn--copy fr-btn"
+          onclick="navigator.clipboard.writeText(window.location).then(function() {alert('Adresse copiée dans le presse papier.')});"
+        >
+          Copier dans le presse-papier
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+<script lang="ts" setup>
+  import '@gouvfr/dsfr/dist/component/share/share.min.css';
+  import { ref } from 'vue';
+
+  const url = ref<string>(window.location.href);
+</script>
