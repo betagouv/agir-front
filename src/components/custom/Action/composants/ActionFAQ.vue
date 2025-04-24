cdcface33e8c78b590f10ea1121552c6f4d28695
Matteo Leclercq <matteo.leclercq@octo.com>
2025-04-24 14:45:59 +0200

    feat: a11y FAQ

--- src/components/custom/Action/composants/ActionFAQ.vue
+++ src/components/custom/Action/composants/ActionFAQ.vue
@@ -10,27 +10,36 @@
       </Accordeon>
     </div>
     <div>
-      <button class="fr-link text--underline fr-p-0 fr-mt-2w" @click.prevent="poserUneQuestion = true">
-        Posez votre question
-      </button>
-      , notre equipe vous répond&nbsp;!
+      <p>
+        <button class="fr-link text--underline fr-p-0 fr-mt-2w" @click.prevent="poserUneQuestion = true">
+          Posez votre question
+        </button>
+        , notre equipe vous répond&nbsp;!
+      </p>
 
-      <div v-if="poserUneQuestion" class="background--main fr-py-4w fr-px-2w fr-mt-2w">
-        <p class="fr-h4">Posez une question</p>
-        <div class="flex">
-          <form class="fr-col-12 fr-col-md-8">
-            <div class="fr-input-group">
-              <label class="fr-label" for="textarea">
-                Une question ou une difficulté pour réaliser cette action&nbsp;?
-                <br />
-                Notre équipe vous répondra par email&nbsp;!
-              </label>
-              <textarea id="textarea" class="fr-input" name="textarea"></textarea>
-            </div>
+      <div v-if="poserUneQuestion" class="background--main fr-py-3w fr-px-2w fr-mt-2w">
+        <div class="fr-grid-row fr-grid-row--gutters">
+          <form class="fr-col-12 fr-col-md-9">
+            <h3 id="question-label" class="fr-h4">Posez une question</h3>
+
+            <p id="question-description">
+              Une question ou une difficulté pour réaliser cette action&nbsp;?
+              <br aria-hidden="true" />
+              Notre équipe vous répondra par email&nbsp;!
+            </p>
+
+            <textarea
+              aria-labelledby="question-label"
+              aria-describedby="question-description"
+              id="textarea"
+              class="fr-input fr-mb-3w"
+              name="textarea"
+            />
+
             <button class="fr-btn fr-btn--secondary fr-btn--icon-right fr-icon-arrow-right-s-line">Envoyer</button>
           </form>
-          <div class="fr-hidden fr-unhidden-md">
-            <img alt="" class="width--fit-content" src="/ic_faq.svg" />
+          <div class="fr-col-md-3 fr-hidden fr-unhidden-md">
+            <img alt="" class="full-width" src="/ic_faq.svg" />
           </div>
         </div>
       </div>
@@ -46,3 +55,10 @@
   defineProps<{ questionsFaq: ActionFAQ[] }>();
   const poserUneQuestion = ref<boolean>(false);
 </script>
+
+<style scoped>
+  textarea {
+    resize: none;
+    height: 7rem;
+  }
+</style>
