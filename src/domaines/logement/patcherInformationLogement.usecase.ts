7bf402f4e7222229faba946ac1c8bb1726b5fe98
Matteo Leclercq <matteo.leclercq@octo.com>
2025-04-30 17:42:57 +0200

    feat: definir adresse principale

---
+++ src/domaines/logement/patcherInformationLogement.usecase.ts
@@ -0,0 +1,10 @@
+import { LogementRepository } from '@/domaines/logement/ports/logement.repository';
+import { Logement } from '@/domaines/logement/recupererInformationLogement.usecase';
+
+export class PatcherInformationLogementUsecase {
+  constructor(private readonly logementRepository: LogementRepository) {}
+
+  async execute(idUtilisateur: string, logement: Partial<Logement>) {
+    await this.logementRepository.patcherLesInformations(idUtilisateur, logement);
+  }
+}
