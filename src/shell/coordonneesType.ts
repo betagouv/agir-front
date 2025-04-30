8ee41bb8dd08d3dbe7ddff0faffbcbea4f7badfd
Matteo Leclercq <matteo.leclercq@octo.com>
2025-04-30 17:42:57 +0200

    feat: definir adresse principale

--- src/shell/coordonneesType.ts
+++ src/shell/coordonneesType.ts
@@ -1,1 +1,11 @@
 export type Coordonnees = { latitude: number; longitude: number };
+export type Adresse = {
+  numeroEtRueEtCodePostal: string;
+  numeroEtRue: string;
+  commune: string;
+  departement: string;
+  codePostal: string;
+  coordonnees: Coordonnees;
+  numeroRue: string;
+  rue: string;
+};
