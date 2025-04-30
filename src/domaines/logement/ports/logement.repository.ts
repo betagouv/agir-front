7c06282657c1fc8add56aba78cfb71a55a261484
Matteo Leclercq <matteo.leclercq@octo.com>
2025-04-30 17:42:57 +0200

    feat: definir adresse principale

--- src/domaines/logement/ports/logement.repository.ts
+++ src/domaines/logement/ports/logement.repository.ts
@@ -3,4 +3,5 @@ import { Logement } from '@/domaines/logement/recupererInformationLogement.useca
 export interface LogementRepository {
   recupererInformation(utilisateurId: string): Promise<Logement>;
   enregistrerLesInformations(utilisateurId: string, logement: Logement): Promise<void>;
+  patcherLesInformations(utilisateurId: string, logement: Partial<Logement>): Promise<void>;
 }
