a53afe535b3122615cc50cdf28937bba6f76b1dd
dlamande <contact@dorianlamande.com>
2025-05-07 17:47:25 +0200

    fix mail dans écran d'attente

--- src/domaines/authentification/authentifierUtilisateur.usecase.ts
+++ src/domaines/authentification/authentifierUtilisateur.usecase.ts
@@ -1,9 +1,16 @@
+import { SessionRepository } from '@/domaines/authentification/ports/session.repository';
 import { UtilisateurRepository } from '@/domaines/authentification/ports/utilisateur.repository';
 
 export class AuthentifierUtilisateurUsecase {
-  constructor(private readonly utilisateurRepository: UtilisateurRepository) {}
+  constructor(
+    private readonly utilisateurRepository: UtilisateurRepository,
+    private readonly sessionRepository: SessionRepository,
+  ) {}
 
   async execute(email: string): Promise<void> {
     await this.utilisateurRepository.envoyerUnMagicLink(email);
+    this.sessionRepository.sauvegarderUtilisateur({
+      mail: email,
+    });
   }
 }
--- tests/authentification/authentifierUtilisateur.usecase.spec.ts
+++ tests/authentification/authentifierUtilisateur.usecase.spec.ts
@@ -1,11 +1,15 @@
 import { AuthentifierUtilisateurUsecase } from '@/domaines/authentification/authentifierUtilisateur.usecase';
 import { UtilisateurRepositorySpy } from './adapters/utilisateur.repository.spy';
+import { SessionRepositoryMock } from '../compte/sessionRepository.mock';
 
 describe("Fichier de tests concernant l'authentification ", () => {
   it("Lorsque je passe un email et un mot de passe doit lancer le processus d'authentification", async () => {
     // GIVEN
     const spyUtilisateurRepository = new UtilisateurRepositorySpy();
-    const usecase = new AuthentifierUtilisateurUsecase(spyUtilisateurRepository);
+    const usecase = new AuthentifierUtilisateurUsecase(
+      spyUtilisateurRepository,
+      SessionRepositoryMock.sansUtilisateurConnecte(),
+    );
     // WHEN
     await usecase.execute('john.doe@example.com');
     // THEN
