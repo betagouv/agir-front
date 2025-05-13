84e337cf528a6fd9af59dbbff54e2d1e4a7447d7
Matteo Leclercq <matteo.leclercq@octo.com>
2025-05-13 17:06:29 +0200

    tech: refactor pour suivre le back-end

--- src/components/custom/Service/ServiceBarreDeRechercheAdresse.vue
+++ src/components/custom/Service/ServiceBarreDeRechercheAdresse.vue
@@ -65,7 +65,7 @@
   import axios from 'redaxios';
   import { ref } from 'vue';
   import { useDebouncedFn } from '@/composables/useDebounce';
-  import { Adresse, Coordonnees } from '@/shell/coordonneesType';
+  import { DonneesAdresseBarreDeRecherche, Coordonnees } from '@/shell/coordonneesType';
 
   defineProps<{
     labelId?: string;
@@ -85,10 +85,10 @@
     };
   };
 
-  const adresseRef = defineModel<Adresse>('adresse');
+  const adresseRef = defineModel<DonneesAdresseBarreDeRecherche>('adresse');
   const coordonnees = defineModel<Coordonnees>('coordonnees');
   const recherche = defineModel<string>('recherche');
-  const adressesAffichees = ref<Adresse[]>([]);
+  const adressesAffichees = ref<DonneesAdresseBarreDeRecherche[]>([]);
   const indexSelectionne = ref<number>(-1);
   const dialogOuverte = ref<boolean>(false);
   const dialogRef = ref<HTMLDialogElement>();
@@ -111,7 +111,7 @@
     const url = `https://data.geopf.fr/geocodage/search/?q=${adresseEncodee}&limit=8&index=address&type=housenumber&autocomplete=1`;
     axios.get(url).then(response => {
       adressesAffichees.value = response.data.features.map(
-        (feature: FeatureApiModel): Adresse => ({
+        (feature: FeatureApiModel): DonneesAdresseBarreDeRecherche => ({
           numeroEtRueEtCodePostal: feature.properties.label,
           numeroEtRue: feature.properties.name,
           commune: feature.properties.city,
@@ -131,7 +131,7 @@
     dialogOuverte.value = true;
   };
 
-  function envoyerCoordonnees(adresse: Adresse) {
+  function envoyerCoordonnees(adresse: DonneesAdresseBarreDeRecherche) {
     indexSelectionne.value = -1;
     recherche.value = `${adresse.numeroEtRue}, ${adresse.commune} (${adresse.codePostal})`;
     coordonnees.value = {
