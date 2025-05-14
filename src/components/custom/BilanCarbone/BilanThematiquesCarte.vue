c43ed4ead42122983787c682dd684435ee32b50a
Matteo Leclercq <matteo.leclercq@octo.com>
2025-05-14 15:16:55 +0200

    feat: nouvelle UI pour cartes dans empreinte

--- src/components/custom/BilanCarbone/BilanThematiquesCarte.vue
+++ src/components/custom/BilanCarbone/BilanThematiquesCarte.vue
@@ -1,35 +1,38 @@
 <template>
-  <div
-    class="thematique-card position--relative fr-p-1w fr-pb-2w border-radius--md shadow background--white full-height"
+  <router-link
+    class="thematique-card position--relative fr-p-1w fr-pb-2w shadow background--white full-height flex flex-column"
+    :to="{
+      name: RouteBilanCarboneName.BILAN_CARBONE_ENCHAINEMENT,
+      params: {
+        thematiqueId: thematique,
+        id: contentId,
+      },
+    }"
+    :title="`${label}: affiner votre empreinte écologique avec ${nombreDeQuestions} questions ${estTermine ? '(Terminé) ' : `(achevé à ${progression}%)`}`"
   >
-    <span v-if="estTermine" class="thematique-card__badge fr-badge background--vert--success text--white">
-      Terminé !
-    </span>
-    <img class="full-width img-object-fit-cover border-radius--xs" height="150" :src="urlImage" alt="" />
+    <div>
+      <h3 class="fr-text--lg text--semi-bold text--black fr-mb-1v text--lh-1-3 fr-pt-1w">
+        {{ label }}
+      </h3>
+      <span class="fr-sr-only">,</span>
+      <p class="fr-m-0">{{ nombreDeQuestions }} questions</p>
+      <span class="fr-sr-only">,</span>
+    </div>
+
     <BarreDeProgression
       class="fr-mb-1w"
-      :label="`Progression ${thematique}`"
+      :label="`Progression ${thematique} : ${progression}%`"
       :value="progression"
       :value-max="100"
       :couleur="estTermine ? '#18753c' : '#0063CB'"
     />
-    <p class="fr-text--lg text--semi-bold text--black fr-mb-1v text--lh-1-3 fr-pt-1w">
-      <router-link
-        class="thematique-card__link"
-        :to="{
-          name: RouteBilanCarboneName.BILAN_CARBONE_ENCHAINEMENT,
-          params: {
-            thematiqueId: thematique,
-            id: contentId,
-          },
-        }"
-        :title="`${label}: affiner votre empreinte écologique sur cette thématique`"
-      >
-        {{ label }}
-      </router-link>
-    </p>
-    {{ nombreDeQuestions }} questions
-  </div>
+
+    <img class="full-width img-object-fit-cover border-radius--xs fr-mb-1w" height="150" :src="urlImage" alt="" />
+
+    <span v-if="estTermine" class="thematique-card__badge fr-badge background--vert--success text--white">
+      Terminé !
+    </span>
+  </router-link>
 </template>
 
 <script setup lang="ts">
@@ -49,30 +52,13 @@
 
 <style scoped>
   .thematique-card {
+    background-image: none;
     transition: box-shadow 0.3s ease-in-out;
+    flex-direction: column-reverse;
   }
 
   .thematique-card:hover {
     box-shadow: 0 6px 18px 0 rgba(0, 0, 18, 0.4);
-  }
-
-  .thematique-card__link {
-    background-image: none;
-    outline-width: 0;
-  }
-
-  .thematique-card__link::before {
-    content: '';
-    position: absolute;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 2;
-    outline-color: inherit;
-    outline-offset: 2px;
-    outline-style: inherit;
-    border-radius: 0.5rem;
   }
 
   .thematique-card__badge {
