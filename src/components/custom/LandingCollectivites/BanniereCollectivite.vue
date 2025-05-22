032524a5f638d038ba23d2ccdd2d880e6dd5a401
Matteo Leclercq <matteo.leclercq@octo.com>
2025-05-22 11:56:13 +0200

    feat: hero banner

--- src/components/custom/LandingCollectivites/BanniereCollectivite.vue
+++ src/components/custom/LandingCollectivites/BanniereCollectivite.vue
@@ -1,21 +1,40 @@
 <template>
-  <div class="banniere-collectivite">
-    <div class="fr-container text--center">
-      <h1 class="fr-display--xs">Comment engager vos habitants dans la transition écologique ?</h1>
-      <p>
-        <strong class="display-block">Déployez J’agis dans votre collectivité.</strong>
-        J’agis s’adapte à votre collectivité pour accompagner vos habitants et valoriser vos initiatives locales
+  <div class="banniere-collectivite position--relative fr-pt-10w">
+    <img src="/hp-hero-perso1-feuille.svg" alt="" class="feuilles-bottom-left" />
+    <img src="/hp-hero-perso2-feuille.svg" alt="" class="feuilles-bottom-right" />
+    <img src="/hp-hero-liere.svg" alt="" class="liere-right" />
+    <img src="/liere-2.svg" alt="" class="liere-left" />
+
+    <div class="fr-container text--center fr-pb-5w">
+      <div class="heading--container">
+        <h1 class="fr-display--xs">Comment engager vos habitants dans la transition écologique ?</h1>
+      </div>
+      <p class="fr-mb-4w">
+        <strong class="display-block fr-mb-1w">Déployez J’agis dans votre collectivité.</strong>
+        <i>J’agis</i> s’adapte à votre collectivité pour accompagner vos habitants et valoriser vos initiatives locales
       </p>
       <div class="position--relative">
         <!--        TODO: tracker le lien-->
         <a
           href="https://calendly.com/abdellah-bouhend-beta/rencontre-j-agis-pour-les-collectivites"
           class="fr-btn shadow"
+          aria-describedby="bouton-rdv-description"
           >Prendre rendez-vous</a
         >
+
+        <div class="fleche" aria-hidden="true"><img src="/collectivites-bouton-fleche.webp" alt="" /></div>
+        <div class="bouton-description">
+          <img
+            id="bouton-rdv-description"
+            src="/collectivites-bouton-description.webp"
+            alt="Découvrez les contenus déjà disponibles pour vos habitants"
+          />
+        </div>
       </div>
 
-      <ul class="banniere-collectivite__partenaires list-style-none fr-mr-0 flex align-items--center flex-center">
+      <ul
+        class="banniere-collectivite__partenaires list-style-none fr-mr-0 flex align-items--center flex-center flex-wrap"
+      >
         <li v-for="partenaire in partenaires" :key="partenaire.name">
           <img :src="partenaire.logo" :alt="partenaire.name" />
         </li>
@@ -50,17 +69,127 @@
 </script>
 
 <style scoped>
-  .banniere-collectivite {
-  }
-
   .banniere-collectivite__partenaires {
     margin-top: 4rem;
-    margin-bottom: 3rem;
     gap: 3rem;
   }
 
   .banniere-collectivite__partenaires img {
     max-width: 11rem;
     height: 5rem;
+  }
+
+  .heading--container {
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  .bouton-description {
+    width: 80%;
+    margin: 0 auto;
+    padding-top: 3rem;
+  }
+
+  .fleche {
+    display: none;
+    position: absolute;
+    top: 15%;
+    right: 80%;
+    width: 4rem;
+    transform: rotate(135deg);
+  }
+
+  .feuilles-bottom-left,
+  .feuilles-bottom-right {
+    display: none;
+  }
+
+  .liere-right {
+    display: none;
+  }
+
+  .liere-left {
+    position: absolute;
+    left: 0;
+    top: 25%;
+  }
+
+  /* xs */
+  @media (min-width: 20em) {
+    .fleche {
+      display: block;
+    }
+  }
+
+  /* sm */
+  @media (min-width: 36em) {
+    .bouton-description {
+      width: 50%;
+      margin: 0 auto;
+      padding-top: 4rem;
+    }
+
+    .fleche {
+      width: 6rem;
+      position: absolute;
+      top: 15%;
+      right: 72%;
+    }
+
+    .liere-right {
+      position: absolute;
+      right: 0;
+      display: block;
+    }
+
+    .feuilles-bottom-left,
+    .feuilles-bottom-right {
+      position: absolute;
+      bottom: 0;
+      z-index: 0;
+      display: block;
+    }
+
+    .feuilles-bottom-left {
+      right: 91%;
+    }
+
+    .feuilles-bottom-right {
+      right: -2%;
+    }
+  }
+
+  /* md */
+  @media (min-width: 48em) {
+    .fleche {
+      transform: rotate(0deg);
+      position: absolute;
+      left: 60%;
+      top: 100%;
+      height: 3rem;
+    }
+
+    .bouton-description {
+      padding-top: 0;
+      position: absolute;
+      left: 70%;
+      height: 3.5rem;
+      top: 0;
+      width: auto;
+    }
+  }
+
+  /* lg */
+  @media (min-width: 62em) {
+    .fleche {
+      top: 50%;
+    }
+  }
+
+  .fleche img,
+  .bouton-description img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
   }
 </style>
