4c13438ec9d598076924439fb35e61173e881619
Matteo Leclercq <matteo.leclercq@octo.com>
2025-05-22 16:41:56 +0200

    feat: intégration de la page

--- src/components/pages/PageCollectivite.vue
+++ src/components/pages/PageCollectivite.vue
@@ -2,8 +2,28 @@
   <section>
     <BanniereCollectivite />
   </section>
+
+  <section>
+    <ResponsableMobilisationCitoyenne />
+  </section>
+
+  <section>
+    <ApprocheSimplifiee lien-rdv="google.com" />
+  </section>
+
+  <section>
+    <CommuniquerSurJagis />
+  </section>
+
+  <section>
+    <LandingRappelAppCTA />
+  </section>
 </template>
 
 <script lang="ts" setup>
-  import BanniereCollectivite from '@/components/custom/LandingCollectivites/BanniereCollectivite.vue';
+  import ApprocheSimplifiee from '@/components/custom/CollectivitesLanding/ApprocheSimplifiee.vue';
+  import BanniereCollectivite from '@/components/custom/CollectivitesLanding/BanniereCollectivite.vue';
+  import CommuniquerSurJagis from '@/components/custom/CollectivitesLanding/CommuniquerSurJagis.vue';
+  import ResponsableMobilisationCitoyenne from '@/components/custom/CollectivitesLanding/ResponsableMobilisationCitoyenne.vue';
+  import LandingRappelAppCTA from '@/components/custom/Landing/LandingRappelAppCTA.vue';
 </script>
