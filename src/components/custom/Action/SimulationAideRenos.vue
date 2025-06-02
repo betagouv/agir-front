7137c8b39425e199f35fbfdc129b45beca7f36e8
Emile Rolley <emile.rolley@tuta.io>
2025-06-02 11:38:37 +0200

    feat(mes_aides_reno): gère la fin de l'action
    en renvoyant la nouvelle situation récupérée + en déclarant l'action
    comme étant réalisée

--- src/components/custom/Action/SimulationAideRenos.vue
+++ src/components/custom/Action/SimulationAideRenos.vue
@@ -1,26 +1,69 @@
 <template>
-  <iframe id="mesaidesreno" :src="url" allow="clipboard-read; clipboard-write" style="width: 100%"></iframe>
+  <iframe
+    ref="iframe_ref"
+    id="mesaidesreno-iframe"
+    :src="iframe_href"
+    allow="clipboard-read; clipboard-write"
+    style="width: 100%"
+  ></iframe>
 </template>
 
 <script lang="ts" setup>
-  import { onMounted, ref } from 'vue';
+  import { onMounted, ref, watchEffect } from 'vue';
   import { AxiosFactory } from '@/axios.factory';
+  import { ActionsEventBus } from '@/domaines/actions/actions.eventbus';
+  import { ActionsRepositoryAxios } from '@/domaines/actions/adapters/actions.repository.axios';
+  import { TypeAction } from '@/domaines/actions/ports/actions.repository';
+  import { TerminerActionUsecase } from '@/domaines/actions/terminerAction.usecase';
+  import { SimulateursSupportes } from '@/shell/simulateursSupportes';
   import { utilisateurStore } from '@/store/utilisateur';
 
-  const url = ref<string>('https://mesaidesreno.beta.gouv.fr/');
+  const iframe_href = ref<string>('https://mesaidesreno.beta.gouv.fr');
+  const iframe_ref = ref<HTMLIFrameElement | null>(null);
+  const situation = ref<object | null>(null);
+  const axios = AxiosFactory.getAxios();
 
   window.addEventListener('message', (event: MessageEvent) => {
-    if (event.data.kind === 'mesaidesreno-resize-height') {
-      const iframe = document.getElementById('mesaidesreno') as HTMLIFrameElement;
-      iframe.style.height = `${event.data.value}px`;
+    switch (event.data.kind) {
+      case 'mesaidesreno-resize-height': {
+        if (iframe_ref.value && event.data.value) {
+          iframe_ref.value.style.height = `${event.data.value}px`;
+        }
+        break;
+      }
+      case 'mesaidesreno-eligibility-done':
+        situation.value = event.data.situation;
+        break;
     }
   });
 
   onMounted(async () => {
-    const axios = AxiosFactory.getAxios();
-    const data = await axios.get(`/utilisateurs/${utilisateurStore().utilisateur.id}/mes_aides_reno/get_iframe_urls`);
-    url.value = data.data.iframe_url;
+    const res = await axios.get(`/utilisateurs/${utilisateurStore().utilisateur.id}/mes_aides_reno/get_iframe_urls`);
+    iframe_href.value = res.data.iframe_url;
   });
+
+  watchEffect(async () => {
+    if (situation.value) {
+      await onEligiliteAtteinte(situation.value);
+      situation.value = null; // Reset situation after processing
+    }
+  });
+
+  const onEligiliteAtteinte = async (situation: object) => {
+    await axios.post(`/utilisateurs/${utilisateurStore().utilisateur.id}/mes_aides_reno/nouvelle_situation`, {
+      situation,
+    });
+
+    const terminerActionUsecase = new TerminerActionUsecase(
+      new ActionsRepositoryAxios(),
+      ActionsEventBus.getInstance(),
+    );
+    await terminerActionUsecase.execute(
+      utilisateurStore().utilisateur.id,
+      SimulateursSupportes.MES_AIDES_RENO,
+      TypeAction.SIMULATEUR,
+    );
+  };
 </script>
 
 <style scoped></style>
