383acd6269b3a011b3ba45680989b39ba7017cb5
dlamande <contact@dorianlamande.com>
2025-06-12 10:27:14 +0200

    dsfr css pour la search

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ import '@gouvfr/dsfr/dist/dsfr.module.min.js';
 import '@gouvfr/dsfr/dist/component/select/select.min.css';
 import '@gouvfr/dsfr/dist/component/stepper/stepper.min.css';
 import '@gouvfr/dsfr/dist/component/card/card.min.css';
+import '@gouvfr/dsfr/dist/component/search/search.min.css';
 
 import './assets/theme/style.css';
 import './assets/theme/markdownFromCMS.css';
