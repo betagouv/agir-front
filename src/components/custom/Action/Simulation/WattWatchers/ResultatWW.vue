39e093e9fd71e1df7125c3cdafbcd21955af32d7
Matteo Leclercq <matteo.leclercq@octo.com>
2025-06-17 16:02:55 +0200

    feat: refacto graph

--- src/components/custom/Action/Simulation/WattWatchers/ResultatWW.vue
+++ src/components/custom/Action/Simulation/WattWatchers/ResultatWW.vue
@@ -14,41 +14,30 @@
         v-for="(item, index) in detailConsommations"
         :key="item.label"
         class="emoji-bubble"
-        :style="bubbleStyle(index, item.color)"
+        :style="positionnerBulle(index, item.color)"
       >
         {{ item.emoji }}
       </div>
     </div>
 
-    <ul class="fr-px-2w list-style-none liste-categories fr-pb-3w fr-my-0">
-      <li
-        v-for="categorie in detailConsommations"
-        :key="categorie.label"
-        class="custom-disc flex align-items--center"
-        :class="`disc-${categorie.id}`"
-      >
-        <div class="flex flex-space-between full-width align-items--baseline">
-          <span class="fr-text--sm fr-mb-0">
-            {{ categorie.label }}
-          </span>
-          <span class="text--bold">{{ categorie.pourcentage }}</span>
-        </div>
-      </li>
-    </ul>
+    <ListeDetailConsommation :detail-consommations="detailConsommations" />
   </div>
 </template>
 
 <script lang="ts" setup>
   import { ArcElement, Chart as ChartJS, ChartData, ChartOptions } from 'chart.js';
-  import { nextTick, onMounted, onUnmounted, ref } from 'vue';
+  import { computed, nextTick, onMounted, onUnmounted, ref } from 'vue';
   import { Doughnut } from 'vue-chartjs';
+  import ListeDetailConsommation from '@/components/custom/Action/Simulation/WattWatchers/ListeDetailConsommation.vue';
   import { ResultatWWViewModel } from '@/domaines/wattWatchers/ports/wattWatchers.repository';
 
   ChartJS.register(ArcElement);
-  const { totalConsommation, detailConsommations } = defineProps<{
+
+  const props = defineProps<{
     totalConsommation: ResultatWWViewModel['totalConsommation'];
     detailConsommations: ResultatWWViewModel['detailConsommations'];
   }>();
+
   const chartRef = ref();
   const chartSize = ref<{ height: number; width: number }>({ height: 0, width: 0 });
 
@@ -63,7 +52,7 @@
 
   const updateChartSize = async () => {
     await nextTick();
-    if (chartRef.value && chartRef.value.$el) {
+    if (chartRef.value?.$el) {
       chartSize.value = {
         height: chartRef.value.$el.clientHeight,
         width: chartRef.value.$el.clientWidth,
@@ -71,17 +60,18 @@
     }
   };
 
-  const graphData: ChartData<'doughnut'> = {
-    labels: detailConsommations.map(c => c.label),
+  const graphData = computed<ChartData<'doughnut'>>(() => ({
+    labels: props.detailConsommations.map(c => c.label),
     datasets: [
       {
-        data: detailConsommations.map(c => c.value),
-        backgroundColor: detailConsommations.map(c => c.color),
+        data: props.detailConsommations.map(c => c.value),
+        backgroundColor: props.detailConsommations.map(c => c.color),
         borderRadius: 10,
         spacing: 2,
       },
     ],
-  };
+  }));
+
   const graphOptions: ChartOptions<'doughnut'> = {
     events: [],
     cutout: '90%',
@@ -93,22 +83,23 @@
     },
   };
 
-  const bubbleStyle = (index: number, color: string) => {
-    const radius = Math.max(chartSize.value.width, chartSize.value.height) / 2 - 5;
-    const total = detailConsommations.reduce((sum, item) => sum + item.value, 0);
+  const valeurTotale = computed(() => props.detailConsommations.reduce((sum, item) => sum + item.value, 0));
 
-    const valueBefore = detailConsommations.slice(0, index).reduce((sum, item) => sum + item.value, 0);
-    const currentValue = detailConsommations[index].value;
+  const positionnerBulle = (index: number, color: string) => {
+    const distance = Math.max(chartSize.value.width, chartSize.value.height) / 2 - 5;
 
-    const angle = ((valueBefore + currentValue / 2) / total) * 2 * Math.PI - Math.PI / 2;
-    const x = radius * Math.cos(angle);
-    const y = radius * Math.sin(angle);
+    const valeurPrecedente = props.detailConsommations.slice(0, index).reduce((sum, item) => sum + item.value, 0);
+    const valeurActuelle = props.detailConsommations[index].value;
+
+    const angle = ((valeurPrecedente + valeurActuelle / 2) / valeurTotale.value) * 2 * Math.PI - Math.PI / 2;
+    const x = distance * Math.cos(angle);
+    const y = distance * Math.sin(angle);
 
     return {
       left: `calc(50% + ${x}px)`,
       top: `calc(50% + ${y}px)`,
       backgroundColor: color,
-      'z-index': detailConsommations.length - index,
+      'z-index': props.detailConsommations.length - index,
     };
   };
 </script>
@@ -134,45 +125,5 @@
     left: 50%;
     transform: translate(-50%, -50%);
     text-align: center;
-  }
-
-  .custom-disc::before {
-    content: '';
-    display: inline-block;
-    width: 14px;
-    height: 9px;
-    border-radius: 12px;
-    background-color: #0a76f6;
-    margin-right: 0.75rem;
-  }
-
-  .liste-categories {
-    columns: 1;
-
-    @media (min-width: 32rem) {
-      columns: 2;
-    }
-  }
-
-  .disc-chauffage::before {
-    background-color: v-bind('detailConsommations[0].color');
-  }
-  .disc-eau-chaude::before {
-    background-color: v-bind('detailConsommations[1].color');
-  }
-  .disc-autres::before {
-    background-color: v-bind('detailConsommations[2].color');
-  }
-  .disc-cuisson::before {
-    background-color: v-bind('detailConsommations[3].color');
-  }
-  .disc-electromenager::before {
-    background-color: v-bind('detailConsommations[4].color');
-  }
-  .disc-eclairage::before {
-    background-color: v-bind('detailConsommations[5].color');
-  }
-  .disc-multimedia::before {
-    background-color: v-bind('detailConsommations[6].color');
   }
 </style>
