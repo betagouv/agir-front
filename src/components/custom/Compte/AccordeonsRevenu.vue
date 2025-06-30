7c311bc4acfbd8c1752a5ae3b35942f008f14d41
Matteo Leclercq <matteo.leclercq@octo.com>
2025-06-30 15:33:02 +0200

    feat: page supprimer mon compte

---
+++ src/components/custom/Compte/AccordeonsRevenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="fr-accordions-group background--white">
+    <Accordeon name-id="ou-trouver">
+      <template #titre>Où trouver ces informations&nbsp;?</template>
+      <template #contenu>
+        <p>
+          Le revenu fiscal de référence et votre nombre de parts se trouvent sur la 1ère page de votre dernier avis
+          d’impôt.
+        </p>
+        <p>
+          <strong>Nombre de parts&nbsp;:</strong><br />
+          Si vous ne disposez pas de votre dernier avis d’impôt, renseignez 1 part pour chaque adulte de votre foyer
+          fiscal, puis 0,5 part par enfant jusqu’à 2 enfants, puis 1 part par enfant à partir du 3ème enfant.
+        </p>
+        <p class="fr-mb-0">
+          <strong>Revenu fiscal de référence&nbsp;:</strong><br />
+          Si vous ne disposez pas de votre dernier avis d’impôt, renseignez la somme des revenus de toutes les personnes
+          avec lequelles vous partagez vos déclarations d’impôts (pour toute l’année) pour vous faire une première idée.
+        </p>
+      </template>
+    </Accordeon>
+    <Accordeon name-id="pourquoi">
+      <template #titre>Pourquoi ces questions&nbsp;?</template>
+      <template #contenu>
+        <p class="fr-mb-0">
+          Votre revenu fiscal de référence et le nombre de parts permettent d’afficher les aides en fonction de vos
+          ressources.
+        </p>
+      </template>
+    </Accordeon>
+  </div>
+</template>
+<script setup lang="ts">
+  import Accordeon from '@/components/dsfr/Accordeon.vue';
+</script>
--- src/components/custom/Compte/CompteFormulaire.vue
+++ src/components/custom/Compte/CompteFormulaire.vue
@@ -83,37 +83,7 @@
         </div>
       </fieldset>
 
-      <div class="fr-accordions-group background--white">
-        <Accordeon name-id="ou-trouver">
-          <template #titre>Où trouver ces informations&nbsp;?</template>
-          <template #contenu>
-            <p>
-              Le revenu fiscal de référence et votre nombre de parts se trouvent sur la 1ère page de votre dernier avis
-              d’impôt.
-            </p>
-            <p>
-              <strong>Nombre de parts&nbsp;:</strong><br />
-              Si vous ne disposez pas de votre dernier avis d’impôt, renseignez 1 part pour chaque adulte de votre foyer
-              fiscal, puis 0,5 part par enfant jusqu’à 2 enfants, puis 1 part par enfant à partir du 3ème enfant.
-            </p>
-            <p class="fr-mb-0">
-              <strong>Revenu fiscal de référence&nbsp;:</strong><br />
-              Si vous ne disposez pas de votre dernier avis d’impôt, renseignez la somme des revenus de toutes les
-              personnes avec lequelles vous partagez vos déclarations d’impôts (pour toute l’année) pour vous faire une
-              première idée.
-            </p>
-          </template>
-        </Accordeon>
-        <Accordeon name-id="pourquoi">
-          <template #titre>Pourquoi ces questions&nbsp;?</template>
-          <template #contenu>
-            <p class="fr-mb-0">
-              Votre revenu fiscal de référence et le nombre de parts permettent d’afficher les aides en fonction de vos
-              ressources.
-            </p>
-          </template>
-        </Accordeon>
-      </div>
+      <AccordeonsRevenu />
 
       <CompteFormulaireBoutonEnregistrer />
     </form>
@@ -123,9 +93,9 @@
 <script lang="ts" setup>
   import { ref, useTemplateRef } from 'vue';
   import Alert from '@/components/custom/Alert.vue';
+  import AccordeonsRevenu from '@/components/custom/Compte/AccordeonsRevenu.vue';
   import CompteFormulaireBoutonEnregistrer from '@/components/custom/Compte/CompteFormulaireBoutonEnregistrer.vue';
   import CompteFormulaireRevenuFiscal from '@/components/custom/Compte/CompteFormulaireRevenuFiscal.vue';
-  import Accordeon from '@/components/dsfr/Accordeon.vue';
   import InputDateDeNaissance from '@/components/dsfr/InputDateDeNaissance.vue';
   import InputText from '@/components/dsfr/InputText.vue';
   import { validationPrenomOuNom, validationPseudoTaille } from '@/components/validations/validationsChampsFormulaire';
