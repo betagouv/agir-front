9cbe5689bbea5a58836cfb67dddeb6dff6b0ef40
Matteo Leclercq <matteo.leclercq@octo.com>
2025-07-09 14:30:29 +0200

    feat(simulation): focus sur bouton suivant

--- src/components/custom/Action/Simulation/Voiture/SimulationVoiture.vue
+++ src/components/custom/Action/Simulation/Voiture/SimulationVoiture.vue
@@ -3,15 +3,17 @@
     :action-id="SimulateursSupportes.VOITURE"
     :idEnchainementKycs="idEnchainementKycs"
     :type-action="TypeAction.SIMULATEUR"
+    @fin-kyc-atteinte="focusResultat"
     class="fr-px-2w"
   >
     <template v-slot:fin>
-      <SimulationResultatVoiture />
+      <SimulationResultatVoiture ref="simulationResultatVoitureRef" />
     </template>
   </KyCsAction>
 </template>
 
 <script setup lang="ts">
+  import { nextTick, ref } from 'vue';
   import SimulationResultatVoiture from '@/components/custom/Action/Simulation/Voiture/SimulationResultatVoiture.vue';
   import KyCsAction from '@/components/custom/KYC/KYCsAction.vue';
   import { TypeAction } from '@/domaines/actions/ports/actions.repository.js';
@@ -20,4 +22,11 @@
   defineProps<{
     idEnchainementKycs: string;
   }>();
+
+  const simulationResultatVoitureRef = ref<InstanceType<typeof SimulationResultatVoiture>>();
+
+  async function focusResultat() {
+    await nextTick();
+    simulationResultatVoitureRef.value?.focus();
+  }
 </script>
