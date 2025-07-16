4ee5968b9ee6a5df51d6ccf1414db8b7ba0a3a85
dlamande <contact@dorianlamande.com>
2025-07-16 17:34:43 +0200

    feat: ajout du service maif dans la thematique logement (#1231)

--- src/components/pages/WidgetsServicesMeLoger.vue
+++ src/components/pages/WidgetsServicesMeLoger.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="fr-mt-4w">
-    <div class="fr-grid-row fr-grid-row--gutters fr-my-6w">
-      <section class="full-height fr-col-12 fr-col-md-6 fr-pr-md-2w">
-        <h2 class="fr-h3">Rénover mon logement</h2>
+    <section class="fr-grid-row fr-grid-row--gutters fr-my-6w">
+      <div class="full-height fr-col-12 fr-col-md-6 fr-pr-md-2w">
+        <h2 class="fr-h3 fr-p-0">Les risques naturels</h2>
+
         <CarteDsfr
           :horizontal="true"
           :to="{
             name: RouteActionsName.ACTION_INDIVIDUELLE,
             params: {
               type: TypeAction.SIMULATEUR,
-              id: 'simu_aides_reno',
+              id: SimulateursSupportes.MAIF,
+              titre: 'connaitre-les-risques-naturels-auxquels-votre-logement-est-expose',
+            },
+          }"
+          image="/carte_service_maif.svg"
+          size="sm"
+          titre="Êtes-vous exposé à des risques ?"
+          variations="shadow"
+        >
+          <template #contenuEnd>
+            <p class="fr-card__desc">
+              Exposition aux risques climatiques, services de proximité, prix de l’immobilier… Retrouvez toutes les
+              infos utiles aux alentours de votre adresse&nbsp;!
+            </p>
+          </template>
+        </CarteDsfr>
+      </div>
+
+      <div class="full-height fr-col-12 fr-col-md-6 fr-pr-md-2w">
+        <h2 class="fr-h3 fr-p-0">Rénover mon logement</h2>
+
+        <CarteDsfr
+          :horizontal="true"
+          :to="{
+            name: RouteActionsName.ACTION_INDIVIDUELLE,
+            params: {
+              type: TypeAction.SIMULATEUR,
+              id: SimulateursSupportes.MES_AIDES_RENO,
               titre: 'calculer-vos-aides-pour-renover-votre-logement',
             },
           }"
@@ -25,12 +53,13 @@
             </p>
           </template>
         </CarteDsfr>
-      </section>
-    </div>
+      </div>
+    </section>
   </div>
 </template>
 <script lang="ts" setup>
   import CarteDsfr from '@/components/dsfr/CarteDsfr.vue';
   import { TypeAction } from '@/domaines/actions/ports/actions.repository';
   import { RouteActionsName } from '@/router/actions/routes';
+  import { SimulateursSupportes } from '@/shell/simulateursSupportes';
 </script>
