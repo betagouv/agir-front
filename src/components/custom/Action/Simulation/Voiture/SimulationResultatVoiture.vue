9f0a3e5add89a966ce564d408bedfd828881ef27
Emile Rolley <emile.rolley@tuta.io>
2025-07-18 17:17:30 +0200

    feat: intégration des alternatives pour le simulateur voiture

--- src/components/custom/Action/Simulation/Voiture/SimulationResultatVoiture.vue
+++ src/components/custom/Action/Simulation/Voiture/SimulationResultatVoiture.vue
@@ -4,38 +4,15 @@
       <p>Calcul du résultat...</p>
       <div class="ball-loader fr-my-3w fr-mx-auto"></div>
     </div>
-    <section v-else-if="resultatSimulationVoiture" class="fr-p-2w">
-      <div v-if="resultatSimulationVoiture?.resultatVoitureActuelle">
-        <h2 class="fr-h3">Votre véhicule actuel</h2>
-        <div class="background--white shadow fr-p-2w fr-mb-4w">
-          <p>
-            Coût annuel:
-            <span class="text--bold">{{ resultatSimulationVoiture.resultatVoitureActuelle.coutAnnuel }}</span>
-          </p>
-
-          <p>
-            Emissions annuelles:
-            <span class="text--bold">{{ resultatSimulationVoiture.resultatVoitureActuelle.emissionAnnuelle }}</span
-            >&nbsp;kgCO2e
-          </p>
-
-          <hr />
-
-          <ul class="fr-grid-row list-style-none">
-            <li
-              v-for="tag in resultatSimulationVoiture.resultatVoitureActuelle.tag"
-              :key="tag"
-              class="fr-tag fr-tag--secondary fr-mr-1w"
-              v-text="tag"
-            />
-          </ul>
-        </div>
-      </div>
+    <section v-else-if="resultatSimulationVoiture">
+      <SimulationResultatVoitureActuelle
+        v-if="resultatSimulationVoiture.resultatVoitureActuelle"
+        :resultat-simulation-voiture="resultatSimulationVoiture.resultatVoitureActuelle"
+        class="fr-mb-2w"
+      />
 
       <div>
-        <h2 class="fr-h3">
-          Les meilleurs alternatives pour le gabarit {{ resultatSimulationVoiture.resultatVoitureActuelle.gabarit }}
-        </h2>
+        <h2 class="fr-h3">Les meilleures solutions pour vous</h2>
         <div class="fr-grid-row fr-grid-row--gutters">
           <SimulationResultatVoitureProposee
             v-if="resultatSimulationVoiture.resultatVoiturePlusEconomique"
@@ -55,7 +32,6 @@
 
 <script lang="ts" setup>
   import { onMounted, ref } from 'vue';
-  import SimulationResultatVoitureProposee from '@/components/custom/Action/Simulation/Voiture/SimulationResultatVoitureProposee.vue';
   import {
     ResultatSimulationVoiturePresenterImpl,
     ResultatSimulationVoitureViewModel,
@@ -63,6 +39,8 @@
   import { SimulateurVoitureRepositoryAxios } from '@/domaines/simulationVoiture/adapters/simulateurVoiture.repository.axios';
   import { CalculerResultatSimulationVoitureUsecase } from '@/domaines/simulationVoiture/calculerResultatSimulationVoiture.usecase';
   import { utilisateurStore } from '@/store/utilisateur';
+  import SimulationResultatVoitureProposee from '@/components/custom/Action/Simulation/Voiture/SimulationResultatVoitureProposee.vue';
+  import SimulationResultatVoitureActuelle from '@/components/custom/Action/Simulation/Voiture/SimulationResultatVoitureActuelle.vue';
 
   const isLoading = ref<boolean>(false);
   const simulationResultat = ref<HTMLDivElement>();
