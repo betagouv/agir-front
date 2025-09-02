3ae17bd34f87a553e092e44ced12f96ace8196fe
dlamande <contact@dorianlamande.com>
2025-09-02 10:14:30 +0200

    fix: gerer pluriel action watt watchers

--- src/components/custom/Action/Simulation/WattWatchers/RedirectionActionsWW.vue
+++ src/components/custom/Action/Simulation/WattWatchers/RedirectionActionsWW.vue
@@ -1,14 +1,14 @@
 <template>
   <section
-    class="actions fr-mx-3w text--center fr-px-1w fr-py-2w jagis-background--bleu-light"
     v-if="nombreActions > 0"
+    class="actions fr-mx-3w text--center fr-px-1w fr-py-2w jagis-background--bleu-light"
   >
     <h3 class="fr-h4 fr-mb-1w">Faites des économies</h3>
     <p class="fr-mb-2w">
       Découvrez des actions personnalisées pour réduire votre facture d’électricité sans sacrifier votre confort.
     </p>
     <router-link :to="{ name: RouteActionsName.CATALOGUE_ACTION_WINTER }" class="fr-btn fr-btn--lg fr-mb-1w"
-      >Découvrir {{ nombreActions }} actions
+      >Découvrir {{ nombreActions }} {{ gererPluriel(nombreActions, 'action', 'actions') }}
     </router-link>
   </section>
 </template>
@@ -16,6 +16,7 @@
 <script lang="ts" setup>
   import { ResultatWWViewModel } from '@/domaines/simulationWattWatchers/adapters/resultatWattWatchers.presenter.impl';
   import { RouteActionsName } from '@/router/actions/routes';
+  import { gererPluriel } from '@/shell/pluriel';
 
   defineProps<{ nombreActions: ResultatWWViewModel['nombreActions'] }>();
 </script>
