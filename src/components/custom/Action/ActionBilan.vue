ac70f753cfee9e95ae308658a85f6fa3a13b126e
Matteo-OCTO <matteo.leclercq@octo.com>
2025-09-09 10:11:39 +0200

    feat: ajout d'un fallback de connexion pour les actions complexes hors connexion (#1319)

--- src/components/custom/Action/ActionBilan.vue
+++ src/components/custom/Action/ActionBilan.vue
@@ -2,16 +2,25 @@
   <section class="background--white fr-p-2w fr-mb-3w shadow">
     <ActionIntroduction :introduction="actionBilanViewModel.introduction" :sources="actionBilanViewModel.sources" />
 
-    <KyCsAction
-      :action-id="actionBilanViewModel.actionId"
-      :idEnchainementKycs="actionBilanViewModel.idEnchainementKYCs"
-      :type-action="TypeAction.BILAN"
-      class="fr-px-2w"
-    >
-      <template v-slot:fin>
-        <ActionBilanResultat :id-action="actionBilanViewModel.actionId" :thematique="actionBilanViewModel.thematique" />
-      </template>
-    </KyCsAction>
+    <template v-if="!estConnecte">
+      <ContenuNonConnecteFallback contenu="bilan" />
+    </template>
+
+    <template v-else>
+      <KyCsAction
+        :action-id="actionBilanViewModel.actionId"
+        :idEnchainementKycs="actionBilanViewModel.idEnchainementKYCs"
+        :type-action="TypeAction.BILAN"
+        class="fr-px-2w"
+      >
+        <template v-slot:fin>
+          <ActionBilanResultat
+            :id-action="actionBilanViewModel.actionId"
+            :thematique="actionBilanViewModel.thematique"
+          />
+        </template>
+      </KyCsAction>
+    </template>
 
     <ActionAides :aides="actionBilanViewModel.aides" />
   </section>
@@ -21,9 +30,12 @@
   import ActionAides from '@/components/custom/Action/composants/ActionAides.vue';
   import ActionBilanResultat from '@/components/custom/Action/composants/ActionBilanResultat.vue';
   import ActionIntroduction from '@/components/custom/Action/composants/ActionIntroduction.vue';
+  import ContenuNonConnecteFallback from '@/components/custom/Action/ContenuNonConnecteFallback.vue';
   import KyCsAction from '@/components/custom/KYC/KYCsAction.vue';
   import { ActionBilanViewModel } from '@/domaines/actions/ports/action.presenter';
   import { TypeAction } from '@/domaines/actions/ports/actions.repository';
+  import { utilisateurStore } from '@/store/utilisateur';
 
   defineProps<{ actionBilanViewModel: ActionBilanViewModel }>();
+  const estConnecte = utilisateurStore().estConnecte;
 </script>
--- src/components/custom/Action/ActionQuiz.vue
+++ src/components/custom/Action/ActionQuiz.vue
@@ -1,27 +1,33 @@
 <template>
   <section class="fr-mb-4w fr-p-2w background--white shadow">
-    <template v-if="indexQuestionActuelle < actionQuizViewModel.quizzes.length">
-      <Navigation
-        ref="navigationRef"
-        :etape-actuelle="indexQuestionActuelle + 1"
-        :etape-totale="actionQuizViewModel.quizzes.length"
-        :on-click-revenir-etape-precedente="retournerQuestionPrecedente"
-      />
-      <ActionQuizComposant
-        v-if="quizActuel"
-        :key="indexQuestionActuelle"
-        :article="quizActuel.articleAssocie ?? undefined"
-        :est-derniere-question="indexQuestionActuelle + 1 === actionQuizViewModel?.quizzes.length"
-        :on-click-continuer="passerQuestionSuivante"
-        :question="quizActuel.question"
-        :quiz-id="quizActuel.id"
+    <template v-if="!estConnecte">
+      <ContenuNonConnecteFallback contenu="quiz" />
+    </template>
+
+    <template v-else>
+      <template v-if="indexQuestionActuelle < actionQuizViewModel.quizzes.length">
+        <Navigation
+          ref="navigationRef"
+          :etape-actuelle="indexQuestionActuelle + 1"
+          :etape-totale="actionQuizViewModel.quizzes.length"
+          :on-click-revenir-etape-precedente="retournerQuestionPrecedente"
+        />
+        <ActionQuizComposant
+          v-if="quizActuel"
+          :key="indexQuestionActuelle"
+          :article="quizActuel.articleAssocie ?? undefined"
+          :est-derniere-question="indexQuestionActuelle + 1 === actionQuizViewModel?.quizzes.length"
+          :on-click-continuer="passerQuestionSuivante"
+          :question="quizActuel.question"
+          :quiz-id="quizActuel.id"
+        />
+      </template>
+      <ActionQuizTerminee
+        v-else
+        :felicitations="actionQuizViewModel.quizzFelicitations"
+        :titre="actionQuizViewModel.titre"
       />
     </template>
-    <ActionQuizTerminee
-      v-else
-      :felicitations="actionQuizViewModel.quizzFelicitations"
-      :titre="actionQuizViewModel.titre"
-    />
   </section>
 </template>
 
@@ -29,9 +35,12 @@
   import { computed, ref } from 'vue';
   import ActionQuizComposant from '@/components/custom/Action/composants/ActionQuizComposant.vue';
   import ActionQuizTerminee from '@/components/custom/Action/composants/ActionQuizTerminee.vue';
+  import ContenuNonConnecteFallback from '@/components/custom/Action/ContenuNonConnecteFallback.vue';
   import Navigation from '@/components/custom/Navigation.vue';
   import { ActionQuizViewModel, ActionQuizzesViewModel } from '@/domaines/actions/ports/action.presenter';
+  import { utilisateurStore } from '@/store/utilisateur';
 
+  const estConnecte = utilisateurStore().estConnecte;
   const navigationRef = ref<InstanceType<typeof Navigation>>();
   const props = defineProps<{ actionQuizViewModel: ActionQuizzesViewModel }>();
 
