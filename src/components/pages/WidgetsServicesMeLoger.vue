aa790ebd1292944cc0a60dc3b1a898ecb2a00ed6
Matteo-OCTO <matteo.leclercq@octo.com>
2025-09-09 15:01:34 +0200

    fix: cacher maif temporairement (#1330)

--- src/components/pages/WidgetsServicesMeLoger.vue
+++ src/components/pages/WidgetsServicesMeLoger.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="fr-mt-4w">
     <section class="fr-grid-row fr-grid-row--gutters fr-my-6w">
-      <div class="full-height fr-col-12 fr-col-md-6 fr-pr-md-2w">
-        <h2 class="fr-h3 fr-p-0">Les risques naturels</h2>
+      <!--      <div class="full-height fr-col-12 fr-col-md-6 fr-pr-md-2w">-->
+      <!--        <h2 class="fr-h3 fr-p-0">Les risques naturels</h2>-->
 
-        <CarteDsfr
-          :horizontal="true"
-          :to="{
-            name: RouteActionsName.ACTION_INDIVIDUELLE,
-            params: {
-              type: TypeAction.SIMULATEUR,
-              id: SimulateursSupportes.MAIF,
-              titre: 'connaitre-les-risques-naturels-auxquels-votre-logement-est-expose',
-            },
-          }"
-          image="/carte_service_maif.svg"
-          size="sm"
-          titre="Êtes-vous exposé à des risques ?"
-          variations="shadow"
-        >
-          <template #contenuEnd>
-            <p class="fr-card__desc">
-              Exposition aux risques climatiques, services de proximité, prix de l’immobilier… Retrouvez toutes les
-              infos utiles aux alentours de votre adresse&nbsp;!
-            </p>
-          </template>
-        </CarteDsfr>
-      </div>
+      <!--        <CarteDsfr-->
+      <!--          :horizontal="true"-->
+      <!--          :to="{-->
+      <!--            name: RouteActionsName.ACTION_INDIVIDUELLE,-->
+      <!--            params: {-->
+      <!--              type: TypeAction.SIMULATEUR,-->
+      <!--              id: SimulateursSupportes.MAIF,-->
+      <!--              titre: 'connaitre-les-risques-naturels-auxquels-votre-logement-est-expose',-->
+      <!--            },-->
+      <!--          }"-->
+      <!--          image="/carte_service_maif.svg"-->
+      <!--          size="sm"-->
+      <!--          titre="Êtes-vous exposé à des risques ?"-->
+      <!--          variations="shadow"-->
+      <!--        >-->
+      <!--          <template #contenuEnd>-->
+      <!--            <p class="fr-card__desc">-->
+      <!--              Exposition aux risques climatiques, services de proximité, prix de l’immobilier… Retrouvez toutes les-->
+      <!--              infos utiles aux alentours de votre adresse&nbsp;!-->
+      <!--            </p>-->
+      <!--          </template>-->
+      <!--        </CarteDsfr>-->
+      <!--      </div>-->
 
       <div class="full-height fr-col-12 fr-col-md-6 fr-pr-md-2w">
         <h2 class="fr-h3 fr-p-0">Rénover mon logement</h2>
