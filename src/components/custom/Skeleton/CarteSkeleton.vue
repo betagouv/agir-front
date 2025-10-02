4a8e02205608ae49187f705bb635c61f6f3eb56d
Matteo Leclercq <matteo.leclercq@octo.com>
2025-10-02 17:32:24 +0200

    tech: clear components

--- src/components/custom/Skeleton/CarteSkeleton.vue
+++ src/components/custom/Skeleton/CarteSkeleton.vue
@@ -1,12 +1,19 @@
 <template>
-  <CarteVierge>
+  <div class="fr-p-6v">
     <Skeleton width="200px" has-margin-bottom class="fr-mx-auto" />
     <Skeleton width="100px" has-margin-bottom class="fr-mx-auto" />
     <Skeleton width="70px" class="fr-mx-auto" />
-  </CarteVierge>
+  </div>
 </template>
 
 <script setup lang="ts">
-  import CarteVierge from '../../CarteVierge.vue';
   import Skeleton from '@/components/custom/Skeleton/Skeleton.vue';
 </script>
+
+<style scoped>
+  div {
+    background-color: #fff;
+    border-radius: 6px;
+    border: 1px solid rgba(0, 0, 0, 0.19);
+  }
+</style>
